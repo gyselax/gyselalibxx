1196e4259de877844b2fc420bbc03654fb3e1d13
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2021-05-05 23:11:10 +0200

    Try to add call to boundary conditions in test_splines.cpp

--- tests/test_splines.cpp
+++ tests/test_splines.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+
+#include <boundary_conditions.h>
+
 using namespace std;
 
 using real = double ;
 
-real eval_cos_scalar(real &x, coeffs, derivative) 
+//BoundaryCondition::sll_p_periodic;
+//constexpr int BC_GREVILLE = BoundaryCondition::sll_p_greville;
+//constexpr int BC_HERMITE  = BoundaryCondition::sll_p_hermite;
+
+
+real eval_cos_scalar(real &x, vector<real> &coeffs, int *derivative=NULL) 
 {
-  // real(F64)                , intent(in   ) :: x
-  //vector<real> coeffs[2];  //real(F64), dimension(0:1), intent(in   ) :: coeffs
-  //integer  , optional      , intent(in   ) :: derivative
+    real y;
 
-  int d;                                       //integer :: d
-  constexpr real pi = 3.141592653589793;       //real(F64), parameter :: pi = 3.141592653589793_f64
-  constexpr real pi_2 = 6.283185307179586_f64; //real(F64), parameter :: pi_2 = 6.283185307179586_f64
+    int d;
+    constexpr real pi = 3.141592653589793;
+    constexpr real pi_2 = 6.283185307179586;
 
-  //if (present(derivative)) then; d = derivative; else; d = 0; end if
+    if ( derivative==NULL )
+        d = 0 ;
+    else
+        d = *derivative; 
 
-  y = (pi_2 * coeffs(0))**d * cos( 0.5_F64*pi*d + pi_2 * (coeffs(0)*x + coeffs(1) ) );
-  return y;
+    y = pow(pi_2 * coeffs[0],d)*cos( 0.5*pi*d + pi_2 * (coeffs[0]*x + coeffs[1]) ) ;
+
+    cout << "y=" << y << endl;
+    return y;
+}
+
+
+void cos_splines_test(
+      real &max_norm_error,
+      real &max_norm_error_diff,
+      real &max_norm_error_int,
+      int  const &degree,
+      real const &h,
+      int  const &N,
+      real const &x0,
+      real const &xN,
+      int  const &bc_xmin,
+      int  const &bc_xmax,
+      real const *coeffs,
+      vector<real> *eval_pts_input)
+{
+    
 }
 
 
 int main()
 {
+  //const int ncells;
+  //const int bc_xmin, bc_xmax;
 
-  //real(F64)                 :: h, x0, xN, ncells
-  //integer                   :: N, bc_xmin, bc_xmax
-  //
-  //x0 = 0.0_F64
-  //xN = 1.0_F64
-  //N  = 22
-  //
-  //!*** coordinate array allocation ***
-  //allocate(eval_pts(0:N))
-  //
-  //!*** coordinate array initialisation ***
-  //h = (xN-x0) / real(N, kind=F64)
-  //do i = 0,N
-  //  eval_pts(i) = x0+i*h
-  //enddo
-  //
-  //max_norm_profile = 1.0_F64
+  constexpr real x0 = 0.0;
+  constexpr real xN = 1.0;
+  constexpr int N = 22;
+
+  constexpr real h = (xN-x0) / float(N);
+
+  cout << "h=" << h << endl;
+
+  // coordinate array initialization
+  std::vector <real> eval_pts(N);
+  //for (int i=0; i<eval_pts.size();i++) eval_pts.operator[](i) = x0+i*h;
+  for (int i=0; i<eval_pts.size();i++) eval_pts.at(i) = x0+i*h;
+  for (real pts: eval_pts) cout << pts << endl;
+
+  //max_norm_profile = 1.0
   //
   //write(*,*)
   //write(*,'(a)') '---------------------------------------------------------------------------'
