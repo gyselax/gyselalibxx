0c70de627f3d45413fc3486599e92e729ba3b9c6
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2021-05-06 10:01:55 +0200

    test_splines:step 1.

--- tests/test_splines.cpp
+++ tests/test_splines.cpp
@@ -1,32 +1,24 @@
+#include <array>
+#include <cmath>
 #include <iostream>
-#include <math.h>
 #include <vector>
 
-#include <boundary_conditions.h>
+#include "boundary_conditions.h"
 
 using namespace std;
 
-using real = double ;
-
 //BoundaryCondition::sll_p_periodic;
-//constexpr int BC_GREVILLE = BoundaryCondition::sll_p_greville;
+constexpr BoundaryCondition BC_GREVILLE = BoundaryCondition::sll_p_greville;
 //constexpr int BC_HERMITE  = BoundaryCondition::sll_p_hermite;
 
 
-real eval_cos_scalar(real &x, vector<real> &coeffs, int *derivative=NULL) 
+double eval_cos_scalar(double const x, vector<double> const & coeffs, int const derivative=0)
 {
-    real y;
+    constexpr double TWOPI = 2.*M_PI;
 
-    int d;
-    constexpr real pi = 3.141592653589793;
-    constexpr real pi_2 = 6.283185307179586;
+    int const d = derivative;
 
-    if ( derivative==NULL )
-        d = 0 ;
-    else
-        d = *derivative; 
-
-    y = pow(pi_2 * coeffs[0],d)*cos( 0.5*pi*d + pi_2 * (coeffs[0]*x + coeffs[1]) ) ;
+    double const y = pow(TWOPI * coeffs[0], d) * cos(M_PI_2 * d + M_PI * (coeffs[0] * x + coeffs[1]));
 
     cout << "y=" << y << endl;
     return y;
@@ -34,47 +26,50 @@ real eval_cos_scalar(real &x, vector<real> &coeffs, int *derivative=NULL)
 
 
 void cos_splines_test(
-      real &max_norm_error,
-      real &max_norm_error_diff,
-      real &max_norm_error_int,
-      int  const &degree,
-      real const &h,
-      int  const &N,
-      real const &x0,
-      real const &xN,
-      int  const &bc_xmin,
-      int  const &bc_xmax,
-      real const *coeffs,
-      vector<real> *eval_pts_input)
+        double& max_norm_error,
+        double& max_norm_error_diff,
+        double& max_norm_error_int,
+        int const degree,
+        double const h,
+        int const N,
+        double const x0,
+        double const xN,
+        int const bc_xmin,
+        int const bc_xmax,
+        array<double, 2> const coeffs,
+        vector<double> const& eval_pts_input)
 {
-    
 }
 
 
 int main()
 {
-  //const int ncells;
-  //const int bc_xmin, bc_xmax;
+    //const int ncells;
+    //const int bc_xmin, bc_xmax;
 
-  constexpr real x0 = 0.0;
-  constexpr real xN = 1.0;
-  constexpr int N = 22;
+    constexpr double x0 = 0.0;
+    constexpr double xN = 1.0;
+    constexpr int N = 22;
 
-  constexpr real h = (xN-x0) / float(N);
+    constexpr double h = (xN - x0) / N;
 
-  cout << "h=" << h << endl;
+    cout << "h=" << h << endl;
 
-  // coordinate array initialization
-  std::vector <real> eval_pts(N);
-  //for (int i=0; i<eval_pts.size();i++) eval_pts.operator[](i) = x0+i*h;
-  for (int i=0; i<eval_pts.size();i++) eval_pts.at(i) = x0+i*h;
-  for (real pts: eval_pts) cout << pts << endl;
+    // coordinate array initialization
+    std::vector<double> eval_pts(N);
+    for (int i = 0; i < eval_pts.size(); i++){
+        eval_pts[i] = x0 + i * h;
+    }
 
-  //max_norm_profile = 1.0
-  //
-  //write(*,*)
-  //write(*,'(a)') '---------------------------------------------------------------------------'
-  //write(*,'(a)') ' TEST: convergence analysis on cos profile (with absolute error bound)     '
-  //write(*,'(a)') '---------------------------------------------------------------------------'
-  //write(*,*)
+    for (double pts : eval_pts){
+        cout << pts << endl;
+    }
+
+    //max_norm_profile = 1.0
+    //
+    //write(*,*)
+    //write(*,'(a)') '---------------------------------------------------------------------------'
+    //write(*,'(a)') ' TEST: convergence analysis on cos profile (with absolute error bound)     '
+    //write(*,'(a)') '---------------------------------------------------------------------------'
+    //write(*,*)
 }
