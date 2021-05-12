d5551fd6d33319722b7c4fbccfcf4b33244522b1
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2021-05-12 15:20:42 +0200

    New version of Splines but where there is still a compiling problem when line 266 of splines.cpp file is uncommented.

--- tests/splines.cpp
+++ tests/splines.cpp
@@ -102,20 +102,21 @@ void cos_splines_test(
 
     // computation of rhs'(0) and rhs'(n)
     // -> deriv_rhs(0) = rhs'(n) and deriv_rhs(1) = rhs'(0)
-    int const shift = 1 - (degree % 2); // shift = 1 for even order, 0 for odd order
+    int const shift 
+    = 1 - (degree % 2); // shift = 1 for even order, 0 for odd order
 
     if (bc_xmin == BoundCond::HERMITE) {
         vector<double> Sderiv_lhs_data(degree / 2);
         View1D<double> Sderiv_lhs(Sderiv_lhs_data.data(), Sderiv_lhs_data.size());
-        for (int i = 1; i <= degree / 2; ++i) {
-            Sderiv_lhs(i - 1) = eval_cos(x0, coeffs, i - shift);
+        for (int ii = 1; ii <= degree / 2; ++ii) {
+            Sderiv_lhs(ii - 1) = eval_cos(x0, coeffs, ii - shift);
         }
         if (bc_xmax == BoundCond::HERMITE) {
             vector<double> Sderiv_rhs_data(degree / 2);
             View1D<double> Sderiv_rhs(Sderiv_rhs_data.data(), Sderiv_rhs_data.size());
 
-            for (int i = 1; i <= degree / 2; ++i) {
-                Sderiv_rhs(i - 1) = eval_cos(xN, coeffs, i - shift);
+            for (int ii = 1; ii <= degree / 2; ++ii) {
+                Sderiv_rhs(ii - 1) = eval_cos(xN, coeffs, ii - shift);
             }
 
             spline_interpolator.compute_interpolant(spline, yvals, &Sderiv_lhs, &Sderiv_rhs);
@@ -139,65 +140,168 @@ void cos_splines_test(
     max_norm_error = 0.;
     max_norm_error_diff = 0.;
 
-    for (int i = 0; i < eval_pts.extent(0); ++i) {
+    for (int ii = 0; ii < eval_pts.extent(0); ++ii) {
         // Check eval function
-        double const spline_value = spline.eval(eval_pts[i]);
+        double const spline_value = spline.eval(eval_pts[ii]);
 
         // Compute error
-        double const error = spline_value - eval_cos(eval_pts[i], coeffs);
+        double const error = spline_value - eval_cos(eval_pts[ii], coeffs);
         max_norm_error = max(max_norm_error, abs(error));
 
         // Check eval_deriv function
-        double const spline_deriv_value = spline.eval_deriv(eval_pts[i]);
+        double const spline_deriv_value = spline.eval_deriv(eval_pts[ii]);
 
         // Compute error
-        double const error_deriv = spline_deriv_value - eval_cos(eval_pts[i], coeffs, 1);
+        double const error_deriv = spline_deriv_value - eval_cos(eval_pts[ii], coeffs, 1);
         max_norm_error_diff = max(max_norm_error_diff, abs(error_deriv));
     }
-    /*        
-    do i = lbound(eval_pts,1), ubound(eval_pts,1)
-          !*** Check eval function ***
-          spline_value = spline1d_eval( spline, eval_pts(i) )
 
-          !*** Compute error ***
-          error = spline_value - eval_cos(eval_pts(i), coeffs)
-          max_norm_error = max( max_norm_error, abs( error ) )
-
-
-          !*** Check eval_deriv function ***
-          spline_value = spline1d_eval_deriv( spline, eval_pts(i) )
-
-          !*** Compute error ***
-          error = spline_value - eval_cos(eval_pts(i), coeffs, 1)
-          max_norm_error_diff = max( max_norm_error_diff, abs( error ) )
-      end do
-      */
-    /* TODO
-      max_norm_error_int = abs( spline1d_integrate( spline ) )
-
-      !*** array deallocation ***
-      call bspline1d__free(bspline)
-      call spline1d_del(spline)
-      call spline1d_interpolator__del(spline_interpolator)
-      */
+    max_norm_error_int = spline.integrate();
 }
 
 
+const char bc_to_char(BoundCond const bc)
+{
+    switch (bc) {
+    case (BoundCond::PERIODIC):
+        return 'P';
+    case (BoundCond::HERMITE):
+        return 'H';
+    case (BoundCond::GREVILLE):
+        return 'G';
+    default:
+        return '?';
+    }
+}
+
+
+//--------------------------------------
+// TESTS
+//--------------------------------------
 TEST(Splines, test)
 {
     //const int ncells;
     //const int bc_xmin, bc_xmax;
 
+
     constexpr double x0 = 0.0;
     constexpr double xN = 1.0;
     constexpr int N = 22;
+    constexpr double tol = 1e-12;
+    constexpr double max_norm_profile = 1.0;
 
-    constexpr double h = (xN - x0) / N;
+    cout << endl;
+    cout << "---------------------------------------------------------------------- " << endl;
+    cout << " TEST: evaluate spline at interpolation points (error should be zero) " << endl;
+    cout << " --------------------------------------------------------------------- " << endl;
+    cout << endl;
+
+    // Print constant parameters
+    cout << " Relative error tolerance for f    : tol      = " << tol << endl;
+    cout << " Number of points in grid: N = " << N << endl;
+    cout << " Number of evaluation points : " << N << endl;
+    cout << endl;
+    cout << " Input:" << endl;
+    cout << "   . bcmin = boundary conditions at x=xmin [H|L] " << endl;
+    cout << "   . bcmax = boundary conditions at x=xmax [H|L] " << endl;
+    cout << endl;
+    cout << " Output:" << endl;
+    cout << "   .  err     = relative max-norm of error on f " << endl;
+    cout << "   .  passed  = 'OK' if all errors <= tol, 'FAIL' otherwise " << endl;
+    cout << endl;
+    cout << " Boundary conditions: " << endl;
+    cout << "   .  P = Periodic " << endl;
+    cout << "   .  N = Neumann " << endl;
+    cout << "   .  H = Hermite " << endl;
+    cout << "   .  L = Hermite-Lagrange " << endl;
+    cout << endl;
+
+    // Print table header
+    cout << "degree  "
+         << "bcmin  "
+         << "bcmax  "
+         << "err  "
+         << "passed" << endl;
+
+    const bool passed = true;
+
+    //TODO : Add missing test conditions NEUMANN and HERMITE_LAGRANGE
+    constexpr BoundCond available_bc[3]
+            = {BoundCond::PERIODIC, BoundCond::HERMITE, BoundCond::GREVILLE};
+
+    double h;
+    double max_norm_error;
+    double max_norm_error_diff;
+    double max_norm_error_int;
+    vector<double> coeffs_data={1., 0.};
+    View1D<double> coeffs(coeffs_data.data(),coeffs_data.size());
+
+    for (int degree = 1; degree < 4; ++degree) {
+    //for (int degree = 1; degree < 9; ++degree) {
+        cout << "degree = " << degree << endl;
+        for (BoundCond bc_xmin : available_bc) {
+            for (BoundCond bc_xmax : available_bc) {
+                if (bc_xmin != bc_xmax
+                    and (bc_xmin == BoundCond::PERIODIC or bc_xmax == BoundCond::PERIODIC)) {
+                    continue;
+                }
+
+                //if (degree != 3
+                //    and (bc_xmin == BoundCond::HERMITE_LAGRANGE
+                //         or bc_xmax == BoundCond::HERMITE_LAGRANGE)) {
+                //    continue;
+                //}
+
+                //if ((degree > 3 or degree == 2)
+                //    and (bc_xmin == BoundCond::NEUMANN or bc_xmax == BoundCond::NEUMANN)) {
+                //    continue;
+                //}
+
+                if (bc_xmin == BoundCond::PERIODIC) {
+                    h = (xN - x0) / N;
+                } else {
+                    h = (xN - x0) / (N - 1);
+                }
+
+                cos_splines_test(
+                        max_norm_error,
+                        max_norm_error_diff,
+                        max_norm_error_int,
+                        degree,
+                        h,
+                        N,
+                        x0,
+                        xN,
+                        bc_xmin,
+                        bc_xmax,
+                        coeffs);
+
+                // Calculate relative error norms from absolute ones
+                max_norm_error = max_norm_error / max_norm_profile;
+                cout << "d=" << degree << " bc_xmin=" << bc_to_char(bc_xmin)
+                     << " bc_xmax=" << bc_to_char(bc_xmax) << " error=" << max_norm_error << endl;
+
+                /*      
+          ! Check tolerances
+          success               = (max_norm_error      <= tol     )
+
+          ! Print test report to terminal on a single line
+          write(*,'(i6)'     , advance='no') degree
+          write(*,'(2a6)'    , advance='no') bc_to_char( bc_xmin ), bc_to_char( bc_xmax )
+          write(*,'(3es10.1)', advance='no') max_norm_error
+          write(*,'(a8)') trim( success_to_string( success ))
+
+          passed = passed .and. success
+*/
+            }
+        }
+    }
 
     // coordinate array initialization
     std::vector<double> eval_pts(N);
-    for (int i = 0; i < eval_pts.size(); i++) {
-        eval_pts[i] = x0 + i * h;
+    for (int ii = 0; ii < eval_pts.size(); ++ii) {
+        eval_pts[ii] = x0 + ii * h;
+        //cout << "eval_pts=" << ii << ": " << eval_pts[ii] << endl;
     }
 
     //View1D<double> myview(eval_pts.data(), eval_pts.size());
