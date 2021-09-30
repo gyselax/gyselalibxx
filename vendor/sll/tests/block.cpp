a3f6de93890465b1d97263dca5a74cdaa8a8f318
Thomas Padioleau <thomas.padioleau@cea.fr>
2021-09-30 14:16:32 +0200

    Format

--- vendor/sll/tests/block.cpp
+++ vendor/sll/tests/block.cpp
@@ -11,8 +11,8 @@
 #include <ddc/UniformMesh>
 #include <ddc/block.hpp>
 
-#include <sll/bsplines_uniform.h>
 #include <sll/bsplines_non_uniform.h>
+#include <sll/bsplines_uniform.h>
 
 #include <gtest/gtest.h>
 
