1419d208ae3a44f6d34cc40e7dcd859337cc3829
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-07-13 15:54:19 +0000

    Resolve "Simplification of interpolator machinery"

--- src/interpolation/i_interpolator.hpp
+++ src/interpolation/i_interpolator.hpp
@@ -21,31 +21,10 @@ public:
 };
 
 template <class DDim>
-class InterpolatorProxy : public IInterpolator<DDim>
-{
-    using CDim = typename DDim::continuous_dimension_type;
-
-    std::unique_ptr<IInterpolator<DDim>> m_impl;
-
-public:
-    InterpolatorProxy(std::unique_ptr<IInterpolator<DDim>>&& impl) : m_impl(std::move(impl)) {}
-
-    ~InterpolatorProxy() override = default;
-
-    ddc::ChunkSpan<double, ddc::DiscreteDomain<DDim>> operator()(
-            ddc::ChunkSpan<double, ddc::DiscreteDomain<DDim>> inout_data,
-            ddc::ChunkSpan<const ddc::Coordinate<CDim>, ddc::DiscreteDomain<DDim>> coordinates)
-            const override
-    {
-        return (*m_impl)(inout_data, coordinates);
-    }
-};
-
-template <class DDim>
 class IPreallocatableInterpolator : public IInterpolator<DDim>
 {
 public:
     ~IPreallocatableInterpolator() override = default;
 
-    virtual InterpolatorProxy<DDim> preallocate() const = 0;
+    virtual std::unique_ptr<IInterpolator<DDim>> preallocate() const = 0;
 };
