a50be080cd1fee2465bcc322fd9158aa65542f71
Yann Munschy <yann.munschy@cea.fr>
2023-11-15 17:06:32 +0100

    [PDI] Allow restarting simulations from a previously saved hdf5 file

---
+++ src/geometryXVx/initialization/README.md
@@ -0,0 +1,3 @@
+# Initializatin methods
+
+Initialization methods define the value of the distribution function at the start of the simulation. For instance, these methods allows for initializing the distribution function as a perturbed maxwellian, or to read the values from a previous simulation.  
--- src/geometryXVx/time_integration/predcorr.hpp
+++ src/geometryXVx/time_integration/predcorr.hpp
@@ -9,6 +9,16 @@
 class IPoissonSolver;
 class IBoltzmannSolver;
 
+/**
+ * @brief A class that solves a Boltzmann-Poisson system of equations using a predictor-corrector scheme.
+ *
+ * A class that solves a Boltzmann-Poisson system with
+ * a predictor corrector scheme. This scheme consists in 
+ * estimating the electric potential after a time interval 
+ * of a half-timestep. This potential is then used to compute
+ * the value of the distribution function at time t+dt, where 
+ * dt is the timestep.
+ */
 class PredCorr : public ITimeSolver
 {
 private:
@@ -17,9 +27,25 @@ private:
     IPoissonSolver const& m_poisson_solver;
 
 public:
+    /**
+     * @brief Creates an instance of the predictor-corrector class.
+     * @param[in] boltzmann_solver A solver for a Boltzmann equation.
+     * @param[in] poisson_solver A solver for a Poisson equation.
+     */
     PredCorr(IBoltzmannSolver const& boltzmann_solver, IPoissonSolver const& poisson_solver);
 
     ~PredCorr() override = default;
 
-    DSpanSpXVx operator()(DSpanSpXVx allfdistribu, double dt, int steps = 1) const override;
+    /**
+     * @brief Solves the Boltzmann-Poisson system.
+     * @param[in, out] allfdistribu On input : the initial value of the distribution function.
+     *                              On output : the value of the distribution function after solving 
+     *                              the Boltzmann-Poisson system a given number of iterations.
+     * @param[in] time_start The physical time at the start of the simulation.
+     * @param[in] dt The timestep.
+     * @param[in] steps The number of iterations to be performed by the predictor-corrector.
+     * @return The distribution function after solving the system.
+     */
+    DSpanSpXVx operator()(DSpanSpXVx allfdistribu, double time_start, double dt, int steps = 1)
+            const override;
 };
