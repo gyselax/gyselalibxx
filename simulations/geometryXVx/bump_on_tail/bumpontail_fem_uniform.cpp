a1085718cf0f1216f3d54de663cb5988338e0a37
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2024-01-12 11:09:26 +0000

    Remove costly use of SplineChargeDensityCalculator

--- simulations/geometryXVx/bump_on_tail/bumpontail_fem_uniform.cpp
+++ simulations/geometryXVx/bump_on_tail/bumpontail_fem_uniform.cpp
@@ -25,16 +25,18 @@
 #include "femnonperiodicpoissonsolver.hpp"
 #endif
 
+#include "chargedensitycalculator.hpp"
 #include "geometry.hpp"
+#include "neumann_spline_quadrature.hpp"
 #include "paraconfpp.hpp"
 #include "params.yaml.hpp"
 #include "pdi_out.yml.hpp"
 #include "predcorr.hpp"
+#include "quadrature.hpp"
 #include "restartinitialization.hpp"
 #include "singlemodeperturbinitialization.hpp"
 #include "species_info.hpp"
 #include "spline_interpolator.hpp"
-#include "splinechargedensitycalculator.hpp"
 #include "splitvlasovsolver.hpp"
 
 using std::cerr;
@@ -232,7 +234,9 @@ int main(int argc, char** argv)
 #else
     using FemPoissonSolverX = FemNonPeriodicPoissonSolver;
 #endif
-    SplineChargeDensityCalculator const rhs(builder_vx, spline_vx_evaluator);
+    DFieldVx const quadrature_coeffs = neumann_spline_quadrature_coefficients(gridvx, builder_vx);
+    Quadrature<IDimVx> const integrate_v(quadrature_coeffs);
+    ChargeDensityCalculator rhs(integrate_v);
     FemPoissonSolverX const poisson(builder_x, spline_x_evaluator, rhs);
 
     PredCorr const predcorr(vlasov, poisson);
--- tests/geometryXVx/fftpoissonsolver.cpp
+++ tests/geometryXVx/fftpoissonsolver.cpp
@@ -13,10 +13,12 @@
 #include <paraconf.h>
 #include <pdi.h>
 
+#include "chargedensitycalculator.hpp"
 #include "fftpoissonsolver.hpp"
 #include "geometry.hpp"
+#include "neumann_spline_quadrature.hpp"
+#include "quadrature.hpp"
 #include "species_info.hpp"
-#include "splinechargedensitycalculator.hpp"
 
 TEST(FftPoissonSolver, CosineSource)
 {
@@ -82,7 +84,9 @@ TEST(FftPoissonSolver, CosineSource)
             std::move(init_perturb_amplitude),
             std::move(init_perturb_mode));
 
-    SplineChargeDensityCalculator rhs(builder_vx, spline_vx_evaluator);
+    DFieldVx const quadrature_coeffs = neumann_spline_quadrature_coefficients(gridvx, builder_vx);
+    Quadrature<IDimVx> const integrate_v(quadrature_coeffs);
+    ChargeDensityCalculator rhs(integrate_v);
     FftPoissonSolver poisson(builder_x, spline_x_evaluator, rhs);
     //FftPoissonSolverSplineless poisson;
     DFieldX electrostatic_potential(gridx);
