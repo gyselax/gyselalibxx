b79593c9347ae0ad6f3c19e1bd17143c088ae45f
GitHub CI Documentation builder <username@users.noreply.github.com>
2024-01-22 11:13:05 +0000

    Update docs

--- classFftPoissonSolver.js
+++ classFftPoissonSolver.js
@@ -1,8 +1,8 @@
 var classFftPoissonSolver =
 [
-    [ "FftPoissonSolver", "classFftPoissonSolver.html#ab6bf61df3fc809cd712aebf13ee7c397", null ],
+    [ "FftPoissonSolver", "classFftPoissonSolver.html#af21fa1248a4a7b73e8e8caf1f339f4fd", null ],
     [ "~FftPoissonSolver", "classFftPoissonSolver.html#ab3902921947ad1bece18b570ead8cfa5", null ],
-    [ "FftPoissonSolver", "classFftPoissonSolver.html#a2a0f63ce7dc6d5f579f8aa4449c476cf", null ],
+    [ "FftPoissonSolver", "classFftPoissonSolver.html#af21fa1248a4a7b73e8e8caf1f339f4fd", null ],
     [ "~FftPoissonSolver", "classFftPoissonSolver.html#ab3902921947ad1bece18b570ead8cfa5", null ],
     [ "operator()", "classFftPoissonSolver.html#a0826f648927ced27cc64ed3991e0d198", null ],
     [ "operator()", "classFftPoissonSolver.html#a39d2b32d21f00f96f2c9f1625211eb98", null ]
