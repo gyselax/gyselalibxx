a3be632c27742dea183bd20b59484f0599b87d39
Emily Bourne <emily.bourne@epfl.ch>
2024-02-16 13:50:33 +0000

    Allow to access attributes of SpeciesInformation on the device

--- src/geometryXVx/rhs/krook_source_constant.cpp
+++ src/geometryXVx/rhs/krook_source_constant.cpp
@@ -24,23 +24,25 @@ KrookSourceConstant::KrookSourceConstant(
     , m_density(density)
     , m_temperature(temperature)
     , m_ftarget(gridvx)
+    , m_mask(gridx)
 {
     // mask that defines the region where the operator is active
+    DFieldX mask_host(gridx);
     switch (m_type) {
     case RhsType::Source:
         // the mask equals one in the interval [x_left, x_right]
-        m_mask = mask_tanh(gridx, m_extent, m_stiffness, MaskType::Normal, false);
+        mask_host = mask_tanh(gridx, m_extent, m_stiffness, MaskType::Normal, false);
         break;
     case RhsType::Sink:
         // the mask equals zero in the center of the plasma
-        m_mask = mask_tanh(gridx, m_extent, m_stiffness, MaskType::Inverted, false);
+        mask_host = mask_tanh(gridx, m_extent, m_stiffness, MaskType::Inverted, false);
         break;
     }
+    ddc::deepcopy(m_mask.span_view(), mask_host);
+
     // target distribution function
-    device_t<DFieldVx> ftarget_device(gridvx);
-    MaxwellianEquilibrium::compute_maxwellian(ftarget_device, m_density, m_temperature, 0.);
-    auto ftarget = ddc::create_mirror_view_and_copy(ftarget_device.span_view());
-    ddc::deepcopy(m_ftarget, ftarget);
+    MaxwellianEquilibrium::compute_maxwellian(m_ftarget.span_view(), m_density, m_temperature, 0.);
+    auto ftarget_host = ddc::create_mirror_view_and_copy(m_ftarget.span_view());
 
     switch (m_type) {
     case RhsType::Source:
@@ -49,8 +51,8 @@ KrookSourceConstant::KrookSourceConstant(
         ddc::expose_to_pdi("krook_source_constant_amplitude", m_amplitude);
         ddc::expose_to_pdi("krook_source_constant_density", m_density);
         ddc::expose_to_pdi("krook_source_constant_temperature", m_temperature);
-        ddc::expose_to_pdi("krook_source_constant_ftarget", m_ftarget);
-        ddc::expose_to_pdi("krook_source_constant_mask", m_mask);
+        ddc::expose_to_pdi("krook_source_constant_ftarget", ftarget_host);
+        ddc::expose_to_pdi("krook_source_constant_mask", mask_host);
         break;
     case RhsType::Sink:
         ddc::expose_to_pdi("krook_sink_constant_extent", m_extent);
@@ -58,8 +60,8 @@ KrookSourceConstant::KrookSourceConstant(
         ddc::expose_to_pdi("krook_sink_constant_amplitude", m_amplitude);
         ddc::expose_to_pdi("krook_sink_constant_density", m_density);
         ddc::expose_to_pdi("krook_sink_constant_temperature", m_temperature);
-        ddc::expose_to_pdi("krook_sink_constant_ftarget", m_ftarget);
-        ddc::expose_to_pdi("krook_sink_constant_mask", m_mask);
+        ddc::expose_to_pdi("krook_sink_constant_ftarget", ftarget_host);
+        ddc::expose_to_pdi("krook_sink_constant_mask", mask_host);
         break;
     }
 }
@@ -69,14 +71,9 @@ device_t<DSpanSpXVx> KrookSourceConstant::operator()(
         double const dt) const
 {
     Kokkos::Profiling::pushRegion("KrookSource");
-    auto ftarget_device_alloc = ddc::
-            create_mirror_view_and_copy(Kokkos::DefaultExecutionSpace(), m_ftarget.span_view());
-    auto ftarget_device = ftarget_device_alloc.span_view();
 
-    auto mask_device_alloc
-            = ddc::create_mirror_view_and_copy(Kokkos::DefaultExecutionSpace(), m_mask.span_view());
-    auto mask_device = mask_device_alloc.span_view();
-
+    auto ftarget = m_ftarget.span_view();
+    auto mask = m_mask.span_view();
     auto const& amplitude = m_amplitude;
 
     ddc::for_each(
@@ -84,11 +81,10 @@ device_t<DSpanSpXVx> KrookSourceConstant::operator()(
             allfdistribu.domain(),
             KOKKOS_LAMBDA(IndexSpXVx const ispxvx) {
                 allfdistribu(ispxvx)
-                        = ftarget_device(ddc::select<IDimVx>(ispxvx))
-                          + (allfdistribu(ispxvx) - ftarget_device(ddc::select<IDimVx>(ispxvx)))
+                        = ftarget(ddc::select<IDimVx>(ispxvx))
+                          + (allfdistribu(ispxvx) - ftarget(ddc::select<IDimVx>(ispxvx)))
                                     * Kokkos::exp(
-                                            -amplitude * mask_device(ddc::select<IDimX>(ispxvx))
-                                            * dt);
+                                            -amplitude * mask(ddc::select<IDimX>(ispxvx)) * dt);
             });
 
     Kokkos::Profiling::popRegion();
