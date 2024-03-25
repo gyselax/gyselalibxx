2a48c91ce38aa934f1a4c556edc10e4a44da0d46
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-03-25 11:05:55 +0000

    Modify default DDC compilation parameters
    By default don't build DDC benchmarks, documentation, examples or tests. Build tests when running on GPU as DDC doesn't test on GPU
    
    See merge request gysela-developpers/gyselalibxx!415
    
    --------------------------------------------

--- src/geometryRTheta/time_solver/bsl_predcorr_second_order_implicit.hpp
+++ src/geometryRTheta/time_solver/bsl_predcorr_second_order_implicit.hpp
@@ -69,10 +69,6 @@ template <class Mapping, class AdvectionDomain>
 class BslImplicitPredCorrRP : public ITimeSolverRP
 {
 private:
-    using Evaluator = SplineEvaluator2D<BSplinesR, BSplinesP>;
-    using Builder = SplineBuilder2D<SplineRBuilder, SplinePBuilder>;
-    using Interpolator = SplineInterpolatorRP;
-
     using EulerMethod = Euler<FieldRP<CoordRP>, VectorDFieldRP<RDimX, RDimY>>;
 
     AdvectionDomain const& m_advection_domain;
@@ -87,8 +83,8 @@ private:
 
     PolarSplineFEMPoissonSolver const& m_poisson_solver;
 
-    Builder const& m_builder;
-    Evaluator const& m_evaluator;
+    SplineRPBuilder const& m_builder;
+    SplineRPEvaluator const& m_evaluator;
 
 
 
