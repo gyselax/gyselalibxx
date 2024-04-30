88a839b92503b4f20629890337a04df0aec95ce4
Abdelhadi KARA <abdelhadi.kara@cea.fr>
2024-04-30 15:53:27 +0200

    Integrate batched  solver in CollisionIntra Solver
    Create a new `MatrixBatchTridiag` in SLL with associated tests. 
    
    This class can be used for solving tridiagonal systems both on CPU and GPU.
    
    It is used for the `CollisionIntra` operator.
    
    Closes #204
    
    See merge request gysela-developpers/gyselalibxx!463
    
    --------------------------------------------
    
    Co-authored-by: KARA Abdelhadi 265943 <AK265943@persee.partenaires.cea.fr>

---
+++ vendor/sll/include/sll/matrix_batch_tridiag.hpp
@@ -0,0 +1,184 @@
+// Copyright (C) The DDC development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <sll/matrix_batch.hpp>
+
+#include <Kokkos_Core.hpp>
+
+/**
+ * @brief A structure for solving a set of independant tridiagonal systems using a direct method.
+ * The parallelism operates on the whole collection by dispatching to threads.
+ * Each problem is treated sequentially, by the tridiagonal matrix algorithm (TDMA).
+ * This solver is stable for tridiagonal matrices which satisfy one of the following conditions:
+ * - Diagonally Dominant.
+ * - Symmetric positive-definite.
+ * Diagonally Dominant property is fully checked.
+ * Only symmetry property is checked, positivity-definiteness is not.
+ * @tparam ExecSpace The execution space related to Kokkos.
+ */
+template <class ExecSpace>
+class MatrixBatchTridiag : public MatrixBatch<ExecSpace>
+{
+private:
+    /**
+     * @brief Alias for 2D double Kokkos views, LayoutRight is specified.
+    */
+    using DKokkosView2D
+            = Kokkos::View<double**, Kokkos::LayoutRight, typename ExecSpace::memory_space>;
+    using MatrixBatch<ExecSpace>::get_size;
+    using MatrixBatch<ExecSpace>::get_batch_size;
+    DKokkosView2D m_subdiag;
+    DKokkosView2D m_diag;
+    DKokkosView2D m_uppdiag;
+
+public:
+    /**
+     * @brief Creates an instance of the MatrixBatchTridiag class.
+     * First dimension is the batch, second one refers to matrix entries indexed by line.
+     * The entries aa,bb,cc are 2D Kokkos views and have the same dimensions.
+     * LayoutRight: means that the "last" dimension is the contiguous one.
+     * aa(batch_idx,0) and cc(batch_idx,mat_size-1) are not used for any values of batch_idx.
+     *
+     * @param[in] batch_size The size of the set of linear problems. 
+     * @param[in] mat_size The common size of each individual matrix .
+     * @param[in] aa 2d Kokkos View which stores subdiagonal components for all matrices.
+     * @param[in] bb 2d Kokkos View which stores diagonal components for all matrices.
+     * @param[in] cc 2d Kokkos View which stores upper diagonal components for all matrices.
+     */
+
+    MatrixBatchTridiag(
+            const int batch_size,
+            const int mat_size,
+            DKokkosView2D const aa,
+            DKokkosView2D const bb,
+            DKokkosView2D const cc)
+
+        : MatrixBatch<ExecSpace>(batch_size, mat_size)
+        , m_subdiag(aa)
+        , m_diag(bb)
+        , m_uppdiag(cc)
+    {
+    }
+
+    /**
+     * @brief Check if the matrices are in the stability area of the solver.
+     *
+     * It checks if each matrix of the batch has one of the following structures:
+     * - Diagonally Dominant.
+     * - Symmetric.
+     *  If assertion fails, there is at least one of the matrices which does not verify these conditions. 
+     *  Positivity-definiteness is not checked, it is needed to fully achieve TDMA algorithm requirement: symmetric positive definite.
+     *
+     * @return A boolean which indicates if the stability condition may be verified.
+     */
+    bool check_stability() const
+    {
+        assert(get_batch_size() == m_subdiag.extent(0));
+        assert(get_size() == m_subdiag.extent(1));
+        assert(get_batch_size() == m_diag.extent(0));
+        assert(get_size() == m_diag.extent(1));
+        assert(get_batch_size() == m_uppdiag.extent(0));
+        assert(get_size() == m_uppdiag.extent(1));
+
+        int const tmp_batch_size = get_batch_size();
+        int const tmp_mat_size = get_size();
+        bool is_diagdom = false;
+        bool is_symmetric = false;
+
+        DKokkosView2D subdiag_proxy = m_subdiag;
+        DKokkosView2D diag_proxy = m_diag;
+        DKokkosView2D uppdiag_proxy = m_uppdiag;
+
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>> batch_policy({0, 0}, {tmp_batch_size, tmp_mat_size});
+        Kokkos::parallel_reduce(
+                "DiagDominant",
+                batch_policy,
+                KOKKOS_LAMBDA(int batch_idx, int k, bool& check_diag_dom) {
+                    check_diag_dom
+                            = (Kokkos::abs(subdiag_proxy(batch_idx, k))
+                                       + Kokkos::abs(uppdiag_proxy(batch_idx, k))
+                               <= Kokkos::abs(diag_proxy(batch_idx, k)));
+                },
+                Kokkos::LAnd<bool>(is_diagdom));
+        Kokkos::parallel_reduce(
+                "Symmetric",
+                batch_policy,
+                KOKKOS_LAMBDA(int batch_idx, int k, bool& check_sym) {
+                    check_sym
+                            = (Kokkos::abs(
+                                       subdiag_proxy(batch_idx, k) - uppdiag_proxy(batch_idx, k))
+                               < 1e-16);
+                },
+                Kokkos::LAnd<bool>(is_symmetric));
+
+        return (is_diagdom || is_symmetric);
+    }
+
+    /**
+     * @brief Solves the collection of problems to find x_i such that: A_i x_i=rhs_i  i=0,batch_size.
+     * @param[inout] rhs_view 2d Kokkos View which stores right hand side, it is also
+     *               used to store the results.
+     * @return  The computation result, stored in a 2d Kokkos View.
+     */
+    DKokkosView2D solve_inplace(DKokkosView2D const rhs_view) const
+    {
+        assert(get_batch_size() == rhs_view.extent(0));
+        assert(get_size() == rhs_view.extent(1));
+
+        int const tmp_batch_size = m_subdiag.extent(0);
+        int const tmp_mat_size = m_subdiag.extent(1);
+
+        Kokkos::View<
+                double**,
+                Kokkos::LayoutRight,
+                Kokkos::DefaultExecutionSpace::memory_space> const
+                cprim("cprim", get_batch_size(), get_size());
+        Kokkos::View<
+                double**,
+                Kokkos::LayoutRight,
+                Kokkos::DefaultExecutionSpace::memory_space> const
+                dprim("dprim", get_batch_size(), get_size());
+
+        DKokkosView2D subdiag_proxy = m_subdiag;
+        DKokkosView2D diag_proxy = m_diag;
+        DKokkosView2D uppdiag_proxy = m_uppdiag;
+
+        Kokkos::parallel_for(
+                "Tridiagonal solver",
+                Kokkos::RangePolicy<ExecSpace>(0, tmp_batch_size),
+                KOKKOS_LAMBDA(const int batch_idx) {
+                    //ForwardStep
+                    cprim(batch_idx, 0) = uppdiag_proxy(batch_idx, 0) / diag_proxy(batch_idx, 0);
+                    dprim(batch_idx, 0) = rhs_view(batch_idx, 0) / diag_proxy(batch_idx, 0);
+                    for (int i = 1; i < tmp_mat_size; i++) {
+                        cprim(batch_idx, i)
+                                = uppdiag_proxy(batch_idx, i)
+                                  / (diag_proxy(batch_idx, i)
+                                     - subdiag_proxy(batch_idx, i) * cprim(batch_idx, i - 1));
+                        dprim(batch_idx, i)
+                                = (rhs_view(batch_idx, i)
+                                   - subdiag_proxy(batch_idx, i) * dprim(batch_idx, i - 1))
+                                  / (diag_proxy(batch_idx, i)
+                                     - subdiag_proxy(batch_idx, i) * cprim(batch_idx, i - 1));
+                    }
+                    //BackwardStep
+                    rhs_view(batch_idx, tmp_mat_size - 1) = dprim(batch_idx, tmp_mat_size - 1);
+                    for (int i = tmp_mat_size - 2; i >= 0; i--) {
+                        rhs_view(batch_idx, i) = dprim(batch_idx, i)
+                                                 - cprim(batch_idx, i) * rhs_view(batch_idx, i + 1);
+                    }
+                });
+        return rhs_view;
+    }
+    /**
+     * @brief function used to execute specific implementation details.
+     * It calls check_stability function to verify if the matrices data is in range of validity of the solver.
+    */
+    void factorize() final
+    {
+        assert(check_stability());
+    }
+};
---
+++ vendor/sll/tests/matrix_batch_tridiag.cpp
@@ -0,0 +1,122 @@
+#include <gtest/gtest.h>
+
+#include "sll/matrix_batch_tridiag.hpp"
+
+using View2d = Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultExecutionSpace>;
+
+void solve_batched_tridiag_system(
+        int const batch_size,
+        int const mat_size,
+        double const sub_diag,
+        double const diag,
+        double const up_diag,
+        Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+                Rhs_view_host)
+{
+    View2d A_view("A", batch_size, mat_size);
+    View2d B_view("B", batch_size, mat_size);
+    View2d C_view("C", batch_size, mat_size);
+    View2d Rhs_view("R", batch_size, mat_size);
+    Kokkos::deep_copy(A_view, sub_diag);
+    Kokkos::deep_copy(B_view, diag);
+    Kokkos::deep_copy(C_view, up_diag);
+    Kokkos::deep_copy(Rhs_view, Rhs_view_host);
+    MatrixBatchTridiag<Kokkos::DefaultExecutionSpace>
+            matrix(batch_size, mat_size, A_view, B_view, C_view);
+    matrix.factorize();
+    matrix.solve_inplace(Rhs_view);
+    Kokkos::deep_copy(Rhs_view_host, Rhs_view);
+}
+
+
+TEST(MatrixBatchTridiag, Symmetric)
+{
+    int const batch_size = 2;
+    int const mat_size = 4;
+
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            Res_view_host("R_host", batch_size, mat_size);
+    Kokkos::deep_copy(Res_view_host, 1.);
+    //Cond number for the matrix 2.72
+    solve_batched_tridiag_system(batch_size, mat_size, 0.5, 2, 0.5, Res_view_host);
+
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 0), 8. / 19.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 1), 6. / 19.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 2), 6. / 19.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 3), 8. / 19.);
+}
+
+TEST(MatrixBatchTridiag, DominantDiag)
+{
+    int const batch_size = 1;
+    int const mat_size = 4;
+
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            Res_view_host("R_host", batch_size, mat_size);
+    Kokkos::deep_copy(Res_view_host, 1.);
+    //Cond number for the matrix 2.56
+    solve_batched_tridiag_system(batch_size, mat_size, 0.5, 1.1, -0.25, Res_view_host);
+
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 0), 1.0468844955326548);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 1), 0.6062917803436817);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 2), 0.7614528245775094);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 3), 0.5629759888284048);
+}
+
+TEST(MatrixBatchTridiag, NotValidMatrix)
+{
+    int const batch_size = 2;
+    int const mat_size = 4;
+
+    View2d A_view("A", batch_size, mat_size);
+    View2d B_view("B", batch_size, mat_size);
+    View2d C_view("C", batch_size, mat_size);
+    //neither positive definite symmetric nor diagonal dominant
+    Kokkos::deep_copy(A_view, 0.93);
+    Kokkos::deep_copy(B_view, -0.367);
+    Kokkos::deep_copy(C_view, 1.42);
+    MatrixBatchTridiag<Kokkos::DefaultExecutionSpace>
+            matrix(batch_size, mat_size, A_view, B_view, C_view);
+    EXPECT_FALSE(matrix.check_stability());
+}
+
+TEST(MatrixBatchTridiag, GeneralValidMatrix)
+{
+    int const batch_size = 1;
+    int const mat_size = 4;
+
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            Res_view_host("R_host", batch_size, mat_size);
+    double subdiag[] = {0., 2.0, 0.9, -0.8};
+    double diag[] = {2.7, 5.0, 2.0, 3.3};
+    double uppdiag[] = {1.8, 2.4, -0.7, 0.};
+    double r[] = {7.8, 11.5, 42., 0.5};
+
+
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            A_view_host(subdiag, batch_size, mat_size);
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            B_view_host(diag, batch_size, mat_size);
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            C_view_host(uppdiag, batch_size, mat_size);
+    Kokkos::View<double**, Kokkos::LayoutRight, Kokkos::DefaultHostExecutionSpace>
+            Rhs_view_host(r, batch_size, mat_size);
+
+    View2d A_view("A", batch_size, mat_size);
+    View2d B_view("B", batch_size, mat_size);
+    View2d C_view("C", batch_size, mat_size);
+    View2d Rhs_view("R", batch_size, mat_size);
+    Kokkos::deep_copy(A_view, A_view_host);
+    Kokkos::deep_copy(B_view, B_view_host);
+    Kokkos::deep_copy(C_view, C_view_host);
+    Kokkos::deep_copy(Rhs_view, Rhs_view_host);
+    MatrixBatchTridiag<Kokkos::DefaultExecutionSpace>
+            matrix(batch_size, mat_size, A_view, B_view, C_view);
+    matrix.solve_inplace(Rhs_view);
+    Kokkos::deep_copy(Res_view_host, Rhs_view);
+
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 0), 272999. / 16896.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 1), -672565. / 33792.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 2), 134315. / 4096.);
+    ASSERT_DOUBLE_EQ(Res_view_host(0, 3), 45625. / 5632.);
+}
