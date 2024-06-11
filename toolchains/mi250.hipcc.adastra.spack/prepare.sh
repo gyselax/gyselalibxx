8e2bd1c8ff20810d445da5abb4c3d420bed5cedf
Etienne Malaboeuf <malaboeuf@cines.fr>
2024-06-11 16:00:17 +0200

    Resolve "Install PDI missing plugging in Adastra spack environnent"
    Closes #245
    
    See merge request gysela-developpers/gyselalibxx!507
    
    --------------------------------------------
    
    Co-authored-by: Etienne Malaboeuf <eti.malaboeuf@gmail.com>

--- toolchains/mi250.hipcc.adastra.spack/prepare.sh
+++ toolchains/mi250.hipcc.adastra.spack/prepare.sh
@@ -12,7 +12,7 @@ export SPACK_USER_PREFIX="${SHAREDWORKDIR}/spack-install-MI250"
 
 # FIXME: Currently in development.
 module load develop
-# FIXME: This loads unneeded modules. It should not interfere with out build.
+# FIXME: This loads unneeded modules. It should not interfere with our build.
 module load spack-MI250-3.1.0
 
 # Inject PDI recipes into our local repo.
@@ -29,7 +29,7 @@ cp -rf -- spack.spack/var/spack/repos/builtin/packages/ginkgo "${SPACK_USER_PREF
 echo "Preparing the Spack environment..."
 
 # # Either do not use the source mirrors or use --no-check-signature. There is an
-# # ongoing issue a CiINES to fix the issue.
+# # ongoing issue a CINES to fix the issue.
 # echo "# Disabled" >"${SPACK_USER_PREFIX}/config_user_spack/mirrors.yaml"
 
 # NOTE: We do two passes because Spack is bugged and even though we specified
@@ -42,6 +42,8 @@ for ((i = 0; i < 2; ++i)); do
         paraconf@1.0.0%gcc@12.1~fortran~ipo~shared~tests build_system=cmake build_type=Release generator==ninja arch=linux-rhel8-zen3 \
         pdi@1.6.0%gcc@12.1~benchs~docs+fortran~ipo+python~tests build_system=cmake build_type=Release generator==ninja arch=linux-rhel8-zen3 \
         pdiplugin-decl-hdf5@1.6.0%gcc@12.1~benchs~fortran~ipo~mpi~tests build_system=cmake build_type=Release generator==ninja arch=linux-rhel8-zen3 \
+        pdiplugin-set-value@1.6.0%gcc@12.1~ipo~tests build_system=cmake build_type=Release generator=ninja arch=linux-rhel8-zen3 \
+        pdiplugin-trace@1.6.0%gcc@12.1~ipo~tests build_system=cmake build_type=Release generator=ninja arch=linux-rhel8-zen3 \
         ginkgo@1.7.0%gcc@12.1~cuda~develtools~full_optimizations~hwloc~ipo~mpi+openmp+rocm~sde~shared~sycl amdgpu_target=gfx90a build_system=cmake build_type=Release generator==ninja arch=linux-rhel8-zen3 \
         eigen@3.4.0%gcc@12.1~ipo build_system=cmake build_type=Release generator==ninja arch=linux-rhel8-zen3 \
         cmake@3.27.7%gcc@12.1~doc+ncurses+ownlibs build_system=generic build_type=Release arch=linux-rhel8-zen3 \
