66c53c4b584554b97736c3be834536cf524a8f88
Pauline Vidal <pauline.vidal@ipp.mpg.de>
2024-07-08 18:19:08 +0200

    Resolve "Add a Guiding Center simulation"
    Closes #219 Add a guiding center in XY geometry for further tests on multipatch geometry. Any new operator to implement, only the simulation. Implementation on CPU in this branch. Maybe an update to GPU or it will be made in another branch.
    
    **New comment:**
    
    * Add a guiding center simulation on XY geometry with Kelvin-Helmholtz test case.
    * KelvinHelmholtzInstabilityInitialisation, PredCorrRK2XY operators implemented.
    * Add a geometryXY folder in source.
    * Simulation runs on CPU and GPU.
    * Add some python scripts for the diagnosis in a geometryXY/ folder.
    
    See merge request gysela-developpers/gyselalibxx!467
    
    --------------------------------------------
    
    Co-authored-by: Pauline Vidal <pauline.vidal@ipp.mpg.de>

---
+++ src/geometryXY/initialization/initialization_Kelvin_Helmholtz.hpp
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <geometry.hpp>
+
+
+/**
+ * @brief Initialize the allfdistribu function. 
+ *  
+ * Set the allfdistribu at 
+ * @f$ f_{eq}(x, y) = \sin(y) @f$
+ * 
+ * and 
+ * @f$ f_0(x, y) = f_{eq}(x, y) + \varepsilon \cos(kx) @f$, 
+ * 
+ * with @f$ \varepsilon @f$ an amplitude of perturbation and 
+ * @f$ k @f$ mode equal to @f$ 2\pi @f$ divided by the length of the 
+ * domain on @f$ x @f$. 
+ */
+class KelvinHelmholtzInstabilityInitialization
+{
+    double const m_epsilon;
+    double const m_mode_k;
+
+public:
+    /**
+     * @brief Instantiate the initializer. 
+     * 
+     * @param epsilon @f$ \varepsilon @f$, the amplitude of perturbation. 
+     * @param mode_k @f$ k @f$, the perturbation mode. 
+     */
+    KelvinHelmholtzInstabilityInitialization(double const epsilon, double const mode_k)
+        : m_epsilon(epsilon)
+        , m_mode_k(mode_k) {};
+
+    ~KelvinHelmholtzInstabilityInitialization() = default;
+
+    /**
+     * @brief Initialise @f$ f_{eq}@f$ and @f$ f @f$.
+     * 
+     * @param allfdistribu ChunkSpan refering to the @f$ f @f$ function.
+     * @param allfdistribu_equilibrium ChunkSpan refering to the @f$ f_{eq} @f$ function.
+     */
+    void operator()(DSpanXY allfdistribu, DSpanXY allfdistribu_equilibrium)
+    {
+        IDomainXY const domain = allfdistribu.domain();
+
+        ddc::parallel_for_each(
+                Kokkos::DefaultExecutionSpace(),
+                domain,
+                KOKKOS_LAMBDA(IndexXY const i_xy) {
+                    CoordXY const coord_xy(ddc::coordinate(i_xy));
+                    double const x = CoordX(coord_xy);
+                    double const y = CoordY(coord_xy);
+                    allfdistribu_equilibrium(i_xy) = Kokkos::sin(y);
+                    allfdistribu(i_xy) = allfdistribu_equilibrium(i_xy)
+                                         + m_epsilon * Kokkos::cos(m_mode_k * x);
+                });
+    };
+};
