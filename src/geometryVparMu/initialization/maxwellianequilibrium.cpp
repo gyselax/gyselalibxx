e214c1a1f0cac35cb7420734f462e4cc87b7f01f
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2024-07-17 18:20:09 +0200

    Create (vpar,mu) collision simulation
    Closes #248
    
    See merge request gysela-developpers/gyselalibxx!520
    
    --------------------------------------------
    
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>

---
+++ src/geometryVparMu/initialization/maxwellianequilibrium.cpp
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+
+#include <cmath>
+
+#include <ddc/ddc.hpp>
+
+#include "maxwellianequilibrium.hpp"
+
+MaxwellianEquilibrium::MaxwellianEquilibrium(
+        host_t<DFieldSp> mass,
+        host_t<DFieldSp> density_eq,
+        host_t<DFieldSp> temperature_eq,
+        host_t<DFieldSp> mean_velocity_eq,
+        double magnetic_field = 1.0)
+    : m_mass(std::move(mass))
+    , m_density_eq(std::move(density_eq))
+    , m_temperature_eq(std::move(temperature_eq))
+    , m_mean_velocity_eq(std::move(mean_velocity_eq))
+    , m_magnetic_field(magnetic_field)
+{
+}
+
+DSpanSpVparMu MaxwellianEquilibrium::operator()(DSpanSpVparMu const allfequilibrium) const
+{
+    IdxRangeSp const idxrange_sp = allfequilibrium.domain<IDimSp>();
+    IdxRangeVparMu const idxrange_vparmu = allfequilibrium.domain<GridVpar, GridMu>();
+
+    // Initialization of the maxwellian
+    DFieldVparMu maxwellian_alloc(idxrange_vparmu);
+    DSpanVparMu maxwellian = maxwellian_alloc.span_view();
+    double const magnetic_field(1.0);
+    ddc::for_each(idxrange_sp, [&](IdxSp const isp) {
+        compute_maxwellian(
+                maxwellian,
+                m_mass(isp),
+                m_density_eq(isp),
+                m_temperature_eq(isp),
+                m_mean_velocity_eq(isp),
+                magnetic_field);
+
+        ddc::parallel_deepcopy(allfequilibrium[isp], maxwellian);
+    });
+    return allfequilibrium;
+}
+
+MaxwellianEquilibrium MaxwellianEquilibrium::init_from_input(
+        IDomainSp dom_kinsp,
+        PC_tree_t const& yaml_input_file)
+{
+    host_t<DFieldSp> mass(dom_kinsp);
+    host_t<DFieldSp> density_eq(dom_kinsp);
+    host_t<DFieldSp> temperature_eq(dom_kinsp);
+    host_t<DFieldSp> mean_velocity_eq(dom_kinsp);
+    double const magnetic_field = 1.0;
+
+    for (IndexSp const isp : dom_kinsp) {
+        PC_tree_t const conf_isp = PCpp_get(yaml_input_file, ".SpeciesInfo[%d]", isp.uid());
+
+        mass(isp) = PCpp_double(conf_isp, ".mass");
+        density_eq(isp) = PCpp_double(conf_isp, ".density_eq");
+        temperature_eq(isp) = PCpp_double(conf_isp, ".temperature_eq");
+        mean_velocity_eq(isp) = PCpp_double(conf_isp, ".mean_velocity_eq");
+    }
+
+    return MaxwellianEquilibrium(
+            std::move(mass),
+            std::move(density_eq),
+            std::move(temperature_eq),
+            std::move(mean_velocity_eq),
+            magnetic_field);
+}
+
+void MaxwellianEquilibrium::compute_maxwellian(
+        DSpanVparMu const fMaxwellian,
+        double const mass,
+        double const density,
+        double const temperature,
+        double const mean_velocity,
+        double const magnetic_field)
+{
+    double const mass_on_2piT = mass / (2. * M_PI * temperature);
+    double const coeff_maxw = Kokkos::sqrt(mass_on_2piT) * mass_on_2piT;
+    IdxRangeVparMu const idxrange_vparmu = fMaxwellian.domain<GridVpar, GridMu>();
+
+    // Compute fM(vpar,mu) = (2*PI*T)**1.5*n*exp(-energy) with
+    //  - n the density, T the temperature and Upar the mean velocity
+    //  - B the magnetic field and
+    //  - energy = (0.5*(vpar-Upar)**2+mu*B)/T
+    ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
+            idxrange_vparmu,
+            KOKKOS_LAMBDA(IdxVparMu const ivparmu) {
+                double const vpar = ddc::coordinate(ddc::select<GridVpar>(ivparmu));
+                double const mu = ddc::coordinate(ddc::select<GridMu>(ivparmu));
+                double const energy = (0.5 * (vpar - mean_velocity) * (vpar - mean_velocity)
+                                       + mu * magnetic_field)
+                                      / temperature;
+                fMaxwellian(ivparmu) = coeff_maxw * density * Kokkos::exp(-energy);
+            });
+}
