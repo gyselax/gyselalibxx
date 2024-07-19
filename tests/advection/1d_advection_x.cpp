6d92513a3803bd2e4de07f539da9ccc062592c5e
Emily Bourne <emily.bourne@epfl.ch>
2024-07-19 10:13:08 +0200

    Merge branch 'create-scopeguards-after-possible-stdexit' into 'main'
    Create ScopeGuards after possible std::exit
    
    Closes #278
    
    See merge request gysela-developpers/gyselalibxx!583

--- tests/advection/1d_advection_x.cpp
+++ tests/advection/1d_advection_x.cpp
@@ -146,8 +146,7 @@ public:
         /*
             Simulation launched on GPU but error checking on CPU. 
         */
-        auto function_host
-                = ddc::create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), function);
+        auto function_host = ddc::create_mirror_view_and_copy(function);
         double max_relative_error = 0;
         ddc::for_each(interpolation_domain, [&](IndexX const idx) {
             double const relative_error = abs(function_host(idx) - exact_function(idx));
