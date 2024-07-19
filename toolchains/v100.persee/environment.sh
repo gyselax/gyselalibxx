71d023e3c6c45fc8b591b7723da62803765a1cc1
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-07-19 09:14:34 +0200

    Update spack environments on Persee
    Closes #279
    
    See merge request gysela-developpers/gyselalibxx!580
    
    --------------------------------------------

--- toolchains/v100.persee/environment.sh
+++ toolchains/v100.persee/environment.sh
@@ -8,7 +8,7 @@ fi
 . /data/gyselarunner/spack-0.20.0/share/spack/setup-env.sh
 
 spack load gcc@11
-spack env activate gyselalibxx-env-omp-cuda-ginkgo-1_8_0
+spack env activate gyselalibxx-env-omp-cuda
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=8
