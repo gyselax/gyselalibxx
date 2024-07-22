5af6bb5cd7b2cd5cc08b9d7b51a8ef95229c877c
Emily Bourne <emily.bourne@epfl.ch>
2024-07-22 18:22:43 +0200

    Let Quadrature integrate functions
    To be merged after !586
    
    See merge request gysela-developpers/gyselalibxx!587
    
    --------------------------------------------

--- src/geometryXVx/rhs/mask_tanh.cpp
+++ src/geometryXVx/rhs/mask_tanh.cpp
@@ -48,7 +48,8 @@ host_t<DFieldX> mask_tanh(
     if (normalized) {
         host_t<DFieldX> const quadrature_coeffs = trapezoid_quadrature_coefficients(gridx);
         host_t<Quadrature<IDomainX>> const integrate_x(quadrature_coeffs);
-        double const coeff_norm = integrate_x(Kokkos::DefaultHostExecutionSpace(), mask);
+        double const coeff_norm
+                = integrate_x(Kokkos::DefaultHostExecutionSpace(), mask.span_cview());
         ddc::for_each(gridx, [&](IndexX const ix) { mask(ix) = mask(ix) / coeff_norm; });
     }
 
