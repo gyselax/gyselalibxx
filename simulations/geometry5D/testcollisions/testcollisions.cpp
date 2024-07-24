ffc41ad12016b23b28ee331644349a6f70ec6072
KARA Abdelhadi 265943 <abdelhadi.kara@cea.fr>
2024-07-24 16:01:08 +0200

    use quadrature coeff on default execspace

--- simulations/geometry5D/testcollisions/testcollisions.cpp
+++ simulations/geometry5D/testcollisions/testcollisions.cpp
@@ -214,16 +214,12 @@ int main(int argc, char** argv)
     DDomTorCS dom_tor1_tor2(dom_tor2, dom_tor1);
     DFieldTorCS B_norm(dom_tor1_tor2);
     ddc::parallel_fill(B_norm, 1.0);
-    host_t<DFieldVpar> const coeff_intdvpar_host
-            = simpson_quadrature_coefficients_1d(allfdistribu.domain<GridVpar>());
-    host_t<DFieldMu> const coeff_intdmu_host
-            = simpson_quadrature_coefficients_1d(allfdistribu.domain<GridMu>());
-    auto coeff_intdvpar = ddc::create_mirror_view_and_copy(
-            Kokkos::DefaultExecutionSpace(),
-            coeff_intdvpar_host.span_cview());
-    auto coeff_intdmu = ddc::create_mirror_view_and_copy(
-            Kokkos::DefaultExecutionSpace(),
-            coeff_intdmu_host.span_cview());
+
+    DFieldVpar const coeff_intdvpar
+            = simpson_quadrature_coefficients_1d<Kokkos::DefaultExecutionSpace>(
+                    allfdistribu.domain<GridVpar>());
+    DFieldMu const coeff_intdmu = simpson_quadrature_coefficients_1d<Kokkos::DefaultExecutionSpace>(
+            allfdistribu.domain<GridMu>());
 
     CollisionSpVparMu collision_operator(
             dom_sp_tor3D_v2D,
