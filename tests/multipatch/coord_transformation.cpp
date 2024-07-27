88e1dc44f0f33bcf27bd9a73337337105d6040a6
Emily Bourne <emily.bourne@epfl.ch>
2024-07-27 15:57:14 +0200

    Avoid renaming conflicts
    Remove unused types in `src/geometryXVx/geometry/geometry.hpp` whose names conflict with names that will appear in `src/utils/ddc_aliases.hpp`.
    
    Rename objects in `tests/data_types/deriv_field.hpp` with those names to be more explicit
    
    See merge request gysela-developpers/gyselalibxx!608
    
    --------------------------------------------

--- tests/multipatch/coord_transformation.cpp
+++ tests/multipatch/coord_transformation.cpp
@@ -4,61 +4,118 @@
 
 #include <gtest/gtest.h>
 
-#include "2patches_2d_non_periodic_uniform.hpp"
 #include "coord_transformation.hpp"
 #include "edge.hpp"
 #include "interface.hpp"
-#include "patch.hpp"
-
 
 namespace {
+// Continuous dimension of patch 1
+struct RDimX1
+{
+    static bool constexpr PERIODIC = false;
+};
+
+struct RDimY1
+{
+    static bool constexpr PERIODIC = false;
+};
+
+// Continuous dimension of patch 2
+struct RDimX2
+{
+    static bool constexpr PERIODIC = false;
+};
+
+struct RDimY2
+{
+    static bool constexpr PERIODIC = false;
+};
+
+
+using CoordX1 = ddc::Coordinate<RDimX1>;
+using CoordY1 = ddc::Coordinate<RDimY1>;
+using CoordX2 = ddc::Coordinate<RDimX2>;
+using CoordY2 = ddc::Coordinate<RDimY2>;
+
+// Discrete dimensions
+struct IDimX1 : ddc::UniformPointSampling<RDimX1>
+{
+};
+struct IDimY1 : ddc::UniformPointSampling<RDimY1>
+{
+};
+struct IDimX2 : ddc::UniformPointSampling<RDimX2>
+{
+};
+struct IDimY2 : ddc::UniformPointSampling<RDimY2>
+{
+};
+
+using IndexX1 = ddc::DiscreteElement<IDimX1>;
+using IndexY1 = ddc::DiscreteElement<IDimY1>;
+using IndexX2 = ddc::DiscreteElement<IDimX2>;
+using IndexY2 = ddc::DiscreteElement<IDimY2>;
+
+
+using IVectX1 = ddc::DiscreteVector<IDimX1>;
+using IVectY1 = ddc::DiscreteVector<IDimY1>;
+using IVectX2 = ddc::DiscreteVector<IDimX2>;
+using IVectY2 = ddc::DiscreteVector<IDimY2>;
+
+using IDomainX1 = ddc::DiscreteDomain<IDimX1>;
+using IDomainY1 = ddc::DiscreteDomain<IDimY1>;
+using IDomainX2 = ddc::DiscreteDomain<IDimX2>;
+using IDomainY2 = ddc::DiscreteDomain<IDimY2>;
+
+
 class CoordinateTransformationTest : public ::testing::Test
 {
 private:
-    static constexpr Patch1::IdxStep1 x1_size = Patch1::IdxStep1(10);
-    static constexpr Patch1::IdxStep2 y1_size = Patch1::IdxStep2(10);
+    static constexpr IVectX1 x1_size = IVectX1(10);
+    static constexpr IVectY1 y1_size = IVectY1(10);
 
-    static constexpr Patch2::IdxStep1 x2_size = Patch2::IdxStep1(10);
-    static constexpr Patch2::IdxStep2 y2_size = Patch2::IdxStep2(10);
+    static constexpr IVectX2 x2_size = IVectX2(10);
+    static constexpr IVectY2 y2_size = IVectY2(10);
 
 protected:
-    Patch1::IdxRange1 const idx_range_x1;
-    Patch1::IdxRange2 const idx_range_y1;
+    IDomainX1 const domain_x1;
+    IDomainY1 const domain_y1;
 
-    Patch2::IdxRange1 const idx_range_x2;
-    Patch2::IdxRange2 const idx_range_y2;
+    IDomainX2 const domain_x2;
+    IDomainY2 const domain_y2;
 
 public:
     CoordinateTransformationTest()
-        : idx_range_x1(Patch1::Idx1(0), x1_size)
-        , idx_range_y1(Patch1::Idx2(0), y1_size)
-        , idx_range_x2(Patch2::Idx1(0), x2_size)
-        , idx_range_y2(Patch2::Idx2(0), y2_size) {};
+        : domain_x1(IndexX1(0), x1_size)
+        , domain_y1(IndexY1(0), y1_size)
+        , domain_x2(IndexX2(0), x2_size)
+        , domain_y2(IndexY2(0), y2_size) {};
 
     static void SetUpTestSuite()
     {
         // Creating of meshes and supports ...........................................................
         // Patch 1
-        Patch1::Coord1 const x1_min(0.0);
-        Patch1::Coord1 const x1_max(2.0);
+        CoordX1 const x1_min(0.0);
+        CoordX1 const x1_max(2.0);
 
-        Patch1::Coord2 const y1_min(2.5);
-        Patch1::Coord2 const y1_max(7.0);
+        CoordY1 const y1_min(2.5);
+        CoordY1 const y1_max(7.0);
 
-        ddc::init_discrete_space<GridX1>(GridX1::init(x1_min, x1_max, x1_size));
-        ddc::init_discrete_space<GridY1>(GridY1::init(y1_min, y1_max, y1_size));
+        ddc::init_discrete_space<IDimX1>(IDimX1::init(x1_min, x1_max, x1_size));
+        ddc::init_discrete_space<IDimY1>(IDimY1::init(y1_min, y1_max, y1_size));
 
         // Patch 2
-        Patch2::Coord1 const x2_min(1.0);
-        Patch2::Coord1 const x2_max(3.0);
+        CoordX2 const x2_min(1.0);
+        CoordX2 const x2_max(3.0);
 
-        Patch2::Coord2 const y2_min(-4.0);
-        Patch2::Coord2 const y2_max(-3.5);
+        CoordY2 const y2_min(-4.0);
+        CoordY2 const y2_max(-3.5);
 
-        ddc::init_discrete_space<GridX2>(GridX2::init(x2_min, x2_max, x2_size));
-        ddc::init_discrete_space<GridY2>(GridY2::init(y2_min, y2_max, y2_size));
+        ddc::init_discrete_space<IDimX2>(IDimX2::init(x2_min, x2_max, x2_size));
+        ddc::init_discrete_space<IDimY2>(IDimY2::init(y2_min, y2_max, y2_size));
     }
 };
+
 
 } // namespace
 
@@ -68,56 +125,54 @@ TEST_F(CoordinateTransformationTest, InvertedOrientation)
 {
     // --- TEST 1 ---
     // Creating interfaces .......................................................................
-    Edge<GridX1> edge_1 = {1, idx_range_x1, BACK};
-    Edge<GridX2> edge_2 = {2, idx_range_x2, FRONT};
+    Edge<IDimX1> edge_1 = {1, domain_x1, BACK};
+    Edge<IDimX2> edge_2 = {2, domain_x2, FRONT};
 
-    Interface<GridX1, GridX2> interface = {edge_1, edge_2, false};
+    Interface<IDimX1, IDimX2> interface = {edge_1, edge_2, false};
 
     // Coordinate transformation .................................................................
     EdgeCoordinatesTransformation coord_transformation(interface);
 
-    Patch1::Coord1 test_coord_x1(1.5);
-    Patch2::Coord1 test_coord_x2(coord_transformation(test_coord_x1));
+    CoordX1 test_coord_x1(1.5);
+    CoordX2 test_coord_x2(coord_transformation(test_coord_x1));
 
     EXPECT_NEAR(double(test_coord_x2), 1.5, 1e-14);
 }
-
 
 
 TEST_F(CoordinateTransformationTest, StickingDifferentDimensions)
 {
     // --- TEST 2 ---
     // Creating interfaces .......................................................................
-    Edge<GridX1> edge_1 = {1, idx_range_x1, BACK};
-    Edge<GridY2> edge_2 = {2, idx_range_y2, FRONT};
+    Edge<IDimX1> edge_1 = {1, domain_x1, BACK};
+    Edge<IDimY2> edge_2 = {2, domain_y2, FRONT};
 
-    Interface<GridX1, GridY2> interface = {edge_1, edge_2, true};
+    Interface<IDimX1, IDimY2> interface = {edge_1, edge_2, true};
 
     // Coordinate transformation .................................................................
     EdgeCoordinatesTransformation coord_transformation(interface);
 
-    Patch1::Coord1 test_coord_x1(0.7);
-    Patch2::Coord2 test_coord_y2(coord_transformation(test_coord_x1));
+    CoordX1 test_coord_x1(0.7);
+    CoordY2 test_coord_y2(coord_transformation(test_coord_x1));
 
     EXPECT_NEAR(double(test_coord_y2), -3.825, 1e-14);
 }
-
 
 
 TEST_F(CoordinateTransformationTest, ReverseTransformation)
 {
     // --- TEST 3 ---
     // Creating interfaces .......................................................................
-    Edge<GridX1> edge_1 = {1, idx_range_x1, BACK};
-    Edge<GridX2> edge_2 = {2, idx_range_x2, FRONT};
+    Edge<IDimX1> edge_1 = {1, domain_x1, BACK};
+    Edge<IDimX2> edge_2 = {2, domain_x2, FRONT};
 
-    Interface<GridX1, GridX2> interface = {edge_1, edge_2, false};
+    Interface<IDimX1, IDimX2> interface = {edge_1, edge_2, false};
 
     // Coordinate transformation .................................................................
     EdgeCoordinatesTransformation coord_transformation(interface);
 
-    Patch2::Coord1 test_coord_x2(1.5);
-    Patch1::Coord1 test_coord_x1 = coord_transformation(test_coord_x2);
+    CoordX2 test_coord_x2(1.5);
+    CoordX1 test_coord_x1 = coord_transformation(test_coord_x2);
 
     EXPECT_NEAR(double(test_coord_x1), 1.5, 1e-14);
 }
