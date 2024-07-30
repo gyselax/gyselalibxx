95195eefea35ccb8cd1b5ea9420c466874083967
Emily Bourne <emily.bourne@epfl.ch>
2024-07-30 17:33:30 +0200

    Save list of patches and list of edges. Start working on get_all_idx_ranges_along_direction

--- src/multipatch/connectivity/connectivity.hpp
+++ src/multipatch/connectivity/connectivity.hpp
@@ -8,10 +8,24 @@ struct MultipatchConnectivity
 {
     using interface_collection = ddc::detail::TypeSeq<Interfaces...>;
 
-    template <class Patch>
-    using get_type_seq_connections_t =
-            typename connectivity_details::PatchConnection<Patch, interface_collection>::type;
+    using inner_edges = filter_edges_t<typename Interfaces::Edge1..., typename Interfaces::Edge2...>;
+
+    using all_patches = extract_patches_t<inner_edges>;
 
     template <class Patch>
-    using get_connections_t = connectivity_details::to_tuple_t<get_type_seq_connections_t<Patch>>;
+    using get_type_seq_connections_t = direct_patch_connections_t<Patch, interface_collection>;
+
+    template <class Patch>
+    using get_connections_t = to_tuple_t<get_type_seq_connections_t<Patch>>;
+
+    template <class Grid1D, class... Domains>
+    auto get_all_idx_ranges_along_direction(std::tuple<Domains..>)
+    {
+        using StartPatch = find_patch_t<Grid1D>;
+        using FrontEdge = Edge<StartPatch, Grid1D, FRONT>;
+        using BackEdge = Edge<StartPatch, Grid1D, BACK>;
+    }
+
+    auto get_idx_range() {
+    }
 };
--- src/multipatch/connectivity/connectivity_details.hpp
+++ src/multipatch/connectivity/connectivity_details.hpp
@@ -34,6 +34,31 @@ struct PatchConnection<Patch, ddc::detail::TypeSeq<InterfaceType1, RemainingInte
 };
 
 template <class TypeSeq>
+struct FilterEdges;
+
+template <>
+struct FilterEdges<ddc::detail::TypeSeq<>>
+{
+};
+
+template <class EdgeType>
+struct FilterEdges<ddc::detail::TypeSeq<EdgeType>>
+{
+    using type = std::conditional_t<
+            std::is_same_v<EdgeType, OutsideEdge>,
+            ddc::detail::TypeSeq<>,
+            ddc::detail::TypeSeq<EdgeType>>;
+};
+
+template <class EdgeType1, class... RemainingEdgeTypes>
+struct FilterEdges<ddc::detail::TypeSeq<EdgeType1, RemainingEdgeTypes...>>
+{
+    using type = ddc::type_seq_merge_t<
+            typename FilterEdges<ddc::detail::TypeSeq<EdgeType1>>::type,
+            typename FilterEdges<ddc::detail::TypeSeq<RemainingEdgeTypes...>>::type>;
+};
+
+template <class TypeSeq>
 struct ToTuple;
 
 template <class... I>
@@ -43,13 +68,57 @@ struct ToTuple<ddc::detail::TypeSeq<I...>>
 };
 
 template <class TypeSeq>
-using to_tuple_t = typename ToTuple<TypeSeq>::type;
+struct ExtractPatches;
 
-
-template <typename T, T val1, T... vals>
-struct AllSame
+template <>
+struct ExtractPatches<ddc::detail::TypeSeq<>>
 {
-    static constexpr T val = ((val1 == vals) && ...);
+    using type = ddc::detail::TypeSeq<>;
 };
 
+template <class EdgeType1, class... EdgeTypes>
+struct ExtractPatches<ddc::detail::TypeSeq<EdgeType1, EdgeTypes...>>
+{
+    using type = ddc::type_seq_merge_t<
+            ddc::detail::TypeSeq<typename EdgeType1::associated_patch>,
+            typename ExtractPatches<ddc::detail::TypeSeq<EdgeTypes...>>::type>;
+};
+
+template <class Grid1D, class PatchTypeSeq>
+struct FindPatch;
+
+template <class Grid1D>
+struct FindPatch<Grid1D, ddc::detail::TypeSeq<>>
+{
+    static_assert(!std::is_same_v<Grid1D, Grid1D>, "No patches found using dimension.");
+};
+
+template <class Grid1D, class Patch1, class... RemainingPatchTypes>
+struct FindPatch<Grid1D, ddc::detail::TypeSeq<Patch1, RemainingPatchTypes...>>
+{
+    using type = std::conditional_t<
+            std::is_same_v<Patch1::Grid1, Grid1D> || std::is_same_v<Patch1::Grid2, Grid1D>,
+            Patch1,
+            typename FindPatch<Grid1D, ddc::detail::TypeSeq<RemainingPatchTypes...>>::type>;
+};
+
+
 } // end namespace connectivity_details
+
+
+
+template <class TypeSeq>
+using to_tuple_t = typename connectivity_details::ToTuple<TypeSeq>::type;
+
+template <class... T>
+using filter_edges_t = typename connectivity_details::FilterEdges<ddc::detail::TypeSeq<T...>>::type;
+
+template <class TypeSeq>
+using extract_patches_t = typename connectivity_details::ExtractPatches<TypeSeq>::type;
+
+template <class Patch, class InterfaceTypeSeq>
+using direct_patch_connections_t =
+        typename connectivity_details::PatchConnection<Patch, InterfaceTypeSeq>::type;
+
+template <class Grid1D, class PatchTypeSeq>
+using find_patch_t = typename FindPatch<Grid1D, PatchTypeSeq>::type;
