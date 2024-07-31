8e8bd3041ca26d234ec0729e78e861d7b13531d3
Emily Bourne <emily.bourne@epfl.ch>
2024-07-31 09:54:57 +0200

    Update naming conventions in mpi_parallelisation
    See merge request gysela-developpers/gyselalibxx!632
    
    --------------------------------------------

--- src/mpi_parallelisation/impilayout.hpp
+++ src/mpi_parallelisation/impilayout.hpp
@@ -2,23 +2,25 @@
 #pragma once
 #include <ddc/ddc.hpp>
 
+#include "ddc_aliases.hpp"
+
 /**
  * @brief A super class describing a way in which data may be laid out across MPI processes.
  *
- * @tparam Domain The DiscreteDomain on which the data is defined.
+ * @tparam DataIdxRange The DiscreteDomain on which the data is defined.
  * @tparam DistributedDim The tags of the discrete dimensions which are distributed
  *              across MPI processes.
  */
-template <class Domain, class... DistributedDim>
+template <class DataIdxRange, class... DistributedDim>
 class IMPILayout
 {
-    static_assert(ddc::is_discrete_domain_v<Domain>);
+    static_assert(ddc::is_discrete_domain_v<DataIdxRange>);
 
 public:
-    /// The domain of the data
-    using discrete_domain_type = Domain;
-    /// The domain of the distributed section of the data
-    using distributed_subdomain = ddc::DiscreteDomain<DistributedDim...>;
+    /// The index range of the data
+    using discrete_domain_type = DataIdxRange;
+    /// The index range of the distributed section of the data
+    using distributed_sub_idx_range = IdxRange<DistributedDim...>;
     /// A type sequence describing the dimensions which are distributed across MPI processes.
     using distributed_type_seq = ddc::detail::TypeSeq<DistributedDim...>;
 
@@ -26,7 +28,7 @@ private:
     using type_seq = typename ddc::to_type_seq_t<discrete_domain_type>;
 
     template <std::size_t... I>
-    static constexpr bool check_distributed_domains_are_first(
+    static constexpr bool check_distributed_idx_ranges_are_first(
             std::integer_sequence<std::size_t, I...>)
     {
         return (ddc::in_tags_v<ddc::type_seq_element_t<I, type_seq>, distributed_type_seq> && ...);
@@ -39,6 +41,6 @@ public:
      * @brief A flag to indicate whether the distributed dimensions are the dimensions which
      * are the furthest from being contiguous in memory.
      */
-    static constexpr bool distributed_domains_are_first = check_distributed_domains_are_first(
+    static constexpr bool distributed_idx_ranges_are_first = check_distributed_idx_ranges_are_first(
             std::make_index_sequence<n_distributed_dimensions>());
 };
