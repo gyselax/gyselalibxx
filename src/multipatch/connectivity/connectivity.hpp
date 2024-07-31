e8b26a19fa9c323ccf680ff4077423796fc49b11
Emily Bourne <emily.bourne@epfl.ch>
2024-07-31 17:30:06 +0200

    First attempt at writing get_all_idx_ranges_along_direction

--- src/multipatch/connectivity/connectivity.hpp
+++ src/multipatch/connectivity/connectivity.hpp
@@ -2,13 +2,16 @@
 #pragma once
 
 #include "connectivity_details.hpp"
+#include "ddc_helper.hpp"
 
 template <class... Interfaces>
-struct MultipatchConnectivity
+class MultipatchConnectivity
 {
+public:
     using interface_collection = ddc::detail::TypeSeq<Interfaces...>;
 
-    using inner_edges = filter_edges_t<typename Interfaces::Edge1..., typename Interfaces::Edge2...>;
+    using inner_edges
+            = filter_edges_t<typename Interfaces::Edge1..., typename Interfaces::Edge2...>;
 
     using all_patches = extract_patches_t<inner_edges>;
 
@@ -19,13 +22,28 @@ struct MultipatchConnectivity
     using get_connections_t = to_tuple_t<get_type_seq_connections_t<Patch>>;
 
     template <class Grid1D, class... Domains>
-    auto get_all_idx_ranges_along_direction(std::tuple<Domains..>)
+    static auto get_all_idx_ranges_along_direction(std::tuple<Domains...> all_domains)
     {
-        using StartPatch = find_patch_t<Grid1D>;
-        using FrontEdge = Edge<StartPatch, Grid1D, FRONT>;
-        using BackEdge = Edge<StartPatch, Grid1D, BACK>;
+        using StartPatch = find_patch_t<Grid1D, all_patches>;
+        using RelevantGrids = collect_grids_on_dim_t<StartPatch, Grid1D, interface_collection>;
+        return get_idx_range(
+                all_domains,
+                std::make_index_sequence<ddcHelper::type_seq_length_v<RelevantGrids>>{});
     }
 
-    auto get_idx_range() {
+private:
+    template <class RelevantGrids, class DomainTuple, std::size_t... PatchIndex>
+    auto get_idx_range(DomainTuple all_domains, std::index_sequence<PatchIndex...>)
+    {
+        return std::tie(get_idx_range<PatchIndex, RelevantGrids, DomainTuple>(all_domains)...);
+    }
+
+    template <std::size_t PatchIndex, class RelevantGrids, class DomainTuple>
+    auto get_idx_range(DomainTuple all_domains)
+    {
+        using GridToLocate = ddc::type_seq_element_t<PatchIndex, RelevantGrids>;
+        using GridLocation
+                = find_relevant_idx_range_t<GridToLocate, ddc::to_type_seq_t<DomainTuple>>;
+        return ddc::select<GridToLocate>(std::get<GridLocation>(all_domains));
     }
 };
--- src/multipatch/connectivity/connectivity_details.hpp
+++ src/multipatch/connectivity/connectivity_details.hpp
@@ -2,6 +2,8 @@
 
 #include <ddc/ddc.hpp>
 
+#include "ddc_aliases.hpp"
+
 namespace connectivity_details {
 /**
  * @brief A class which extracts the derivative dimensions from a type sequence.
@@ -93,18 +95,171 @@ struct FindPatch<Grid1D, ddc::detail::TypeSeq<>>
     static_assert(!std::is_same_v<Grid1D, Grid1D>, "No patches found using dimension.");
 };
 
+template <class QueryGrid1D, class OGrid, class BSpl1, class BSpl2, class... RemainingPatchTypes>
+struct FindPatch<
+        QueryGrid1D,
+        ddc::detail::TypeSeq<Patch<QueryGrid1D, OGrid, BSpl1, BSpl2>, RemainingPatchTypes...>>
+{
+    using type = Patch<QueryGrid1D, OGrid, BSpl1, BSpl2>;
+};
+
+template <class QueryGrid1D, class OGrid, class BSpl1, class BSpl2, class... RemainingPatchTypes>
+struct FindPatch<
+        QueryGrid1D,
+        ddc::detail::TypeSeq<Patch<OGrid, QueryGrid1D, BSpl1, BSpl2>, RemainingPatchTypes...>>
+{
+    using type = Patch<QueryGrid1D, OGrid, BSpl1, BSpl2>;
+};
+
 template <class Grid1D, class Patch1, class... RemainingPatchTypes>
 struct FindPatch<Grid1D, ddc::detail::TypeSeq<Patch1, RemainingPatchTypes...>>
 {
-    using type = std::conditional_t<
-            std::is_same_v<Patch1::Grid1, Grid1D> || std::is_same_v<Patch1::Grid2, Grid1D>,
-            Patch1,
-            typename FindPatch<Grid1D, ddc::detail::TypeSeq<RemainingPatchTypes...>>::type>;
+    static_assert(!std::is_same_v<typename Patch1::Grid1, Grid1D>);
+    static_assert(!std::is_same_v<typename Patch1::Grid2, Grid1D>);
+    using type = typename FindPatch<Grid1D, ddc::detail::TypeSeq<RemainingPatchTypes...>>::type;
 };
 
+template <class Edge, class InterfaceTypeSeq>
+struct FindInterface;
+
+template <class Edge>
+struct FindInterface<Edge, ddc::detail::TypeSeq<>>
+{
+    static_assert(!std::is_same_v<Edge, Edge>, "Edge not found in collection of interfaces");
+};
+
+template <class Edge, class OEdge, bool Orientations, class... RemainingInterfaceTypes>
+struct FindInterface<
+        Edge,
+        ddc::detail::TypeSeq<Interface<Edge, OEdge, Orientations>, RemainingInterfaceTypes...>>
+{
+    using type = Interface<Edge, OEdge, Orientations>;
+};
+
+template <class Edge, class OEdge, bool Orientations, class... RemainingInterfaceTypes>
+struct FindInterface<
+        Edge,
+        ddc::detail::TypeSeq<Interface<OEdge, Edge, Orientations>, RemainingInterfaceTypes...>>
+{
+    using type = Interface<Edge, OEdge, Orientations>;
+};
+
+template <class Edge, class Interface1, class... RemainingInterfaceTypes>
+struct FindInterface<Edge, ddc::detail::TypeSeq<Interface1, RemainingInterfaceTypes...>>
+{
+    static_assert(
+            !(std::is_same_v<
+                      Edge,
+                      typename Interface1::
+                              Edge1> || std::is_same_v<Edge, typename Interface1::Edge2>));
+    using type =
+            typename FindInterface<Edge, ddc::detail::TypeSeq<RemainingInterfaceTypes...>>::type;
+};
+
+template <class EdgeType>
+struct SwapEnd;
+
+template <class Patch, class Grid1D>
+struct SwapEnd<Edge<Patch, Grid1D, FRONT>>
+{
+    using type = Edge<Patch, Grid1D, BACK>;
+};
+
+template <class Patch, class Grid1D>
+struct SwapEnd<Edge<Patch, Grid1D, BACK>>
+{
+    using type = Edge<Patch, Grid1D, FRONT>;
+};
+
+template <class Edge, class InterfaceTypeSeq>
+struct GetNextEdge
+{
+    using current_interface = typename FindInterface<Edge, InterfaceTypeSeq>::type;
+    using type = typename SwapEnd<typename current_interface::template OtherEdge<Edge>>::type;
+};
+
+template <class StartEdge, class InterfaceTypeSeq, class FoundGrids = ddc::detail::TypeSeq<>>
+struct CollectGridsOnDimFrontToBack
+{
+    using current_front_to_back_interface =
+            typename FindInterface<StartEdge, InterfaceTypeSeq>::type;
+    using MatchingBackEdge =
+            typename current_front_to_back_interface::template OtherEdge<StartEdge>;
+    using type = std::conditional_t<
+            std::is_same_v<
+                    MatchingBackEdge,
+                    OutsideEdge> || ddc::in_tags_v<StartEdge::associated_patch, FoundGrids>,
+            FoundGrids,
+            CollectGridsOnDimFrontToBack<
+                    typename SwapEnd<MatchingBackEdge>::type,
+                    InterfaceTypeSeq,
+                    ddc::type_seq_merge_t<
+                            FoundGrids,
+                            ddc::detail::TypeSeq<typename StartEdge::grid>>>>;
+};
+
+template <class StartEdge, class InterfaceTypeSeq, class FoundGrids = ddc::detail::TypeSeq<>>
+struct CollectGridsOnDimBackToFront
+{
+    using current_back_to_front_interface =
+            typename FindInterface<StartEdge, InterfaceTypeSeq>::type;
+    using MatchingFrontEdge =
+            typename current_back_to_front_interface::template OtherEdge<StartEdge>;
+    using type = std::conditional_t<
+            std::is_same_v<
+                    MatchingFrontEdge,
+                    OutsideEdge> || ddc::in_tags_v<typename StartEdge::associated_patch, FoundGrids>,
+            FoundGrids,
+            CollectGridsOnDimBackToFront<
+                    typename SwapEnd<MatchingFrontEdge>::type,
+                    InterfaceTypeSeq,
+                    ddc::type_seq_merge_t<
+                            ddc::detail::TypeSeq<typename StartEdge::grid>,
+                            FoundGrids>>>;
+};
+
+template <class StartPatch, class Grid1D, class InterfaceTypeSeq>
+struct CollectGridsOnDim
+{
+    using type = ddc::type_seq_merge_t<
+            typename CollectGridsOnDimBackToFront<
+                    Edge<StartPatch, Grid1D, BACK>,
+                    InterfaceTypeSeq>::type,
+            typename CollectGridsOnDimFrontToBack<
+                    Edge<StartPatch, Grid1D, FRONT>,
+                    InterfaceTypeSeq>::type>;
+};
+
+template <class QueryGrid1D, class IdxRangeType>
+struct SelectRelevantIdxRangeType;
+
+template <class QueryGrid1D, class... IdxRangeGrids>
+struct SelectRelevantIdxRangeType<QueryGrid1D, IdxRange<IdxRangeGrids...>>
+{
+    using type = std::conditional_t<
+            ddc::in_tags_v<QueryGrid1D, ddc::detail::TypeSeq<IdxRangeGrids...>>,
+            ddc::detail::TypeSeq<IdxRange<IdxRangeGrids...>>,
+            ddc::detail::TypeSeq<>>;
+};
+
+template <class QueryGrid1D, class IdxRangeTuple>
+struct FindRelevantIdxRangeType;
+
+template <class QueryGrid1D>
+struct FindRelevantIdxRangeType<QueryGrid1D, std::tuple<>>
+{
+    using type = ddc::detail::TypeSeq<>;
+};
+
+template <class QueryGrid1D, class HeadIdxRangeType, class... IdxRangeTypes>
+struct FindRelevantIdxRangeType<QueryGrid1D, std::tuple<HeadIdxRangeType, IdxRangeTypes...>>
+{
+    using type = ddc::type_seq_merge_t<
+            typename SelectRelevantIdxRangeType<QueryGrid1D, HeadIdxRangeType>::type,
+            typename FindRelevantIdxRangeType<QueryGrid1D, std::tuple<IdxRangeTypes...>>::type>;
+};
 
 } // end namespace connectivity_details
-
 
 
 template <class TypeSeq>
@@ -121,4 +276,13 @@ using direct_patch_connections_t =
         typename connectivity_details::PatchConnection<Patch, InterfaceTypeSeq>::type;
 
 template <class Grid1D, class PatchTypeSeq>
-using find_patch_t = typename FindPatch<Grid1D, PatchTypeSeq>::type;
+using find_patch_t = typename connectivity_details::FindPatch<Grid1D, PatchTypeSeq>::type;
+
+template <class StartPatch, class Grid1D, class InterfaceTypeSeq>
+using collect_grids_on_dim_t = typename connectivity_details::
+        CollectGridsOnDim<StartPatch, Grid1D, InterfaceTypeSeq>::type;
+
+template <class QueryGrid1D, class IdxRangeTuple>
+using find_relevant_idx_range_t = ddc::type_seq_element_t<
+        0,
+        typename connectivity_details::FindRelevantIdxRangeType<QueryGrid1D, IdxRangeTuple>::type>;
