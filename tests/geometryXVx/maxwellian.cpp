83a956968fd7747ebf93cee4143359b4f8995bba
Yann Munschy <yann.munschy@cea.fr>
2024-08-09 13:13:18 +0200

    Re-enable Maxwellian test
    See merge request gysela-developpers/gyselalibxx!650
    
    --------------------------------------------

--- tests/geometryXVx/maxwellian.cpp
+++ tests/geometryXVx/maxwellian.cpp
@@ -13,7 +13,7 @@
 #include <quadrature.hpp>
 #include <trapezoid_quadrature.hpp>
 
-TEST(Maxwellian, Moments)
+static void TestMaxwellian()
 {
     /**
      * Tests if the DFieldMemVx maxwellian function defines a maxwellian with the correct moments
@@ -25,41 +25,59 @@ TEST(Maxwellian, Moments)
     // Creating mesh & supports
     ddc::init_discrete_space<BSplinesVx>(vx_min, vx_max, vx_size);
 
-    ddc::init_discrete_space<GridVx>(InterpPointsVx::get_sampling());
+    ddc::init_discrete_space<GridVx>(SplineInterpPointsVx::get_sampling<GridVx>());
 
-    IdxRangeVx gridvx(InterpPointsVx::get_domain());
+    IdxRangeVx gridvx(SplineInterpPointsVx::get_domain<GridVx>());
 
     SplineVxBuilder_1d const builder_vx(gridvx);
 
-    host_t<DFieldMemVx> const quadrature_coeffs = trapezoid_quadrature_coefficients(gridvx);
-    host_t<Quadrature<IdxRangeVx>> const integrate_v(quadrature_coeffs);
+    host_t<DFieldMemVx> const quadrature_coeffs_host = trapezoid_quadrature_coefficients(gridvx);
+    auto quadrature_coeffs = ddc::create_mirror_view_and_copy(
+            Kokkos::DefaultExecutionSpace(),
+            get_field(quadrature_coeffs_host));
+    Quadrature<IdxRangeVx> integrate_v(get_const_field(quadrature_coeffs));
 
     double const density = 1.e-5;
     double const mean_velocity = 0.5;
     double const temperature = 0.5;
 
-    host_t<DFieldMemVx> fdistribu(gridvx);
-    MaxwellianEquilibrium::
-            compute_maxwellian(get_field(fdistribu), density, temperature, mean_velocity);
+    DFieldMemVx fdistribu_alloc(gridvx);
+    DFieldVx fdistribu = get_field(fdistribu_alloc);
+    MaxwellianEquilibrium::compute_maxwellian(fdistribu, density, temperature, mean_velocity);
 
     // density
-    double const density_res = integrate_v(fdistribu);
+    double const density_res = integrate_v(Kokkos::DefaultExecutionSpace(), fdistribu);
 
     // mean velocity
-    host_t<DFieldMemVx> integrand(gridvx);
-    ddc::for_each(gridvx, [&](IdxVx const ivx) {
-        integrand(ivx) = ddc::coordinate(ivx) * fdistribu(ivx);
-    });
-    double const mean_velocity_res = integrate_v(integrand) / density_res;
+    DFieldMemVx integrand_alloc(gridvx);
+    DFieldVx integrand = get_field(integrand_alloc);
+    ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
+            gridvx,
+            KOKKOS_LAMBDA(IdxVx const ivx) {
+                integrand(ivx) = ddc::coordinate(ivx) * fdistribu(ivx);
+            });
+
+    double const mean_velocity_res
+            = integrate_v(Kokkos::DefaultExecutionSpace(), get_field(integrand)) / density_res;
 
     // temperature
-    ddc::for_each(gridvx, [&](IdxVx const ivx) {
-        double const velocity = ddc::coordinate(ivx) - mean_velocity_res;
-        integrand(ivx) = velocity * velocity * fdistribu(ivx);
-    });
-    double const temperature_res = integrate_v(integrand) / density_res;
+    ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
+            gridvx,
+            KOKKOS_LAMBDA(IdxVx const ivx) {
+                double const velocity = ddc::coordinate(ivx) - mean_velocity_res;
+                integrand(ivx) = velocity * velocity * fdistribu(ivx);
+            });
+    double const temperature_res
+            = integrate_v(Kokkos::DefaultExecutionSpace(), get_field(integrand)) / density_res;
 
     EXPECT_LE(std::fabs(density_res - density), 1e-12);
     EXPECT_LE(std::fabs(mean_velocity_res - mean_velocity), 1e-12);
     EXPECT_LE(std::fabs(temperature_res - temperature), 1e-12);
+}
+
+TEST(Maxwellian, Moments)
+{
+    TestMaxwellian();
 }
