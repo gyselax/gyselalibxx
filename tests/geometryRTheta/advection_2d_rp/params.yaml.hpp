816723ce8ea74ebd804f062be10e80d7436b7206
Emily Bourne <emily.bourne@epfl.ch>
2024-08-14 16:56:53 +0200

    Add helper functions to build test meshes
    Add helper functions to build uniform or non-uniform test meshes. The uniform case is used to clean up existing code. The non-uniform case is used to test basic quadrature (related to #257).
    
    Closes #334
    
    See merge request gysela-developpers/gyselalibxx!661
    
    --------------------------------------------

--- tests/geometryRTheta/advection_2d_rp/params.yaml.hpp
+++ tests/geometryRTheta/advection_2d_rp/params.yaml.hpp
@@ -2,13 +2,15 @@
 
 #pragma once
 
-constexpr char const* const params_yaml = R"PDI_CFG(Mesh:
+constexpr char const* const params_yaml = R"PDI_CFG(SplineMesh:
   r_min: 0.
   r_max: 1.
-  r_size: 128
-  p_size: 256
+  r_ncells: 128
+  p_ncells: 256
+Time:
   time_step: 1e-2
   final_time: 1
+Output:
   save_curves: false
   save_feet: false
 )PDI_CFG";
