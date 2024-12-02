068e04afd8969c9400ccd3c3422a952cd7df2731
Emily Bourne <emily.bourne@epfl.ch>
2024-12-02 09:55:12 +0100

    Set up GitHub CI for pull requests
    Set up the GitHub CI for pull requests. The following tests are run on all PRs:
    - Clang Formatting Check
    - Markdown format check
    - Python Best Practices
    - Documentation check
    - CppCheck static analysis (this check is split into 2, 1 (which will be compulsory) which only fails if there are errors, 1 which also fails for warnings, this is done because GitHub has no way to indicate warnings on tests)
    - CMake static analysis
    
    The following tests are run on non-draft PRs:
    - CPU Test (test_toolchain)
    - CPU Test (test_release_toolchain)
    - CPU Test (test_release_omp_toolchain)
    - GPU Test
    
    If the 10 tests pass then the label "Ready to review" is automatically added to the PR.
    
    If one of the tests fail and the PR is marked as ready for review then the PR is automatically sent back to draft status and a comment is left on the PR to warn the author.
    
    If commits are added to a PR that is marked as ready for review while tests are already running then the outdated tests are cancelled and the PR is marked as draft again.
    
    CPU and GPU tests are only run if .hpp, .cpp, CMake files, or vendor files are changed.
    
    See merge request gysela-developpers/gyselalibxx!778
    
    --------------------------------------------

--- src/utils/non_uniform_interpolation_points.hpp
+++ src/utils/non_uniform_interpolation_points.hpp
@@ -101,9 +101,11 @@ public:
             if (point == ddc::coordinate(current_cell_end_idx)) {
                 check_n_points_in_cell(n_points_in_cell + 1, current_cell_end_idx);
                 n_points_in_cell = 1;
+                current_cell_end_idx += 1;
             } else if (point > ddc::coordinate(current_cell_end_idx)) {
                 check_n_points_in_cell(n_points_in_cell, current_cell_end_idx);
                 n_points_in_cell = 1;
+                current_cell_end_idx += 1;
             } else {
                 n_points_in_cell += 1;
             }
