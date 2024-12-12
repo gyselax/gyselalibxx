9be65737025c197f5607b7f3af1848b4a6fcc7a3
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2024-12-12 23:51:55 +0100

    [GEOM TOKAMAXI] Add partial_derivatives.hpp.

---
+++ src/math_tools/partial_derivatives.hpp
@@ -0,0 +1,71 @@
+
+// SPDX-License-Identifier: MIT
+/**
+ * @file partial_derivatives.hpp
+ * File containing functions to compute the partial derivatives
+ */
+
+#pragma once
+
+#include <ddc/ddc.hpp>
+
+#include "ddc_alias_inline_functions.hpp"
+#include "ddc_aliases.hpp"
+
+
+/**
+ * @brief Compute the partial derivative of a 
+ *
+ * For a given field @f$F(X1,X2)@f$ , compute
+ * @f$\partial_{Xi} F(X1,X2)@f$ with i=1 or 2.
+ *
+ * @param[in] Xi
+ *      The given coordinate for the partial derivative.
+ *
+ * @return @f$\partial_{Xi} F(X1,X2)@f$ 
+ */
+
+template <
+        class GridXi,
+        class GridX1,
+        class GridX2,
+        class FieldX1Builder,
+        class FieldX1Evaluator>
+class PartialDerivative
+{
+private:
+    using IdxRangeX1X2 = IdxRange<GridX1, GridX2>;
+    using IdxX1X2 = typename IdxRangeX1X2::discrete_element_type;
+    using DFieldX1X2 = DField<double, IdxRangeX1X2>;
+    using DConstFieldX1X2 = DConstField<double, IdxRangeX1X2>;
+
+    // Type for spline representation of the field
+    using IdxRangeBSFieldX1 = typename FieldX1Builder::batched_spline_domain_type;
+    using FieldX1SplineMem = DFieldMem<IdxRangeBSFieldX1>;
+    using FieldX1SplineCoeffs = DField<IdxRangeBSFieldX1>;
+
+    FieldX1Builder const& m_fieldx1_builder;
+    FieldX1Evaluator const& m_fieldx1_evaluator;
+
+public:
+    explicit PartialDerivative(
+        FieldX1Builder const& fieldx1_builder,
+        FieldX1Evaluator const& fieldx1_evaluator)
+        : m_fieldx1_builder(fieldx1_builder)
+        , m_fieldx1_evaluator(fieldx1_evaluator)             
+    {    
+    }
+
+    ~PartialDerivative() = default;
+
+    DFieldX1X2 operator()(DFieldX1X2 dfield_dx1_x1x2, DConstFieldX1X2 field_x1x2)
+    {
+        // Build spline representation of the field ....................................
+        FieldX1SplineMem fieldx1_coefs_alloc(
+            m_fieldx1_builder.batched_spline_domain());
+        FieldX1SplineCoeffs fieldx1_coefs = get_field(fieldx1_coefs_alloc);
+
+        m_fieldx1_builder(fieldx1_coefs, get_const_field(field_x1x2));
+        m_fieldx1_evaluator.deriv(dfield_dx1_x1x2, fieldx1_coefs);
+    }
+};
---
+++ tests/math_tools/test_partial_derivatives.cpp
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+#include <ddc/ddc.hpp>
+
+#include <gtest/gtest.h>
+
+//VG//#include "ddc_alias_inline_functions.hpp"
+#include "ddc_aliases.hpp"
+//VG//#include "ddc_helper.hpp"
+#include "partial_derivatives.hpp"
+
+namespace {
+
+struct X
+{
+    static bool constexpr PERIODIC = false;
+};
+using CoordX = Coord<X>;
+struct BSplinesX : ddc::UniformBSplines<X, 3>
+{
+};
+auto constexpr SplineXBoundary = ddc::BoundCond::GREVILLE;
+using SplineInterpPointsX
+        = ddc::GrevilleInterpolationPoints<BSplinesX, SplineXBoundary, SplineXBoundary>;
+
+struct GridX : SplineInterpPointsX::interpolation_discrete_dimension_type
+{
+};
+using IdxX = Idx<GridX>;
+using IdxStepX = IdxStep<GridX>;
+using IdxRangeX = IdxRange<GridX>;
+
+struct Y
+{  
+    static bool constexpr PERIODIC = false;
+};
+using CoordY = Coord<Y>;
+struct BSplinesY : ddc::UniformBSplines<Y, 3>
+{
+};
+auto constexpr SplineYBoundary = ddc::BoundCond::GREVILLE;
+using SplineInterpPointsY
+        = ddc::GrevilleInterpolationPoints<BSplinesY, SplineYBoundary, SplineYBoundary>;
+struct GridY : SplineInterpPointsY::interpolation_discrete_dimension_type
+{
+};
+using IdxY = Idx<GridY>;
+using IdxStepY = IdxStep<GridY>;
+using IdxRangeY = IdxRange<GridY>;
+
+using IdxXY = Idx<GridX, GridY>;
+using IdxRangeXY = IdxRange<GridX, GridY>;
+using DFieldMemXY = DFieldMem<IdxRangeXY>;
+using DFieldXY = DField<IdxRangeXY>;
+using DConstFieldXY = DConstField<IdxRangeXY>;
+
+// --- Operators ---
+using SplineXBuilder = ddc::SplineBuilder<
+        Kokkos::DefaultExecutionSpace,
+        Kokkos::DefaultExecutionSpace::memory_space,
+        BSplinesX,
+        GridX,
+        SplineXBoundary,
+        SplineXBoundary,
+        ddc::SplineSolver::LAPACK,
+        GridX>;
+using SplineXEvaluator = ddc::SplineEvaluator<
+        Kokkos::DefaultExecutionSpace,
+        Kokkos::DefaultExecutionSpace::memory_space,
+        BSplinesX,
+        GridX,
+        ddc::ConstantExtrapolationRule<X>,
+        ddc::ConstantExtrapolationRule<X>,
+        GridX,
+        GridY>;
+
+TEST(PartialDerivative, PartialDerivativeDx)
+{
+    int n_elems_x(10);
+    int n_elems_y(20);
+
+    Coord<X> const x_min(0.0);
+    Coord<X> const x_max(1.0);
+    IdxStepX x_ncells(n_elems_x);
+
+    Coord<Y> const y_min(0.0);
+    Coord<Y> const y_max(2.0);
+    IdxStepY y_ncells(n_elems_y);
+
+    ddc::init_discrete_space<BSplinesX>(x_min, x_max, x_ncells);
+    ddc::init_discrete_space<GridX>(SplineInterpPointsX::get_sampling<GridX>());
+    IdxRangeX idxrange_x(SplineInterpPointsX::get_domain<GridX>());
+
+    ddc::init_discrete_space<BSplinesY>(y_min, y_max, y_ncells);
+    ddc::init_discrete_space<GridY>(SplineInterpPointsY::get_sampling<GridY>());
+    IdxRangeY idxrange_y(SplineInterpPointsY::get_domain<GridY>());
+
+    SplineXBuilder const builder_x(idxrange_x);
+    ddc::ConstantExtrapolationRule<X> bv_x_min(x_min);
+    ddc::ConstantExtrapolationRule<X> bv_x_max(x_max);
+    SplineXEvaluator const spline_evaluator_x(bv_x_min, bv_x_max);
+
+    IdxRangeXY idxrange_xy(idxrange_x, idxrange_y);
+    DFieldMemXY field_xy_alloc(idxrange_xy);
+    DFieldXY field_xy = get_field(field_xy_alloc);
+
+    ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
+            idxrange_xy,
+            KOKKOS_LAMBDA(IdxXY const idx_xy) {
+                IdxX idx_x(idx_xy);
+                IdxY idx_y(idx_xy);
+                field_xy(idx_xy) = ddc::coordinate(idx_x)*ddc::coordinate(idx_x) * ddc::coordinate(idx_y);
+            });
+
+    PartialDerivative<GridX, GridX, GridY, SplineXBuilder, SplineXEvaluator> partial_dx(builder_x, spline_evaluator_x);
+    DFieldMemXY dfield_dx_xy_alloc(idxrange_xy);
+    DFieldXY dfield_dx_xy = get_field(dfield_dx_xy_alloc);
+    partial_dx(dfield_dx_xy, field_xy);
+}
+
+} // namespace
