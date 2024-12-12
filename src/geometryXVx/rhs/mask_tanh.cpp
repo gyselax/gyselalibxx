6efbf97349aea81bd24f4b5753dacd70a73179cf
Matthieu PROTAIS <matthieu.protais@cea.fr>
2024-12-12 10:15:15 +0100

    Resolve "Add tests in the mask initialiation"
    Closes #415
    
    See merge request gysela-developpers/gyselalibxx!807
    
    --------------------------------------------

--- src/geometryXVx/rhs/mask_tanh.cpp
+++ src/geometryXVx/rhs/mask_tanh.cpp
@@ -15,42 +15,50 @@ host_t<DFieldMemX> mask_tanh(
         MaskType const type,
         bool const normalized)
 {
-    host_t<DFieldMemX> mask(gridx);
-
-    IdxStepX const Nx(gridx.size());
-    CoordX const x_min(ddc::coordinate(gridx.front()));
-    double const Lx = ddcHelper::total_interval_length(gridx);
-    CoordX const x_left(x_min + Lx * extent);
-    CoordX const x_right(x_min + Lx - Lx * extent);
-
-    switch (type) {
-    case MaskType::Normal:
-        ddc::for_each(gridx, [&](IdxX const ix) {
-            CoordX const coordx = ddc::coordinate(ix);
-            mask(ix) = 0.5
-                       * (std::tanh((coordx - x_left) / stiffness)
-                          - std::tanh((coordx - x_right) / stiffness));
-        });
-        break;
-
-    case MaskType::Inverted:
-        ddc::for_each(gridx, [&](IdxX const ix) {
-            CoordX const coordx = ddc::coordinate(ix);
-            mask(ix) = 1
-                       - 0.5
-                                 * (std::tanh((coordx - x_left) / stiffness)
-                                    - std::tanh((coordx - x_right) / stiffness));
-        });
-        break;
+    if (stiffness <= 0) {
+        throw std::runtime_error("Invalid stiffness, cannot be negative");
     }
+    if (extent < 0 || extent > 0.5) {
+        throw std::runtime_error("Invalid extent, cannot be more than 0.5");
+    }
+    host_t<DFieldMemX> mask(gridx);
+    if (extent == 0) {
+        ddc::parallel_fill(mask, 0.);
+    } else {
+        CoordX const x_min(ddc::coordinate(gridx.front()));
+        double const Lx = ddcHelper::total_interval_length(gridx);
+        CoordX const x_left(x_min + Lx * extent);
+        CoordX const x_right(x_min + Lx - Lx * extent);
 
-    if (normalized) {
-        host_t<DFieldMemX> const quadrature_coeffs
-                = trapezoid_quadrature_coefficients<Kokkos::DefaultHostExecutionSpace>(gridx);
-        host_t<Quadrature<IdxRangeX>> const integrate_x(get_const_field(quadrature_coeffs));
-        double const coeff_norm
-                = integrate_x(Kokkos::DefaultHostExecutionSpace(), get_const_field(mask));
-        ddc::for_each(gridx, [&](IdxX const ix) { mask(ix) = mask(ix) / coeff_norm; });
+        switch (type) {
+        case MaskType::Normal:
+            ddc::for_each(gridx, [&](IdxX const ix) {
+                CoordX const coordx = ddc::coordinate(ix);
+                mask(ix) = 0.5
+                           * (std::tanh((coordx - x_left) / stiffness)
+                              - std::tanh((coordx - x_right) / stiffness));
+            });
+            break;
+
+        case MaskType::Inverted:
+            ddc::for_each(gridx, [&](IdxX const ix) {
+                CoordX const coordx = ddc::coordinate(ix);
+                mask(ix) = 1
+                           - 0.5
+                                     * (std::tanh((coordx - x_left) / stiffness)
+                                        - std::tanh((coordx - x_right) / stiffness));
+            });
+            break;
+        }
+
+        if (normalized) {
+            host_t<DFieldMemX> const quadrature_coeffs
+                    = trapezoid_quadrature_coefficients<Kokkos::DefaultHostExecutionSpace>(gridx);
+            host_t<Quadrature<IdxRangeX>> const integrate_x(get_const_field(quadrature_coeffs));
+            double const coeff_norm
+                    = integrate_x(Kokkos::DefaultHostExecutionSpace(), get_const_field(mask));
+            ddc::for_each(gridx, [&](IdxX const ix) { mask(ix) = mask(ix) / coeff_norm; });
+        }
     }
 
     return mask;
