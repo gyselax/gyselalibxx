61658df62d990a7c86f14d3ce1ed273ead16e18d
Emily Bourne <emily.bourne@epfl.ch>
2025-01-07 10:06:11 +0100

    Adapt test to allow other partial derivative calculators to be tested

--- tests/math_tools/test_partial_derivatives.cpp
+++ tests/math_tools/test_partial_derivatives.cpp
@@ -73,8 +73,41 @@ using SplineXEvaluator = ddc::SplineEvaluator<
         GridX,
         GridY>;
 
+void test_partial_derivative_dx(IPartialDerivative<DFieldXY, X> partial_dx, IdxRangeXY idxrange_xy)
+{
+    DFieldMemXY field_xy_alloc(idxrange_xy);
+    DFieldXY field_xy = get_field(field_xy_alloc);
 
-static void TestSplinePartialDerivativeDx()
+    ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
+            idxrange_xy,
+            KOKKOS_LAMBDA(IdxXY const idx_xy) {
+                IdxX idx_x(idx_xy);
+                IdxY idx_y(idx_xy);
+                field_xy(idx_xy)
+                        = ddc::coordinate(idx_x) * ddc::coordinate(idx_x) * ddc::coordinate(idx_y);
+            });
+
+    DFieldMemXY dfield_dx_xy_alloc(idxrange_xy);
+    DFieldXY dfield_dx_xy = get_field(dfield_dx_xy_alloc);
+    partial_dx(dfield_dx_xy, get_const_field(field_xy));
+
+    double max_error = ddc::parallel_transform_reduce(
+            Kokkos::DefaultExecutionSpace(),
+            idxrange_xy,
+            0.,
+            ddc::reducer::max<double>(),
+            KOKKOS_LAMBDA(IdxXY const idx_xy) {
+                IdxX idx_x(idx_xy);
+                IdxY idx_y(idx_xy);
+                double const dfield_dx_anal = 2. * ddc::coordinate(idx_x) * ddc::coordinate(idx_y);
+                return Kokkos::abs(dfield_dx_xy(idx_xy) - dfield_dx_anal);
+            });
+    EXPECT_LE(max_error, 1e-12);
+}
+
+
+TEST(PartialDerivative, SplinePartialDerivativeDx)
 {
     int n_elems_x(10);
     int n_elems_y(20);
@@ -102,42 +135,9 @@ static void TestSplinePartialDerivativeDx()
     ddc::ConstantExtrapolationRule<X> bv_x_max(x_max);
     SplineXEvaluator const spline_evaluator_x(bv_x_min, bv_x_max);
 
-    DFieldMemXY field_xy_alloc(idxrange_xy);
-    DFieldXY field_xy = get_field(field_xy_alloc);
-
-    ddc::parallel_for_each(
-            Kokkos::DefaultExecutionSpace(),
-            idxrange_xy,
-            KOKKOS_LAMBDA(IdxXY const idx_xy) {
-                IdxX idx_x(idx_xy);
-                IdxY idx_y(idx_xy);
-                field_xy(idx_xy)
-                        = ddc::coordinate(idx_x) * ddc::coordinate(idx_x) * ddc::coordinate(idx_y);
-            });
-
-    SplinePartialDerivative<SplineXBuilder, SplineXEvaluator> partial_dx(builder_x, spline_evaluator_x);
-    DFieldMemXY dfield_dx_xy_alloc(idxrange_xy);
-    DFieldXY dfield_dx_xy = get_field(dfield_dx_xy_alloc);
-    partial_dx(dfield_dx_xy, get_const_field(field_xy));
-
-    double max_error = ddc::parallel_transform_reduce(
-            Kokkos::DefaultExecutionSpace(),
-            idxrange_xy,
-            0.,
-            ddc::reducer::max<double>(),
-            KOKKOS_LAMBDA(IdxXY const idx_xy) {
-                IdxX idx_x(idx_xy);
-                IdxY idx_y(idx_xy);
-                double const dfield_dx_anal = 2. * ddc::coordinate(idx_x) * ddc::coordinate(idx_y);
-                return Kokkos::abs(dfield_dx_xy(idx_xy) - dfield_dx_anal);
-            });
-    EXPECT_LE(max_error, 1e-12);
-}
-
-
-TEST(PartialDerivative, SplinePartialDerivativeDx)
-{
-    TestSplinePartialDerivativeDx();
+    SplinePartialDerivative<SplineXBuilder, SplineXEvaluator>
+            partial_dx(builder_x, spline_evaluator_x);
+    test_partial_derivative_dx(partial_dx, idxrange_xy);
 }
 
 } // namespace
