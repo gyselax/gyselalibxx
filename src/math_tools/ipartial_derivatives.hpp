c544e7106c3d255b16902e5d6f8f1cc38576ddd2
Emily Bourne <emily.bourne@epfl.ch>
2025-01-07 10:37:34 +0100

    Add more helpful aliases

--- src/math_tools/ipartial_derivatives.hpp
+++ src/math_tools/ipartial_derivatives.hpp
@@ -11,6 +11,7 @@
 
 #include "ddc_alias_inline_functions.hpp"
 #include "ddc_aliases.hpp"
+#include "type_seq_tools.hpp"
 
 
 /**
@@ -19,7 +20,7 @@
 template <class DFieldValue, class DerivDirection>
 class IPartialDerivative
 {
-    static_assert(is_borrowed_chunk_v<DFieldValue>);
+    static_assert(ddc::is_borrowed_chunk_v<DFieldValue>);
 
 public:
     /// The dimension Xi on which the partial derivative is calculated.
@@ -34,6 +35,15 @@ public:
     /// The type of the calculated derivative.
     using DConstFieldVal = typename DFieldValue::view_type;
 
+    /// The type of the grid on the dimension Xi on which the partial derivative is calculated.
+    using GridDerivativeDirection = find_grid_t<DerivativeDirection, ddc::to_type_seq_t<IdxRangeFieldVal>>;
+
+    /// The index range of the dimension Xi on which the partial derivative is calculated.
+    using IdxRangeDeriv = IdxRange<GridDerivativeDirection>;
+
+    /// The index range of all dimensions except Xi.
+    using IdxRangeBatch = ddc::remove_dims_of_t<IdxRangeFieldVal, GridDerivativeDirection>;
+
 public:
     /**
     * @brief Compute the partial derivative of @f$ F(X1,..,Xn)@f$ in Xi direction.
@@ -41,5 +51,5 @@ public:
     * @param[out] dfieldval_dxi Partial derivatives in Xi direction.
     * @param[in] fieldval Values of the field @f$ F(X1,..,Xn)@f$.
     */
-    void operator()(DFieldVal dfieldval_dxi, DConstFieldVal fieldval) = 0;
+    virtual void operator()(DFieldVal dfieldval_dxi, DConstFieldVal fieldval) const = 0;
 };
