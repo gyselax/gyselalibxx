0a5733e9d4da2de9d86473db198fb735d599c816
BOURNE Emily EPFL <emily.bourne@epfl.ch>
2025-02-11 11:10:58 +0100

    Use GPU objects

--- tests/geometryRTheta/advection_2d_rp/advection_all_tests.cpp
+++ tests/geometryRTheta/advection_2d_rp/advection_all_tests.cpp
@@ -295,12 +295,13 @@ int main(int argc, char** argv)
 
     // DEFINITION OF OPERATORS ------------------------------------------------------------------
     // --- Builders for the test function and the to_physical_mapping:
-    SplineRThetaBuilder_host const builder(grid);
+    SplineRThetaBuilder_host const builder_host(grid);
+    SplineRThetaBuilder const builder(grid);
 
     // --- Evaluator for the test function:
     ddc::NullExtrapolationRule r_extrapolation_rule;
     ddc::PeriodicExtrapolationRule<Theta> p_extrapolation_rule;
-    SplineRThetaEvaluatorNullBound_host spline_evaluator(
+    SplineRThetaEvaluatorNullBound spline_evaluator(
             r_extrapolation_rule,
             r_extrapolation_rule,
             p_extrapolation_rule,
@@ -331,7 +332,7 @@ int main(int argc, char** argv)
     DiscreteMappingBuilder const discrete_czarny_map_builder(
             Kokkos::DefaultHostExecutionSpace(),
             from_czarny_map,
-            builder,
+            builder_host,
             spline_evaluator_extrapol);
     DiscreteToCartesian const from_discrete_czarny_map = discrete_czarny_map_builder();
 
@@ -381,7 +382,7 @@ int main(int argc, char** argv)
     GeneralParameters params
             = {grid,
                interpolator,
-               builder,
+               builder_host,
                spline_evaluator_extrapol,
                final_time,
                if_save_curves,
