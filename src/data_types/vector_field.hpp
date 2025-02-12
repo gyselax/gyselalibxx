364c6504eb168ceea15b63b8576d57989b2f0d3f
BOURNE Emily EPFL <emily.bourne@epfl.ch>
2025-02-12 17:47:33 +0100

    Missing return

--- src/data_types/vector_field.hpp
+++ src/data_types/vector_field.hpp
@@ -455,6 +455,7 @@ auto create_mirror_view_and_copy(
                 field_alloc(get_idx_range(field));
         ((ddc::parallel_deepcopy(field_alloc.template get<Dims>(), field.template get<Dims>())),
          ...);
+        return field_alloc;
     }
 }
 } // namespace ddcHelper
