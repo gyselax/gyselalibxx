04ee850d06a8db621e14f525257fd268cd1d76e7
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-21 11:14:46 +0100

    Review of Emily
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>

--- toolchains/docker.gyselalibxx_env/tests_toolchain.cmake
+++ toolchains/docker.gyselalibxx_env/tests_toolchain.cmake
@@ -2,7 +2,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../cicd_default_toolchain.cmake)
 
 # CMake options
-set(CMAKE_BUILD_TYPE FakeDebug)
+set(CMAKE_BUILD_TYPE Debug)
 
 # Compiler options
 set(CMAKE_CXX_FLAGS "-Wall -Werror=parentheses -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare")
