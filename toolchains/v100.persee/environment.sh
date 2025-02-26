7c0d2ad7a554019820937ecdc4be9703476cafec
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-26 14:13:23 +0100

    Revert back to gyselalibxx-env-omp-cuda

--- toolchains/v100.persee/environment.sh
+++ toolchains/v100.persee/environment.sh
@@ -8,7 +8,7 @@ fi
 . /data/gyselarunner/spack-0.23.0/share/spack/setup-env.sh
 
 spack load gcc@12
-spack env activate gyselalibxx-env-omp-cuda-candidate
+spack env activate gyselalibxx-env-omp-cuda
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=8
