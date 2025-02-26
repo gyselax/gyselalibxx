45cd6b785488286c095d06a2be8761cdf854b2f3
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-26 08:38:36 +0100

    Update Persee environment with Kokkos 4.5.1, Kokkos Kernels 4.5.1 and Kokkos-fft 0.2.1

--- toolchains/v100.persee/toolchain.cmake
+++ toolchains/v100.persee/toolchain.cmake
@@ -5,16 +5,11 @@ include(${CMAKE_CURRENT_LIST_DIR}/../cicd_default_toolchain.cmake)
 set(CMAKE_BUILD_TYPE Release)
 
 # Compiler options
-set(CMAKE_CXX_COMPILER ${CMAKE_CURRENT_LIST_DIR}/../../vendor/kokkos/bin/nvcc_wrapper)
+set(CMAKE_CXX_COMPILER nvcc_wrapper)
 set(CMAKE_CXX_FLAGS "-Wall -Wno-sign-compare --Werror cross-execution-space-call -Xcudafe --diag_suppress=unsigned_compare_with_zero -Xcudafe --diag_suppress=integer_sign_change")
 
 # Gyselalibxx options
 set(GYSELALIBXX_DEFAULT_CXX_FLAGS "" CACHE STRING "Default flags for C++ specific to Voice++" FORCE)
-
-# Kokkos options
-set(Kokkos_ENABLE_SERIAL ON CACHE BOOL "Allow serial code to run" FORCE)
-set(Kokkos_ENABLE_CUDA ON CACHE BOOL "Activate GPU usage via cuda" FORCE)
-set(Kokkos_ARCH_VOLTA70 ON CACHE BOOL "Indicate that the GPU architecture is V100" FORCE)
 
 # Activate/deactivate parts of the code
 if (DEFINED ENV{DDC_BUILD_TESTING})
