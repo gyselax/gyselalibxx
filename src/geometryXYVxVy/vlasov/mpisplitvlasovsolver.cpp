91e2bdd8dcf19773507d6e8ba68455b1a28253e4
Emily Bourne <emily.bourne@epfl.ch>
2025-02-28 16:29:34 +0100

    Add an MPI Vlasov solver

---
+++ src/geometryXYVxVy/vlasov/mpisplitvlasovsolver.cpp
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+#include "mpisplitvlasovsolver.hpp"
+
+SplitVlasovSolver::SplitVlasovSolver(
+        IAdvectionSpatial<GeometryXYVxVy, GridX> const& advec_x,
+        IAdvectionSpatial<GeometryXYVxVy, GridY> const& advec_y,
+        IAdvectionVelocity<GeometryXYVxVy, GridVx> const& advec_vx,
+        IAdvectionVelocity<GeometryXYVxVy, GridVy> const& advec_vy,
+        MPITransposeAllToAll<X2DSplit, V2DSplit> const& transpose)
+    : m_advec_x(advec_x)
+    , m_advec_y(advec_y)
+    , m_advec_vx(advec_vx)
+    , m_advec_vy(advec_vy)
+    , m_transpose(transpose)
+{
+}
+
+DFieldSpXYVxVy SplitVlasovSolver::operator()(
+        DFieldSpXYVxVy const allfdistribu_x2Dsplit,
+        DConstFieldXY const electric_field_x,
+        DConstFieldXY const electric_field_y,
+        double const dt) const
+{
+    IdxRangeSpVxVyXY idxrange_v2Dsplit(transpose.get_local_idx_range<V2DSplit>());
+    IdxRangeXY idx_range_xy_v2Dsplit(idxrange_v2Dsplit);
+    DFieldMemSpVxVyXY const allfdistribu_v2Dsplit_alloc(idxrange_v2Dsplit);
+    DFieldSpVxVyXY const allfdistribu_v2Dsplit(allfdistribu_v2Dsplit_alloc);
+    m_advec_vx(allfdistribu_x2Dsplit, electric_field_x[idx_range_xy_v2Dsplit], dt / 2);
+    m_advec_vy(allfdistribu_x2Dsplit, electric_field_y[idx_range_xy_v2Dsplit], dt / 2);
+    transpose(allfdistribu_v2Dsplit, allfdistribu_x2Dsplit);
+    m_advec_x(allfdistribu_v2Dsplit, dt / 2);
+    m_advec_y(allfdistribu_v2Dsplit, dt);
+    m_advec_x(allfdistribu_v2Dsplit, dt / 2);
+    transpose(allfdistribu_x2Dsplit, allfdistribu_v2Dsplit);
+    m_advec_vx(allfdistribu_x2Dsplit, electric_field_x[idx_range_xy_v2Dsplit], dt / 2);
+    m_advec_vy(allfdistribu_x2Dsplit, electric_field_y[idx_range_xy_v2Dsplit], dt / 2);
+
+    return allfdistribu_x2Dsplit;
+}
---
+++ src/geometryXYVxVy/vlasov/mpisplitvlasovsolver.hpp
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "geometry.hpp"
+#include "iadvectionvx.hpp"
+#include "iadvectionx.hpp"
+#include "ivlasovsolver.hpp"
+
+/**
+ * @brief A class that solves a Vlasov equation using Strang's splitting
+ * on an MPI distributed mesh.
+ *
+ * The Vlasov equation is split between four advection equations 
+ * along the X, Y, Vx and Vy directions. The splitting involves solving 
+ * the advections in the X, Y, and Vx directions first on a time interval
+ * of length dt/2, then the Vy-direction advection on a time dt, and
+ * finally the X, Y, and Vx directions again in reverse order on dt/2.
+ */
+class MpiSplitVlasovSolver : public IVlasovSolver
+{
+    /// Advection operator in the x direction
+    IAdvectionSpatial<GeometryVxVyXY, GridX> const& m_advec_x;
+    /// Advection operator in the y direction
+    IAdvectionSpatial<GeometryVxVyXY, GridY> const& m_advec_y;
+
+    /// Advection operator in the vx direction
+    IAdvectionVelocity<GeometryXYVxVy, GridVx> const& m_advec_vx;
+    /// Advection operator in the vy direction
+    IAdvectionVelocity<GeometryXYVxVy, GridVy> const& m_advec_vy;
+
+    /// MPI transpose operator
+    MPITransposeAllToAll<X2DSplit, V2DSplit> const& m_transpose;
+
+public:
+    /**
+     * @brief Creates an instance of the split vlasov solver class.
+     * @param[in] advec_x An advection operator along the x direction.
+     * @param[in] advec_y An advection operator along the y direction.
+     * @param[in] advec_vx An advection operator along the vx direction.
+     * @param[in] advec_vy An advection operator along the vy direction.
+     */
+    SplitVlasovSolver(
+            IAdvectionSpatial<GeometryVxVyXY, GridX> const& advec_x,
+            IAdvectionSpatial<GeometryVxVyXY, GridY> const& advec_y,
+            IAdvectionVelocity<GeometryXYVxVy, GridVx> const& advec_vx,
+            IAdvectionVelocity<GeometryXYVxVy, GridVy> const& advec_vy,
+            MPITransposeAllToAll<X2DSplit, V2DSplit> const& transpose);
+
+    ~SplitVlasovSolver() override = default;
+
+    /**
+     * @brief Solves a Vlasov equation on a timestep dt.
+     *
+     * @param[in, out] allfdistribu On input : the initial value of the distribution function.
+     *                              On output : the value of the distribution function after solving 
+     *                              the Vlasov equation.
+     * @param[in] electric_field_x The electric field in the x direction computed at all spatial positions. 
+     * @param[in] electric_field_y The electric field in the y direction computed at all spatial positions. 
+     * @param[in] dt The timestep. 
+     *
+     * @return The distribution function after solving the Vlasov equation.
+     */
+    DFieldSpXYVxVy operator()(
+            DFieldSpXYVxVy allfdistribu,
+            DConstFieldXY electric_field_x,
+            DConstFieldXY electric_field_y,
+            double dt) const override;
+};
+
