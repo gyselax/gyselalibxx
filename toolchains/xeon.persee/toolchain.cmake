581a66e5712b79daae5b9b60dae1a8ede2958e29
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-28 17:23:46 +0100

    Remove DDC option based on environment variable

--- toolchains/xeon.persee/toolchain.cmake
+++ toolchains/xeon.persee/toolchain.cmake
@@ -8,7 +8,4 @@ set(CMAKE_BUILD_TYPE Release)
 set(CMAKE_CXX_FLAGS "-Wall -Wno-sign-compare -Wno-unused-but-set-variable")
 
 # Activate/deactivate parts of the code
-if (DEFINED ENV{DDC_BUILD_TESTING})
-    set(DDC_BUILD_TESTS $ENV{DDC_BUILD_TESTING})
-endif()
 set(ACTIVATE_RESTART_TESTS OFF)
