41e4aa808eb9170a377df26c892ec7f87ab190b6
Emily Bourne <emily.bourne@epfl.ch>
2025-03-06 14:27:32 +0100

    Clang formatting

--- src/data_types/tensor.hpp
+++ src/data_types/tensor.hpp
@@ -294,43 +294,53 @@ public:
         result -= val;
         return result;
     }
-        /**
+    /**
 ￼     * @brief An operator to subtract one tensor from another elementwise.
 ￼     * @param val The tensor that should be subtracted from the current tensor.
 ￼     * @return A new tensor containing the result of the subtraction.
 ￼     */
-￼    KOKKOS_FUNCTION Tensor operator-()
-￼    {
-￼        Tensor result;
-￼        for (std::size_t i(0); i < s_n_elements; ++i) {
-￼            result.m_data[i] = -m_data[i];
-￼        }
-￼        return result;
-￼    }
+    ￼ KOKKOS_FUNCTION Tensor operator-()
 ￼
-￼    /**
+    {
+        ￼ Tensor result;
+        ￼ for (std::size_t i(0); i < s_n_elements; ++i)
+        {
+            ￼ result.m_data[i] = -m_data[i];
+            ￼
+        }
+        ￼ return result;
+        ￼
+    }
+    ￼
+￼ /**
 ￼     * @brief An operator to compare one tensor to another elementwise.
 ￼     * @param o_tensor The tensor that should be compared with the current tensor.
 ￼     * @return True if the tensors are equal, false otherwise.
 ￼     */
-￼    KOKKOS_FUNCTION bool operator==(Tensor const& o_tensor) const
-￼    {
-￼        bool equal(true);
-￼        for (std::size_t i(0); i < s_n_elements; ++i) {
-￼            equal &= (m_data[i] == o_tensor.m_data[i]);
-￼        }
-￼        return equal;
-￼    }
+￼ KOKKOS_FUNCTION bool operator==(Tensor const& o_tensor) const
 ￼
-￼    /**
+    {
+        ￼ bool equal(true);
+        ￼ for (std::size_t i(0); i < s_n_elements; ++i)
+        {
+            ￼ equal &= (m_data[i] == o_tensor.m_data[i]);
+            ￼
+        }
+        ￼ return equal;
+        ￼
+    }
+    ￼
+￼ /**
 ￼     * @brief An operator to compare one tensor to another elementwise.
 ￼     * @param o_tensor The tensor that should be compared with the current tensor.
 ￼     * @return False if the tensors are equal, true otherwise.
 ￼     */
-￼    KOKKOS_FUNCTION bool operator!=(Tensor const& o_tensor) const
-￼    {
-￼        return !(*this == o_tensor);
-￼    }
+￼ KOKKOS_FUNCTION bool operator!=(Tensor const& o_tensor) const
+￼
+    {
+        ￼ return !(*this == o_tensor);
+        ￼
+    }
 
     /**
      * @brief A helper type alias to get all possible indices along a
