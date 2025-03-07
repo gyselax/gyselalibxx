ffff6f9e1a7914c1ec7f0dfc6dd4b44b9a2ec70f
Emily Bourne <emily.bourne@epfl.ch>
2025-03-07 15:39:21 +0100

    Clean up example

--- tests/data_types/static_assert_tests/tensor_mul_cov_cov.cpp
+++ tests/data_types/static_assert_tests/tensor_mul_cov_cov.cpp
@@ -18,13 +18,13 @@ TEST(TensorTest, Mul)
     using Tensor2D_C = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R_cov, Theta_cov>>;
     Tensor2D_A A;
     Tensor2D_B B;
-    ddcHelper::get<R, R>(A) = 1;
-    ddcHelper::get<R, Theta>(A) = 0;
-    ddcHelper::get<Theta, R>(A) = 2;
-    ddcHelper::get<Theta, Theta>(A) = 4;
-    ddcHelper::get<R, R_cov>(B) = 6;
-    ddcHelper::get<R, Theta_cov>(B) = 8;
-    ddcHelper::get<Theta, R_cov>(B) = 4;
-    ddcHelper::get<Theta, Theta_cov>(B) = 5;
+    ddcHelper::get<R, R_cov>(A) = 1;
+    ddcHelper::get<R, Theta_cov>(A) = 0;
+    ddcHelper::get<Theta, R_cov>(A) = 2;
+    ddcHelper::get<Theta, Theta_cov>(A) = 4;
+    ddcHelper::get<R_cov, R_cov>(B) = 6;
+    ddcHelper::get<R_cov, Theta_cov>(B) = 8;
+    ddcHelper::get<Theta_cov, R_cov>(B) = 4;
+    ddcHelper::get<Theta_cov, Theta_cov>(B) = 5;
     Tensor2D_C C = tensor_mul(index<'i', 'j'>(A), index<'j', 'k'>(B));
 }
