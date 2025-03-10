100615b11fab83dfea2bc81e2f2d37fe0d1485e5
Emily Bourne <emily.bourne@epfl.ch>
2025-03-10 18:14:03 +0100

    Remove gtest from compilation tests

--- tests/data_types/static_assert_tests/tensor_mul_3_indices.cpp
+++ tests/data_types/static_assert_tests/tensor_mul_3_indices.cpp
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 #include <ddc/ddc.hpp>
 
-#include <gtest/gtest.h>
-
 #include "../geometry_tensor.hpp"
 
 #include "indexed_tensor.hpp"
@@ -10,21 +8,11 @@
 #include "tensor_index_tools.hpp"
 #include "vector_index_tools.hpp"
 
-TEST(TensorTest, Mul)
+using Tensor2D_A = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R, Theta>>;
+using Tensor2D_B = Tensor<int, VectorIndexSet<R_cov, Theta_cov>, VectorIndexSet<R_cov, Theta_cov>>;
+using Tensor2D_C = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R_cov, Theta_cov>>;
+
+Tensor2D_C compile_tensor_test_mul(Tensor2D_A const& A, Tensor2D_B const& B)
 {
-    using Tensor2D_A = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R, Theta>>;
-    using Tensor2D_B
-            = Tensor<int, VectorIndexSet<R_cov, Theta_cov>, VectorIndexSet<R_cov, Theta_cov>>;
-    using Tensor2D_C = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R_cov, Theta_cov>>;
-    Tensor2D_A A;
-    Tensor2D_B B;
-    ddcHelper::get<R, R>(A) = 1;
-    ddcHelper::get<R, Theta>(A) = 0;
-    ddcHelper::get<Theta, R>(A) = 2;
-    ddcHelper::get<Theta, Theta>(A) = 4;
-    ddcHelper::get<R, R_cov>(B) = 6;
-    ddcHelper::get<R, Theta_cov>(B) = 8;
-    ddcHelper::get<Theta, R_cov>(B) = 4;
-    ddcHelper::get<Theta, Theta_cov>(B) = 5;
-    Tensor2D_C C = tensor_mul(index<'j', 'j'>(A), index<'j', 'k'>(B));
+    return tensor_mul(index<'j', 'j'>(A), index<'j', 'k'>(B));
 }
--- tests/data_types/static_assert_tests/tensor_mul_wrong_index.cpp
+++ tests/data_types/static_assert_tests/tensor_mul_wrong_index.cpp
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 #include <ddc/ddc.hpp>
 
-#include <gtest/gtest.h>
-
 #include "../geometry_tensor.hpp"
 
 #include "indexed_tensor.hpp"
@@ -10,10 +8,10 @@
 #include "tensor_index_tools.hpp"
 #include "vector_index_tools.hpp"
 
-TEST(TensorTest, Index)
+using Tensor2D_A = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R_cov, Theta_cov>>;
+
+void compile_tensor_test_fill(Tensor2D_A& A)
 {
-    using Tensor2D_A = Tensor<int, VectorIndexSet<R, Theta>, VectorIndexSet<R_cov, Theta_cov>>;
-    Tensor2D_A A;
     ddcHelper::get<R, X>(A) = 1;
     ddcHelper::get<R, Y>(A) = 0;
     ddcHelper::get<Theta, X>(A) = 2;
