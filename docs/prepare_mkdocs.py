85d5c3ce70a8bc3e2c83229335d1a9ee11a3899f
Emily Bourne <emily.bourne@epfl.ch>
2025-03-18 17:37:36 +0100

    Use pathlib to simplify code. Add exclude_dirs option. Call replace_math_tags_with_mkdoc_compatible_tags for valid markdown

--- docs/prepare_mkdocs.py
+++ docs/prepare_mkdocs.py
@@ -1,35 +1,31 @@
-import os
+from pathlib import Path
 import shutil
 import argparse
+import sys
+from readme_to_mkdocs import replace_math_tags_with_mkdoc_compatible_tags
 
-def copy_files(src_dir, dest_dir, extensions=(".md", ".png")):
-    # Get a list of all subdirectories
-    subdirs = [os.path.join(root) for root, _, _ in os.walk(src_dir)]
-    subdirs.append(src_dir)
-    for root in subdirs:
-        
-        # List all files in the current directory
-        files = [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))]
-        
-        # Filter files with the desired extensions
-        relevant_files = [f for f in files if f.lower().endswith(extensions)]
-        
-        if relevant_files:
+def copy_files(src_dir, dest_dir, exclude_dirs, extensions=(".md", ".png")):
+    exclude_dirs = [Path(e) for e in exclude_dirs]
+    for e in extensions:
+        for src_file in src_dir.rglob(f"*{e}"):
+            if any(e in src_file.parents for e in exclude_dirs):
+                continue
             # Determine relative path and corresponding destination
-            rel_path = os.path.relpath(root, src_dir)
-            target_dir = os.path.join(dest_dir, rel_path)
-            os.makedirs(target_dir, exist_ok=True)
-            
-            for file in relevant_files:
-                src_file = os.path.join(root, file)
-                dest_file = os.path.join(target_dir, file)
-                shutil.copy2(src_file, dest_file)
-                print(f"Copied: {src_file} -> {dest_file}")
+            dest_file = dest_dir / src_file
+
+            dest_file.parent.mkdir(parents=True, exist_ok=True)
+
+            if src_file.suffix == '.md':
+                replace_math_tags_with_mkdoc_compatible_tags(str(src_file), str(dest_file))
+            else:
+                shutil.copy2(str(src_file), str(dest_file))
+            print(f"Copied: {src_file} -> {dest_file}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Copy Markdown and PNG files while preserving directory structure.")
-    parser.add_argument("src_dir", help="Source directory to search")
-    parser.add_argument("dest_dir", help="Destination directory to copy files to")
+    parser.add_argument("src_dir", type=Path, help="Source directory to search")
+    parser.add_argument("dest_dir", type=Path, help="Destination directory to copy files to")
+    parser.add_argument("exclude_dirs", nargs='*', help="Directories that should be ignored during the copy")
     args = parser.parse_args()
     
-    copy_files(args.src_dir, args.dest_dir)
+    copy_files(args.src_dir, args.dest_dir, args.exclude_dirs)
