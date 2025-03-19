c44ebed32c876888c54c7b2fa5c08c4e04f855ba
yanmnc <yann.munschy@epfl.ch>
2025-03-19 10:57:21 +0100

    static analysis

--- tests/utils/test_ddcHelpers.cpp
+++ tests/utils/test_ddcHelpers.cpp
@@ -31,17 +31,17 @@ struct GridNonUniform : NonUniformGridBase<Tag>
 TEST(DDCHelper, UniformPeriodicRestriction)
 {
     std::size_t constexpr ncells(5);
-    using Grid = GridUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
     Coord1D x_min(-1.0);
     Coord1D x_max(1.0);
     IdxStep npoints(ncells + 1);
     double x_len = x_max - x_min;
 
-    ddc::init_discrete_space<Grid>(Grid::init(x_min, x_max, npoints));
+    ddc::init_discrete_space<Grid1D>(Grid1D::init(x_min, x_max, npoints));
 
     IdxRange idx_range(Idx(0), npoints - 1);
 
@@ -74,10 +74,10 @@ TEST(DDCHelper, UniformPeriodicRestriction)
 TEST(DDCHelper, NonUniformPeriodicRestriction)
 {
     std::size_t constexpr ncells(5);
-    using Grid = GridNonUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridNonUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
     Coord1D x_min(-1.0);
     Coord1D x_max(1.0);
@@ -90,7 +90,7 @@ TEST(DDCHelper, NonUniformPeriodicRestriction)
     }
     nu_points[5] = x_max;
 
-    ddc::init_discrete_space<Grid>(nu_points);
+    ddc::init_discrete_space<Grid1D>(nu_points);
 
     IdxRange idx_range(Idx(0), npoints - 1);
 
@@ -127,22 +127,22 @@ TEST(DDCHelper, NonUniformPeriodicRestriction)
 TEST(DDCHelper, ComputeMaxDistanceUniformGrid)
 {
     std::size_t constexpr ncells(10);
-    using Grid = GridUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
     Coord1D x_min(0.);
     Coord1D x_max(1.0);
     IdxStep npoints(ncells + 1);
     double x_len = x_max - x_min;
 
-    ddc::init_discrete_space<Grid>(Grid::init(x_min, x_max, npoints));
+    ddc::init_discrete_space<Grid1D>(Grid1D::init(x_min, x_max, npoints));
 
     IdxRange idx_range(Idx(0), npoints);
     double const max_dx(x_len / (ncells));
     double const max_dx_ddchelper(
-            ddcHelper::maximum_distance_between_adjacent_points<Grid>(idx_range));
+            ddcHelper::maximum_distance_between_adjacent_points<Grid1D>(idx_range));
 
     EXPECT_NEAR(max_dx, max_dx_ddchelper, 1e-12);
 }
@@ -154,21 +154,21 @@ TEST(DDCHelper, ComputeMaxDistanceUniformGrid)
 TEST(DDCHelper, ComputeMaxDistanceNonUniformGridFirst)
 {
     std::size_t constexpr ncells(5);
-    using Grid = GridNonUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridNonUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
 
     std::vector<double> points_list = {0., 0.9, 1., 1.1, 1.5, 2.};
-    ddc::init_discrete_space<Grid>(points_list);
+    ddc::init_discrete_space<Grid1D>(points_list);
 
     IdxStep npoints(ncells + 1);
     IdxRange idx_range(Idx(0), npoints);
 
     double const max_dx(0.9);
     double const max_dx_ddchelper(
-            ddcHelper::maximum_distance_between_adjacent_points<Grid>(idx_range));
+            ddcHelper::maximum_distance_between_adjacent_points<Grid1D>(idx_range));
 
     EXPECT_NEAR(max_dx, max_dx_ddchelper, 1e-12);
 }
@@ -180,21 +180,21 @@ TEST(DDCHelper, ComputeMaxDistanceNonUniformGridFirst)
 TEST(DDCHelper, ComputeMaxDistanceNonUniformGridMiddle)
 {
     std::size_t constexpr ncells(5);
-    using Grid = GridNonUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridNonUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
 
     std::vector<double> points_list = {0., 0.1, 1., 1.1, 1.5, 2.};
-    ddc::init_discrete_space<Grid>(points_list);
+    ddc::init_discrete_space<Grid1D>(points_list);
 
     IdxStep npoints(ncells + 1);
     IdxRange idx_range(Idx(0), npoints);
 
     double const max_dx(0.9);
     double const max_dx_ddchelper(
-            ddcHelper::maximum_distance_between_adjacent_points<Grid>(idx_range));
+            ddcHelper::maximum_distance_between_adjacent_points<Grid1D>(idx_range));
 
     EXPECT_NEAR(max_dx, max_dx_ddchelper, 1e-12);
 }
@@ -206,21 +206,21 @@ TEST(DDCHelper, ComputeMaxDistanceNonUniformGridMiddle)
 TEST(DDCHelper, ComputeMaxDistanceNonUniformGridLast)
 {
     std::size_t constexpr ncells(5);
-    using Grid = GridNonUniform<ncells>;
-    using Idx = Idx<Grid>;
-    using IdxStep = IdxStep<Grid>;
-    using IdxRange = IdxRange<Grid>;
+    using Grid1D = GridNonUniform<ncells>;
+    using Idx = Idx<Grid1D>;
+    using IdxStep = IdxStep<Grid1D>;
+    using IdxRange = IdxRange<Grid1D>;
 
 
     std::vector<double> points_list = {0., 0.3, 0.9, 1., 1.1, 2.};
-    ddc::init_discrete_space<Grid>(points_list);
+    ddc::init_discrete_space<Grid1D>(points_list);
 
     IdxStep npoints(ncells + 1);
     IdxRange idx_range(Idx(0), npoints);
 
     double const max_dx(0.9);
     double const max_dx_ddchelper(
-            ddcHelper::maximum_distance_between_adjacent_points<Grid>(idx_range));
+            ddcHelper::maximum_distance_between_adjacent_points<Grid1D>(idx_range));
 
     EXPECT_NEAR(max_dx, max_dx_ddchelper, 1e-12);
 }
