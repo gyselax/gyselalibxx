d0e990e13a91a8b6915628d19f9bffda8ed9ddf3
philipp137 <philipp.l.krah@gmail.com>
2025-03-20 08:14:16 +0100

    adapted readme_to_mkdocs for pylint

--- docs/readme_to_mkdocs.py
+++ docs/readme_to_mkdocs.py
@@ -58,38 +58,33 @@ def get_code_blocks(line):
     n_code_tags //= 2
     return [(code_tags[2*i], code_tags[2*i+1]) for i in range(n_code_tags)]
 
-def format_equations(line, start_tag, end_tag, start_replace, end_replace, contents = None, idx = None):
+def format_equations(line, tags, replacements, contents=None, idx=None):
     """
     Format equations with Doxygen style.
-
-    Use regexes to find equations. If they are not in code blocks (e.g. for
-    documentation explaining how to format equations) then replace Markdown
-    formatting with Doxygen style formatting.
 
     Parameters
     ----------
     line : str
         A line of markdown text.
 
-    start_tag : re.Pattern
-        A regex pattern matching the start of the equation.
+    tags : tuple (re.Pattern, re.Pattern)
+        A tuple containing regex patterns matching the start and end of the equation.
 
-    end_tag : re.Pattern
-        A regex pattern matching the end of the equation.
-
-    start_replace : str
-        A Doxygen-compatible string which should be used to introduce the equation.
-
-    end_replace : str
-        A Doxygen-compatible string which should be used to close the equation.
+    replacements : tuple (str, str)
+        A tuple containing Doxygen-compatible strings to introduce and close the equation.
 
     contents : list, optional
-        A list of all the lines in the file. This should be provided for multi-line
-        equations.
+        A list of all the lines in the file. Required for multi-line equations.
 
-    idx : int
-        The index of the line in the contents. This should be provided for multi-line
-        equations.
+    idx : int, optional
+        The index of the line in the contents. Required for multi-line equations.
+
+    Example
+    --------
+
+    start_tag = re.compile(r"$$")
+    end_tag = re.compile(r"$$")
+    formatted_line, new_idx = format_equations(line, (start_tag, end_tag), ("\\(", "\\)"), contents, idx)
 
     Returns
     -------
@@ -99,12 +94,12 @@ def format_equations(line, start_tag, end_tag, start_replace, end_replace, conte
     int
         The updated line index.
     """
+    start_tag, end_tag = tags
+    start_replace, end_replace = replacements
+
     code_blocks = get_code_blocks(line)
     start_match = start_tag.search(line)
-    if contents:
-        n = len(contents)
-    else:
-        n = 1
+    n = len(contents) if contents else 1
 
     while start_match:
         if not any(start < start_match.start() < end for start, end in code_blocks):
@@ -112,7 +107,7 @@ def format_equations(line, start_tag, end_tag, start_replace, end_replace, conte
             if end_match is None:
                 assert contents is not None
 
-            while end_match is None and idx < n-1:
+            while end_match is None and idx < n - 1:
                 idx += 1
                 line += '\n' + contents[idx]
                 end_match = end_tag.search(line, start_match.end())
@@ -121,11 +116,11 @@ def format_equations(line, start_tag, end_tag, start_replace, end_replace, conte
             line = line[:start_match.start()] + replacement + line[end_match.end():]
             code_blocks = get_code_blocks(line)
             start_match = start_tag.search(line, start_match.start() + len(replacement))
-
         else:
             start_match = start_tag.search(line, start_match.end())
 
     return line, idx
+
 
 def replace_math_tags_with_mkdoc_compatible_tags(in_file, out_file):
     """
@@ -165,7 +160,7 @@ def replace_math_tags_with_mkdoc_compatible_tags(in_file, out_file):
                 in_code = True
         elif not in_code and not in_math_code:
             # Replace inline math with mkdoc tag
-            line, _ = format_equations(line, single_math_start_tag, single_math_end_tag, "\\(", "\\)", contents=None, idx=None)
+            line, _ = format_equations(line, (single_math_start_tag, single_math_end_tag), ("\\(", "\\)"))
 
         body.append(line)
 
