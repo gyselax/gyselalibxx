e2ba7aa246bd9f62953ea7286ebe2cd4f84e0c77
yanmnc <yann.munschy@epfl.ch>
2025-03-25 15:00:05 +0100

    added test

--- src/math_tools/gradient.hpp
+++ src/math_tools/gradient.hpp
@@ -15,17 +15,17 @@
 template <class Mapping, class PositionCoordinate>
 class Gradient
 {
-  using MetricTensorType = MetricTensorEvaluator<Mapping, PositionCoordinate>;
+    using MetricTensorType = MetricTensorEvaluator<Mapping, PositionCoordinate>;
 
-  using ContravariantVectorType = typename MetricTensorType::ContravariantVectorType;
-  using CovariantVectorType = typename MetricTensorType::CovariantVectorType;
+    using ContravariantVectorType = typename MetricTensorType::ContravariantVectorType;
+    using CovariantVectorType = typename MetricTensorType::CovariantVectorType;
 
-  using Dims = ddc::to_type_seq_t<typename Mapping::CoordArg>;
-  using Dims_cov = vector_index_set_dual_t<Dims>;
-  using Dim0_cov = ddc::type_seq_element_t<0, Dims_cov>;
-  using Dim1_cov = ddc::type_seq_element_t<1, Dims_cov>;
+    using Dims = ddc::to_type_seq_t<typename Mapping::CoordArg>;
+    using Dims_cov = vector_index_set_dual_t<Dims>;
+    using Dim0_cov = ddc::type_seq_element_t<0, Dims_cov>;
+    using Dim1_cov = ddc::type_seq_element_t<1, Dims_cov>;
 
-  MetricTensorType const m_metric_tensor;
+    MetricTensorType const m_metric_tensor;
 
 public:
     /**
@@ -33,8 +33,7 @@ public:
      *
      * @param metric_tensor A MetricTensorEvaluator.
      */
-    explicit KOKKOS_FUNCTION Gradient(
-            MetricTensorType const metric_tensor)
+    explicit KOKKOS_FUNCTION Gradient(MetricTensorType const metric_tensor)
         : m_metric_tensor(metric_tensor)
     {
     }
@@ -45,7 +44,8 @@ public:
      *
      * @param[out] differentiated_field Contains on output the value of the differentiated field.
      */
-    KOKKOS_FUNCTION CovariantVectorType operator()(CovariantVectorType const& partial_derivatives) const
+    KOKKOS_FUNCTION CovariantVectorType
+    operator()(CovariantVectorType const& partial_derivatives) const
     {
         CovariantVectorType gradient;
         ddcHelper::get<Dim0_cov>(gradient) = ddcHelper::get<Dim0_cov>(partial_derivatives);
@@ -60,10 +60,10 @@ public:
      *
      * @param[out] differentiated_field Contains on output the value of the differentiated field.
      */
-    KOKKOS_FUNCTION ContravariantVectorType operator()(ContravariantVectorType const& partial_derivatives, PositionCoordinate const& coord) const
+    KOKKOS_FUNCTION ContravariantVectorType operator()(
+            CovariantVectorType const& partial_derivatives,
+            PositionCoordinate const& coord) const
     {
-        ContravariantVectorType gradient;
-
         return tensor_mul(
                 index<'i', 'j'>(m_metric_tensor.inverse(coord)),
                 index<'j'>(partial_derivatives));
---
+++ tests/math_tools/test_gradient.cpp
@@ -0,0 +1,148 @@
+/// Test of the metric tensor and its inverse: (singular point avoided)
+#include <ddc/kernels/splines.hpp>
+
+#include <gtest/gtest.h>
+
+#include "../mapping/geometry_mapping_tests.hpp"
+#include "../mapping/mapping_testing_tools.hpp"
+
+#include "circular_to_cartesian.hpp"
+#include "ddc_aliases.hpp"
+#include "ddc_helper.hpp"
+#include "gradient.hpp"
+#include "math_tools.hpp"
+#include "metric_tensor_evaluator.hpp"
+#include "vector_field.hpp"
+
+namespace {
+
+using DVectorType = DTensor<VectorIndexSet<R, Theta>>;
+using DVectorCovType = DTensor<VectorIndexSet<R_cov, Theta_cov>>;
+
+
+/**
+ * @brief A class that represents a scalar field.
+ * To be used to compute the gradient of the field.
+ */
+class GradientTestFunction
+{
+public:
+    /**
+     * @brief Default constructor.
+     */
+    KOKKOS_DEFAULTED_FUNCTION GradientTestFunction() = default;
+
+    /**
+     * @brief Default copy constructor.
+     */
+    KOKKOS_DEFAULTED_FUNCTION GradientTestFunction(GradientTestFunction const&) = default;
+
+    /**
+     * @brief Default destructor.
+     */
+    KOKKOS_DEFAULTED_FUNCTION ~GradientTestFunction() = default;
+
+    /**
+     * @brief Get the value of the function at given coordinate.
+     *
+     * @param[in] coord_rth The coordinate where we want to evaluate
+     * the function.
+     *
+     * @return The value of the function at the coordinate.
+     */
+    KOKKOS_FUNCTION double operator()(CoordRTheta const coord_rth) const
+    {
+        double const r = ddc::get<R>(coord_rth);
+        double const theta = ddc::get<Theta>(coord_rth);
+        return ipow(r, 2) * Kokkos::cos(theta) + 2 * r * Kokkos::sin(theta);
+    }
+
+    /**
+     * @brief Get the value of the gradient of the field 
+     * at a given coordinate.
+     *
+     * @param[in] coord_rth The coordinate where we want to evaluate 
+     * the gradient.
+     *
+     * @return A vector type representing the value of the gradient.
+     */
+    KOKKOS_INLINE_FUNCTION DVectorType gradient(CoordRTheta const coord_rth) const
+    {
+        double const r = ddc::get<R>(coord_rth);
+        double const theta = ddc::get<Theta>(coord_rth);
+        DVectorType gradient(
+                2 * r * Kokkos::cos(theta) + 2 * Kokkos::sin(theta),
+                -r * Kokkos::sin(theta) + 2 * Kokkos::cos(theta));
+
+        return gradient;
+    }
+
+    /**
+     * @brief Get the value of the partial derivatives of the field 
+     * at a given coordinate.
+     *
+     * @param[in] coord_rth The coordinate where we want to evaluate 
+     * the partial derivatives.
+     *
+     * @return A vector type representing the value of the partial derivatives.
+     */
+    KOKKOS_INLINE_FUNCTION DVectorCovType partial_derivatives(CoordRTheta const coord_rth) const
+    {
+        double const r = ddc::get<R>(coord_rth);
+        double const theta = ddc::get<Theta>(coord_rth);
+        DVectorCovType derivatives(
+                2 * r * Kokkos::cos(theta) + 2 * Kokkos::sin(theta),
+                -ipow(r, 2) * Kokkos::sin(theta) + 2 * r * Kokkos::cos(theta));
+        return derivatives;
+    }
+};
+
+
+class GradientTest : public testing::TestWithParam<std::tuple<std::size_t, std::size_t>>
+{
+};
+
+TEST(GradientTest, Circular)
+{
+    double const TOL = 1e-12;
+    CircularToCartesian<R, Theta, X, Y> const mapping;
+    FieldMemRTheta_host<CoordRTheta> coords = get_example_coords(IdxStepR(1), IdxStepTheta(1));
+    IdxRangeRTheta grid = get_idx_range(coords);
+
+    MetricTensorEvaluator<CircularToCartesian<R, Theta, X, Y>, CoordRTheta> metric_tensor(mapping);
+    Gradient<CircularToCartesian<R, Theta, X, Y>, CoordRTheta> gradient_evaluator(metric_tensor);
+
+    GradientTestFunction test_function;
+
+    // Test for each coordinates if the gradient is equal to its prediction
+    ddc::for_each(grid, [&](IdxRTheta const irtheta) {
+        CoordRTheta const coord_rth(coords(irtheta));
+        DVectorCovType partial_derivatives = test_function.partial_derivatives(coord_rth);
+
+        // contravariant expression of the gradient
+        DVectorType gradient_contra = gradient_evaluator(partial_derivatives, coord_rth);
+        DVectorType gradient_prediction_contra = test_function.gradient(coord_rth);
+
+        EXPECT_NEAR(
+                ddcHelper::get<R>(gradient_contra),
+                ddcHelper::get<R>(gradient_prediction_contra),
+                TOL);
+        EXPECT_NEAR(
+                ddcHelper::get<Theta>(gradient_contra),
+                ddcHelper::get<Theta>(gradient_prediction_contra),
+                TOL);
+
+        // covariant expression of the gradient
+        DVectorCovType gradient_cov = gradient_evaluator(partial_derivatives);
+
+        EXPECT_NEAR(
+                ddcHelper::get<R_cov>(gradient_cov),
+                ddcHelper::get<R_cov>(partial_derivatives),
+                TOL);
+        EXPECT_NEAR(
+                ddcHelper::get<Theta_cov>(gradient_cov),
+                ddcHelper::get<Theta_cov>(partial_derivatives),
+                TOL);
+    });
+}
+} // namespace
