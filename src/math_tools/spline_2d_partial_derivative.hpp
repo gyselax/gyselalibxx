6b1a7ddfa7dc54b7071099e1a31299b290b4fc61
Yann Munschy <yann.munschy@epfl.ch>
2025-03-25 11:25:35 +0100

    Update src/math_tools/spline_2d_partial_derivative.hpp
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>

--- src/math_tools/spline_2d_partial_derivative.hpp
+++ src/math_tools/spline_2d_partial_derivative.hpp
@@ -21,9 +21,9 @@ class Spline2DPartialDerivative
               DerivativeDimension>
 {
     static_assert(
-            std::is_same_v<
+            (std::is_same_v<
                     typename SplineEvaluator2D::continuous_dimension_type1,
-                    DerivativeDimension> || std::is_same_v<typename SplineEvaluator2D::continuous_dimension_type2, DerivativeDimension>);
+                    DerivativeDimension>) || (std::is_same_v<typename SplineEvaluator2D::continuous_dimension_type2, DerivativeDimension>));
 
 private:
     using base_type = IPartialDerivative<
