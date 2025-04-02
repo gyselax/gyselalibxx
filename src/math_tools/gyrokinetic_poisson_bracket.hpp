03bbd436648afa662ba7d8363aced4e0673a3740
Emily Bourne <emily.bourne@epfl.ch>
2025-04-02 15:26:44 +0200

    Docstrings and explicit

--- src/math_tools/gyrokinetic_poisson_bracket.hpp
+++ src/math_tools/gyrokinetic_poisson_bracket.hpp
@@ -3,13 +3,16 @@
 
 #include "ddc_alias_inline_functions.hpp"
 #include "ddc_aliases.hpp"
+#include "gradient.hpp"
 #include "metric_tensor_evaluator.hpp"
 #include "vector_field.hpp"
 
 
 /**
- * @brief A class which implements a gradient operator.
- * @tparam MetricTensorType A type representing a metric tensor.
+ * @brief A class which implements a gyrokinetic Poisson bracket operator.
+ * The implemented equation is:
+ * @f$ \{F, G\} = b\dot(\nabla F \cross \nabla G) @f$
+ * @tparam Mapping3D A type representing a mapping in 3 dimensions.
  */
 template <class Mapping3D>
 class GyrokineticPoissonBracket
@@ -25,12 +28,28 @@ private:
     Gradient<MetricTensorEvaluator<Mapping3D>> m_grad;
 
 public:
-    GyrokineticPoissonBracket(Mapping3D const& mapping)
+    /**
+     * @brief Build a GyrokineticPoissonBracket operator.
+     * @param mapping The mapping describing the system of coordinates on which the
+     *              expression is calculated.
+     */
+    explicit GyrokineticPoissonBracket(Mapping3D const& mapping)
         : m_mapping(mapping)
         , m_grad(MetricTensorEvaluator<Mapping3D>(mapping))
     {
     }
 
+    /**
+     * @brief Compute the gyrokinetic Poisson bracket at a given coordinate, from
+     * the partial derivatives of the two fields, and the magnetic field.
+     *
+     * @param[in] partial_derivatives_f A vector containing the partial derivatives
+     * of the scalar field f expressed at the given coordinate.
+     * @param[in] partial_derivatives_g A vector containing the partial derivatives
+     * of the scalar field g expressed at the given coordinate.
+     * @param[in] B A vector containing the magnetic field at the given coordinate.
+     * @param[in] coord The coordinate where the calculation is carried out.
+     */
     KOKKOS_INLINE_FUNCTION double operator()(
             DTensor<CovBasisSpatial> const& partial_derivatives_f,
             DTensor<CovBasisSpatial> const& partial_derivatives_g,
@@ -47,6 +66,19 @@ public:
                / m_mapping.jacobian(coord);
     }
 
+    /**
+     * @brief Compute the gyrokinetic Poisson bracket at every point on a grid from the
+     * partial derivatives of the fields f and g, and the magnetic field.
+     *
+     * @param[in] exec_space The space (CPU/GPU) where the calculation should be executed.
+     * @param[out] poisson_bracket The result of the calculation of the gyrokinetic Poisson
+     * bracket at every point on a grid.
+     * @param[in] partial_derivatives_f A vector field containing the partial derivatives
+     * of f at each point on the grid.
+     * @param[in] partial_derivatives_g A vector field containing the partial derivatives
+     * of f at each point on the grid.
+     * @param[in] B A vector field describing the magnetic field.
+     */
     template <class ExecSpace, class IdxRange, class MemorySpace>
     void operator()(
             ExecSpace exec_space,
