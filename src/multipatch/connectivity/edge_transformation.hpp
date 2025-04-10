5546142148e783fcbf59ff623578f6ef03330196
Pauline Vidal <pauline.vidal@ipp.mpg.de>
2025-04-10 18:08:40 +0200

    Change loop to replace the current_idx in the correct range .

--- src/multipatch/connectivity/edge_transformation.hpp
+++ src/multipatch/connectivity/edge_transformation.hpp
@@ -259,7 +259,7 @@ public:
                 !std::is_same_v<TargetGrid, CurrentGrid>,
                 "The types of the indices should be different");
 
-        using TargetPatch = std::conditional<std::is_same_v<TargetGrid, EdgeGrid1>, Patch1, Patch2>;
+        using TargetPatch = std::conditional_t<std::is_same_v<TargetGrid, EdgeGrid1>, Patch1, Patch2>;
 
         // Index range of CurrentIdx.
         using IdxRangeCurrent = IdxRange<CurrentGrid>;
@@ -295,8 +295,19 @@ public:
 
         // Periodicity property.
         if constexpr (CurrentGrid::continuous_dimension_type::PERIODIC) {
-            current_idx = (current_idx == current_idx_range.back()) ? current_idx_range.front()
-                                                                    : current_idx;
+            std::size_t counter = 0; 
+            while ((current_idx < current_idx_range.front()) && (counter < 10)) {
+                counter ++; 
+                current_idx += n_cells_current; 
+            }
+            counter = 0; 
+            while ((current_idx >= current_idx_range.back()) && (counter < 10)) {
+                counter ++; 
+                current_idx -= n_cells_current; 
+            }
+        } else {
+            assert(current_idx <= current_idx_range.back()); 
+            assert(current_idx >= current_idx_range.front()); 
         }
 
         if constexpr ( // Uniform case
@@ -361,7 +372,7 @@ private:
             IdxRange<TargetGrid> const& target_idx_range,
             Idx<CurrentGrid> const& current_idx) const
     {
-        using TargetPatch = std::conditional<std::is_same_v<TargetGrid, EdgeGrid1>, Patch1, Patch2>;
+        using TargetPatch = std::conditional_t<std::is_same_v<TargetGrid, EdgeGrid1>, Patch1, Patch2>;
 
         // Coordinate on the discontinuous dimension of the current grid.
         using CurrentCoord = Coord<typename CurrentGrid::continuous_dimension_type>;
