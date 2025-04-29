3bdcb3744c3f17d52f1b72be3fe7b33c12eca1fd
Emily Bourne <emily.bourne@epfl.ch>
2025-04-29 15:32:30 +0200

    Add test

--- tests/mapping/metric_tensor_evaluator.cpp
+++ tests/mapping/metric_tensor_evaluator.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 
 #include "circular_to_cartesian.hpp"
+#include "combined_mapping.hpp"
 #include "cylindrical_to_cartesian.hpp"
 #include "czarny_to_cartesian.hpp"
 #include "ddc_alias_inline_functions.hpp"
@@ -12,8 +13,8 @@
 #include "geometry_mapping_tests.hpp"
 #include "mapping_testing_tools.hpp"
 #include "metric_tensor_evaluator.hpp"
+#include "toroidal_to_cylindrical.hpp"
 #include "view.hpp"
-
 
 
 class InverseMetricTensor : public testing::TestWithParam<std::tuple<std::size_t, std::size_t>>
@@ -76,6 +77,29 @@ TEST_P(InverseMetricTensor3D, InverseMatrixCylindricalMap)
     MetricTensorEvaluator<Mapping, Coord<R, Z, Zeta>> metric_tensor(mapping);
     // Test for each coordinates if the inverse_metric_tensor is the inverse of the metric_tensor
     ddc::for_each(grid, [&](IdxRZZeta const idx) {
+        check_inverse_tensor(metric_tensor(coords(idx)), metric_tensor.inverse(coords(idx)), 1e-10);
+    });
+}
+
+TEST_P(InverseMetricTensor3D, InverseMatrixToroidalMap)
+{
+    auto const [Nrho, Ntheta, Nphi] = GetParam();
+    using Mapping2D = CircularToCartesian<Rho, Theta, R, Z>;
+    using ToroidalMapping = ToroidalToCylindrical<Mapping2D, Zeta, Phi>;
+    using CylindricalMapping = CylindricalToCartesian<R, Z, Zeta, X, Y>;
+    using Mapping = CombinedMapping<CylindricalMapping, ToroidalMapping, Coord<Rho, Theta, Phi>>;
+    Mapping2D polar_to_RZ;
+    ToroidalMapping toroidal_to_cylindrical(polar_to_RZ);
+    CylindricalMapping cylindrical_to_cartesian;
+    Mapping mapping(cylindrical_to_cartesian, toroidal_to_cylindrical);
+
+    FieldMemRhoThetaPhi_host<Coord<Rho, Theta, Phi>> coords
+            = get_example_coords(IdxStepRho(Nrho), IdxStepTheta(Ntheta), IdxStepPhi(Nphi));
+    IdxRangeRhoThetaPhi grid = get_idx_range(coords);
+
+    MetricTensorEvaluator<Mapping, Coord<Rho, Theta, Phi>> metric_tensor(mapping);
+    // Test for each coordinates if the inverse_metric_tensor is the inverse of the metric_tensor
+    ddc::for_each(grid, [&](IdxRhoThetaPhi const idx) {
         check_inverse_tensor(metric_tensor(coords(idx)), metric_tensor.inverse(coords(idx)), 1e-10);
     });
 }
