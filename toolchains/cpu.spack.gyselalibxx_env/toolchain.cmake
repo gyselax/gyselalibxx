56cef75c537944caac08a59c3f006aec9446b7f7
Emily Bourne <emily.bourne@epfl.ch>
2025-05-13 17:28:49 +0200

    More generous compiler flags. Don't enable serial

--- toolchains/cpu.spack.gyselalibxx_env/toolchain.cmake
+++ toolchains/cpu.spack.gyselalibxx_env/toolchain.cmake
@@ -5,10 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../cicd_default_toolchain.cmake)
 set(CMAKE_BUILD_TYPE Release)
 
 # Compiler options
-set(CMAKE_CXX_FLAGS "-Wall -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare")
-
-# Kokkos options
-set(Kokkos_ENABLE_SERIAL ON CACHE BOOL "Allow serial code to run" FORCE)
+set(CMAKE_CXX_FLAGS "-Wall -Wno-sign-compare")
 
 # Activate/deactivate parts of the code
 set(BUILD_BENCHMARKS ON)
