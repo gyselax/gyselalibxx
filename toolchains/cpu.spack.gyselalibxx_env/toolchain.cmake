6cd20e5c05537a94255da38efe07c580fd2c0121
Emily Bourne <louise.bourne@gmail.com>
2025-05-14 08:04:48 +0200

    Add python path specification to toolchain

--- toolchains/cpu.spack.gyselalibxx_env/toolchain.cmake
+++ toolchains/cpu.spack.gyselalibxx_env/toolchain.cmake
@@ -1,6 +1,9 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/../cicd_default_toolchain.cmake)
 
+# Specify Python version to ensure spack packages are used
+set(Python3_EXECUTABLE <SPACK_PYTHON_PATH>)
+
 # CMake options
 set(CMAKE_BUILD_TYPE Release)
 
