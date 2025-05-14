f272dd91a6a1900297be6e3a15b95f68c1c5513a
Emily Bourne <emily.bourne@epfl.ch>
2025-05-14 11:10:44 +0200

    Add constexpr identity tensor

--- src/data_types/static_tensors.hpp
+++ src/data_types/static_tensors.hpp
@@ -8,6 +8,86 @@
 
 /**
  * A class containing only static constexpr methods which describes
+ * the identity tensor.
+ * @tparam ElementType The type of the elements of the tensor (usually double/complex).
+ * @tparam ValidIndexSetRow The indices of the rows.
+ * @tparam ValidIndexSetCol The indices of the columns.
+ */
+template <class ElementType, class ValidIndexSetRow, class ValidIndexSetCol>
+class IdentityTensor
+{
+    static_assert(is_vector_index_set_v<ValidIndexSetRow>);
+    static_assert(is_vector_index_set_v<ValidIndexSetCol>);
+    static_assert(ddc::type_seq_size_v<ValidIndexSetRow> == ddc::type_seq_size_v<ValidIndexSetCol>);
+
+public:
+    /// The type of the elements of the tensor.
+    using element_type = ElementType;
+
+    /**
+     * @brief The rank of the tensor.
+     * This is equivalent to the number of indices required to
+     * access an element of the tensor.
+     *
+     * @return The rank of the tensor.
+     */
+    KOKKOS_FUNCTION static constexpr std::size_t rank()
+    {
+        return 2;
+    }
+
+    /**
+     * @brief The size of the tensor.
+     * This is the number of elements in the tensor.
+     *
+     * @return The number of elements in the tensor.
+     */
+    KOKKOS_FUNCTION static constexpr std::size_t size()
+    {
+        return ddc::type_seq_size_v<ValidIndexSetRow> * ddc::type_seq_size_v<ValidIndexSetCol>;
+    }
+
+    /// The TensorIndexSet describing the possible indices.
+    using index_set = ddc::detail::TypeSeq<ValidIndexSetRow, ValidIndexSetCol>;
+
+    /**
+     * @brief Construct an uninitialised tensor object.
+     */
+    KOKKOS_DEFAULTED_FUNCTION IdentityTensor() = default;
+
+    /**
+     * @brief Get an element of the tensor.
+     * @tparam QueryIndexTag A type describing the relevant index.
+     * @return The relevant element of the tensor.
+     */
+    template <class QueryTensorIndexElement>
+    static constexpr KOKKOS_FUNCTION ElementType get()
+    {
+        static_assert(tensor_tools::is_tensor_index_element_v<QueryTensorIndexElement>);
+        constexpr int row_index = ddc::type_seq_rank_v<
+                typename QueryTensorIndexElement::index_on_dim_t<0>,
+                ValidIndexSetRow>;
+        constexpr int col_index = ddc::type_seq_rank_v<
+                typename QueryTensorIndexElement::index_on_dim_t<1>,
+                ValidIndexSetCol>;
+        if constexpr (row_index == col_index) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @brief A helper type alias to get all possible indices along a
+     * specified dimension.
+     * @tparam Dim The dimension of interest (0 <= dim < rank()).
+     */
+    template <std::size_t dim>
+    using vector_index_set_t = std::conditional_t<dim == 0, ValidIndexSetRow, ValidIndexSetCol>;
+};
+
+/**
+ * A class containing only static constexpr methods which describes
  * the Levi-Civita tensor.
  * @tparam ElementType The type of the elements of the tensor (usually double/complex).
  * @tparam ValidIndexSet The indices that can be used along any of the dimensions of the tensor.
@@ -82,6 +162,21 @@ public:
 
 
 namespace ddcHelper {
+/**
+ * @brief A helper function to get a modifiable reference to an element of the tensor.
+ * @tparam QueryIndexTag A type describing the relevant index.
+ * @param tensor The tensor whose elements are examined.
+ * @return The relevant element of the tensor.
+ */
+template <class... QueryIndexTag, class ElementType, class ValidIndexSetRow, class ValidIndexSetCol>
+KOKKOS_INLINE_FUNCTION constexpr double get(
+        IdentityTensor<ElementType, ValidIndexSetRow, ValidIndexSetCol> const& tensor)
+{
+    return tensor.template get<tensor_tools::TensorIndexElement<
+            typename IdentityTensor<ElementType, ValidIndexSetRow, ValidIndexSetCol>::index_set,
+            QueryIndexTag...>>();
+}
+
 /**
  * @brief A helper function to get a modifiable reference to an element of the tensor.
  * @tparam QueryIndexTag A type describing the relevant index.
