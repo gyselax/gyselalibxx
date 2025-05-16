e586227fe498f98ae367845374eec73dec1f8a3c
Emily Bourne <emily.bourne@epfl.ch>
2025-05-16 18:33:27 +0200

    Add a static assert to MetricTensorEvaluator now Jacobian can be checked

--- src/mapping/metric_tensor_evaluator.hpp
+++ src/mapping/metric_tensor_evaluator.hpp
@@ -15,11 +15,16 @@
  * @tparam Mapping The mapping providing the Jacobian operator.
  * @tparam PositionCoordinate The coordinate type where the metric tensor can be evaluated.
  */
-template <class Mapping, class PositionCoordinate = typename Mapping::CoordArg>
+template <class Mapping, class PositionCoordinate = typename Mapping::CoordJacobian>
 class MetricTensorEvaluator
 {
     static_assert(is_mapping_v<Mapping>);
     static_assert(has_jacobian_v<Mapping>);
+    static_assert(
+            std::is_same_v<PositionCoordinate, typename Mapping::CoordJacobian>,
+            "The metric tensor is calculated from the Jacobian matrix. In order to evaluate the "
+            "metric tensor on a coordinate different to the one given as argument to the mapping, "
+            "please define a specialisation of this class.");
     static_assert(
             (is_covariant_vector_index_set_v<ddc::to_type_seq_t<typename Mapping::CoordResult>>)&&(
                     is_contravariant_vector_index_set_v<
