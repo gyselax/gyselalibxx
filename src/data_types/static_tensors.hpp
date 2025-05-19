c247574a736c6caba0970de07f2ff548ed0b0060
Emily Bourne <emily.bourne@epfl.ch>
2025-05-19 17:38:08 +0200

    Add a definition of the Levi-Civita tensor in general coordinates

--- src/data_types/static_tensors.hpp
+++ src/data_types/static_tensors.hpp
@@ -157,6 +157,88 @@ public:
     using vector_index_set_t = ValidIndexSet;
 };
 
+/**
+ * A class containing methods which describe the Levi-Civita tensor in general coordinates.
+ * @tparam ElementType The type of the elements of the tensor (usually double/complex).
+ * @tparam ValidIndexSet The indices that can be used along any of the dimensions of the tensor.
+ */
+template <class ElementType, class ValidIndexSet>
+class LeviCivitaTensor
+{
+    static_assert(is_vector_index_set_v<ValidIndexSet>);
+    static_assert(
+            (is_covariant_vector_index_set_v<ValidIndexSet>)
+            || (is_contravariant_vector_index_set_v<ValidIndexSet>));
+
+private:
+    double m_coeff;
+
+public:
+    /// The type of the elements of the tensor.
+    using element_type = ElementType;
+
+    /**
+     * @brief The rank of the tensor.
+     * This is equivalent to the number of indices required to
+     * access an element of the tensor.
+     *
+     * @return The rank of the tensor.
+     */
+    KOKKOS_FUNCTION static constexpr std::size_t rank()
+    {
+        return ddc::type_seq_size_v<ValidIndexSet>;
+    }
+
+    /**
+     * @brief The size of the tensor.
+     * This is the number of elements in the tensor.
+     *
+     * @return The number of elements in the tensor.
+     */
+    KOKKOS_FUNCTION static constexpr std::size_t size()
+    {
+        return rank() * rank();
+    }
+
+    /// The TensorIndexSet describing the possible indices.
+    using index_set = type_seq_duplicate_t<ValidIndexSet, rank()>;
+
+    /**
+     * @brief Construct an uninitialised tensor object.
+     */
+    KOKKOS_FUNCTION LeviCivitaTensor(double jacobian)
+    {
+        if constexpr (is_covariant_vector_index_set_v<ValidIndexSet>) {
+            m_coeff = jacobian;
+        } else {
+            m_coeff = 1.0 / jacobian;
+        }
+    }
+
+    /**
+     * @brief Get an element of the tensor.
+     * @tparam QueryIndexTag A type describing the relevant index.
+     * @return The relevant element of the tensor.
+     */
+    template <class QueryTensorIndexElement>
+    KOKKOS_INLINE_FUNCTION ElementType get() const
+    {
+        double constexpr eps = type_seq_permutation_parity_v<
+                typename QueryTensorIndexElement::IdxTypeSeq,
+                ValidIndexSet>;
+        return eps * m_coeff;
+    }
+
+    /**
+     * @brief A helper type alias to get all possible indices along a
+     * specified dimension.
+     * @tparam Dim The dimension of interest (0 <= dim < rank()).
+     */
+    template <std::size_t dim>
+    using vector_index_set_t = ValidIndexSet;
+};
+
+
 namespace ddcHelper {
 /**
  * @brief A helper function to get a modifiable reference to an element of the tensor.
@@ -185,6 +267,21 @@ KOKKOS_INLINE_FUNCTION constexpr double get(
 {
     return tensor.template get<tensor_tools::TensorIndexElement<
             typename CartesianLeviCivitaTensor<ElementType, ValidIndexSet>::index_set,
+            QueryIndexTag...>>();
+}
+
+/**
+ * @brief A helper function to get a modifiable reference to an element of the tensor.
+ * @tparam QueryIndexTag A type describing the relevant index.
+ * @param tensor The tensor whose elements are examined.
+ * @return The relevant element of the tensor.
+ */
+template <class... QueryIndexTag, class ElementType, class ValidIndexSet>
+KOKKOS_INLINE_FUNCTION constexpr double get(
+        LeviCivitaTensor<ElementType, ValidIndexSet> const& tensor)
+{
+    return tensor.template get<tensor_tools::TensorIndexElement<
+            typename LeviCivitaTensor<ElementType, ValidIndexSet>::index_set,
             QueryIndexTag...>>();
 }
 } // namespace ddcHelper
