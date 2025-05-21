4cf658f32bd7a1f073bbe839cf1ecbee00cb71a2
Emily Bourne <emily.bourne@epfl.ch>
2025-05-21 11:33:41 +0200

    GPU fix

--- src/coord_transformations/vector_mapper.hpp
+++ src/coord_transformations/vector_mapper.hpp
@@ -8,6 +8,20 @@
 #include "vector_index_tools.hpp"
 #include "view.hpp"
 
+/**
+ * @brief A helper method to get a vector on a different vector space.
+ *
+ * @param[in] mapping A mapping describing the relation between the 2 vector
+ *              spaces (or describing the relation between the vector space and
+ *              the Cartesian space if a change of variance is required.
+ * @param[in] coord The coordinate where the vector is defined.
+ * @param[in] in_vector The vector on the original vector space.
+ *
+ * @tparam OutVectorSpace The vector space of the final vector.
+ *
+ * @returns A VectorField or VectorFieldMem containing the vectors in the
+ *      requested vector space.
+ */
 template <
         class OutVectorSpace,
         class Mapping,
@@ -83,21 +97,68 @@ KOKKOS_INLINE_FUNCTION Tensor<ElementType, OutVectorSpace> to_vector_space(
 }
 
 /**
- * @brief A helper class to get a vector field on a pseudo-Cartesian geometry.
- * If the pseudo-Cartesian geometry is the same as the Cartesian geometry then
- * the same vector field is returned.
- * If the pseudo-Cartesian geometry is different then the vectors in the vector
- * field are mapped to the new geometry and a VectorFieldMem is returned.
+ * @brief A helper method to get a vector field on a different vector space.
  *
  * @param[in] exec_space The space on which the function is executed (CPU/GPU).
- * @param[in] vector_field The vector field to be mapped to the pseudo-Cartesian
- *      geometry.
- * @param[in] mapping A mapping describing the relation between the Cartesian
- *      and pseudo-Cartesian geometries.
+ * @param[in] vector_field The vector field to be mapped to the new vector space.
+ * @param[in] mapping A mapping describing the relation between the 2 vector
+ *              spaces (or describing the relation between the vector space and
+ *              the Cartesian space if a change of variance is required.
  *
  * @returns A VectorField or VectorFieldMem containing the vectors in the
- *      pseudo-Cartesian geometry.
-*/
+ *      requested vector space.
+ */
+template <
+        class OutVectorSpace,
+        class ExecSpace,
+        class Mapping,
+        class ElementType,
+        class IdxRangeType,
+        class InVectorSpace,
+        class LayoutStridedPolicy>
+void copy_to_vector_space(
+        ExecSpace exec_space,
+        VectorField<ElementType, IdxRangeType, OutVectorSpace, typename ExecSpace::memory_space>
+                vector_field_out,
+        Mapping mapping,
+        VectorConstField<
+                ElementType,
+                IdxRangeType,
+                InVectorSpace,
+                typename ExecSpace::memory_space,
+                LayoutStridedPolicy> vector_field)
+{
+    ddc::parallel_for_each(
+            exec_space,
+            get_idx_range(vector_field),
+            KOKKOS_LAMBDA(IdxType idx) {
+                IdxJacobianType coord_idx(idx);
+                CoordType coord = ddc::coordinate(coord_idx);
+                ddcHelper::assign_vector_field_element(
+                        vector_field_out,
+                        idx,
+                        to_vector_space<OutVectorSpace>(mapping, coord, vector_field(idx)));
+            });
+}
+
+/**
+ * @brief A helper method to get a vector field on a different vector space.
+ * If the requested vector space is the same as the current vector space
+ * then the same vector field is returned.
+ * If the vector space is different then the vectors in the vector
+ * field are mapped to the new vector space and a VectorFieldMem is returned.
+ *
+ * @param[in] exec_space The space on which the function is executed (CPU/GPU).
+ * @param[in] vector_field The vector field to be mapped to the new vector space.
+ * @param[in] mapping A mapping describing the relation between the 2 vector
+ *              spaces (or describing the relation between the vector space and
+ *              the Cartesian space if a change of variance is required.
+ *
+ * @tparam OutVectorSpace The vector space of the final vector.
+ *
+ * @returns A VectorField or VectorFieldMem containing the vectors in the
+ *      requested vector space.
+ */
 template <
         class OutVectorSpace,
         class ExecSpace,
@@ -127,19 +188,12 @@ auto create_mirror_view_and_copy_on_vector_space(
                 IdxRangeType,
                 OutVectorSpace,
                 typename ExecSpace::memory_space>
-                vector_field_out_alloc(get_idx_range(vector_field));
-        VectorField vector_field_out = get_field(vector_field_out_alloc);
-        ddc::parallel_for_each(
+                vector_field_out(get_idx_range(vector_field));
+        copy_to_vector_space(
                 exec_space,
-                get_idx_range(vector_field),
-                KOKKOS_LAMBDA(IdxType idx) {
-                    IdxJacobianType coord_idx(idx);
-                    CoordType coord = ddc::coordinate(coord_idx);
-                    ddcHelper::assign_vector_field_element(
-                            vector_field_out,
-                            idx,
-                            to_vector_space<OutVectorSpace>(mapping, coord, vector_field(idx)));
-                });
-        return vector_field_out_alloc;
+                get_field(vector_field_out),
+                get_const_field(vector_field),
+                mapping);
+        return vector_field_out;
     }
 }
