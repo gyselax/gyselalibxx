dc3cf800801dd8170cfa324c9c82f13d55bd7ca3
Emily Bourne <emily.bourne@epfl.ch>
2025-05-27 09:09:31 +0200

    Add static assertions for the Builder

--- src/advection/spline_polar_foot_finder.hpp
+++ src/advection/spline_polar_foot_finder.hpp
@@ -78,6 +78,30 @@ class SplinePolarFootFinder
     static_assert(ddc::in_tags_v<
                   typename SplineRThetaBuilderAdvection::interpolation_discrete_dimension_type2,
                   ddc::to_type_seq_t<IdxRangeBatched>>);
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type1::s_nbc_xmin == 0,
+            "This class is designed to work with a spline builder which does not require "
+            "additional information at the boundaries (e.g. Hermite boundary conditions require "
+            "information about the derivatives and therefore will not work with this class. Please "
+            "check the choice of boundary conditions.");
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type1::s_nbc_xmax == 0,
+            "This class is designed to work with a spline builder which does not require "
+            "additional information at the boundaries (e.g. Hermite boundary conditions require "
+            "information about the derivatives and therefore will not work with this class. Please "
+            "check the choice of boundary conditions.");
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type1::s_bc_xmin != ddc::BoundCond::PERIODIC,
+            "Periodic boundary conditions in the radial direction are nonsensical.");
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type1::s_bc_xmax != ddc::BoundCond::PERIODIC,
+            "Periodic boundary conditions in the radial direction are nonsensical.");
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type2::s_bc_xmin == ddc::BoundCond::PERIODIC,
+            "Expected periodic boundary conditions in the poloidal direction.");
+    static_assert(
+            SplineRThetaBuilderAdvection::builder_type2::s_bc_xmax == ddc::BoundCond::PERIODIC,
+            "Expected periodic boundary conditions in the poloidal direction.");
 
     using base_type = IPolarFootFinder<
             typename SplineRThetaBuilderAdvection::interpolation_discrete_dimension_type1,
