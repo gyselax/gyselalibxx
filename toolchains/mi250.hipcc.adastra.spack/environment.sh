cb03f6a315e45ee6f4058ab6b6453cd02130b6ce
Virginie Grandgirard <49058376+gdgirard@users.noreply.github.com>
2025-06-13 16:55:43 +0200

    Update environment for Adastra  (#198)
    - Create the environment for GENOA compiling and running
    
    ---
    
    Please complete the checklist to ensure that all tasks are completed
    before marking your pull request as ready for review.
    
    ### All Submissions
    
    - [ ] Have you ensured that all lines changed in this PR are justified
    by a comment found in the description ?
    - [ ] Have you updated the
    [CHANGELOG.md](https://github.com/gyselax/gyselalibxx/blob/devel/CHANGELOG.md)
    ?
    - [ ] Have you linked any issues that should be closed when this PR is
    merged (using closing keywords) ?
    - [ ] Have you checked that the AUTHORS file is up to date ?
    - [ ] Have you checked that the copyright information in the LICENCE
    file is up to date (including dates) ?
    - [ ] Do you follow the conventions specified in our [coding
    standards](https://gyselax.github.io/gyselalibxx/docs/standards/CODING_STANDARD.html)
    ?
    
    ### New Feature Submissions
    
    - [ ] Have you added tests for the new functionalities ?
    - [ ] Have you documented the new functionalities:
    - [ ] API documentation describing the available methods, when each
    should be used and how to use them ?
    - [ ] User-friendly documentation in README files (which may link to the
    API documentation).
    - [ ] If the new functionality is non-trivial to use, provide a tutorial
    or example ? (optional)
    
    ### Changes to Existing Features
    
    - [ ] Have you checked that existing tests cover all code after the
    changes ?
    - [ ] Have you checked that existing tests are still passing ?
    - [ ] Have you checked that the existing documentation is still accurate
    (API and README files) ?
    
    ### Changes to the CI
    
    - [ ] Have you made the same changes to both the GitHub CI and the
    GitLab CI (for the private fork) ?
    
    ---------
    
    Co-authored-by: gdgirard <gdgirard@login6.head.adastra.cines.fr>
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>
    Co-authored-by: geoflow <elkmine31@hotmail.fr>
    Co-authored-by: Etienne Malaboeuf <eti.malaboeuf@gmail.com>

--- toolchains/mi250.hipcc.adastra.spack/environment.sh
+++ toolchains/mi250.hipcc.adastra.spack/environment.sh
@@ -6,16 +6,17 @@ module load cpe/24.07
 # FIXME:
 # craype-accel-amd-gfx90a Error with the cray wrappers clang: error: unsupported option '-fopenmp-targets=' for language mode 'HIP'
 module load craype-x86-trento
-module load PrgEnv-cray-amd amd-mixed/6.1.2
+# NOTE: Force 6.3.3 due to startup failures (https://github.com/gyselax/gyselalibxx/pull/198#issuecomment-2943081411)
+module load PrgEnv-cray-amd amd-mixed/6.3.3
 
-module load rocm/6.1.2
+module load rocm/6.3.3
 module load cray-fftw
 module load cray-hdf5-parallel
 module load cray-python
 
-# FIXME: SPACK_USER_PREFIX path too long
-# export SPACK_USER_PREFIX="${ENVIRONMENT_ROOT_DIRECTORY}/gyselalibxx-spack-install-MI250"
-export SPACK_USER_PREFIX="${SHAREDWORKDIR}/gyselalibxx-spack-install-MI250"
+SPACK_USER_VERSION="spack-user-4.0.0"
+
+export SPACK_USER_PREFIX="${SHAREDWORKDIR}/gyselalibxx-spack-install-MI250/Configuration.${SPACK_USER_VERSION}"
 module use "${SPACK_USER_PREFIX}/modules/tcl/linux-rhel8-zen3"
 
 module load \
@@ -24,9 +25,14 @@ module load \
     gcc/13.2.1.mi250/zen3/paraconf \
     gcc/13.2.1.mi250/zen3/pdi \
     gcc/13.2.1.mi250/zen3/pdiplugin-decl-hdf5 \
+    gcc/13.2.1.mi250/zen3/pdiplugin-mpi \
     gcc/13.2.1.mi250/zen3/pdiplugin-set-value \
     gcc/13.2.1.mi250/zen3/pdiplugin-trace \
     gcc/13.2.1.mi250/zen3/ginkgo \
-    gcc/13.2.1.mi250/zen3/ninja
+    gcc/13.2.1.mi250/zen3/eigen \
+    gcc/13.2.1.mi250/zen3/ninja \
+    gcc/13.2.1.mi250/zen3/py-matplotlib \
+    gcc/13.2.1.mi250/zen3/py-xarray \
+    gcc/13.2.1.mi250/zen3/py-h5py
 
 module list
