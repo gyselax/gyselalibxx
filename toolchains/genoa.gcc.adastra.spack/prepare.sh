2cdfaabcc43093bdb62c18861f87fbfddc884c96
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2025-06-19 11:10:39 +0200

    Toolchain rework with spack env

--- toolchains/genoa.gcc.adastra.spack/prepare.sh
+++ toolchains/genoa.gcc.adastra.spack/prepare.sh
@@ -18,70 +18,37 @@ export SPACK_USER_PREFIX="${SHAREDWORKDIR}/gyselalibxx-spack-install-GENOA/Confi
 export SPACK_USER_CACHE_PATH="${SPACK_USER_PREFIX}/cache"
 
 module purge
-module load develop
 module load "${SPACK_USER_VERSION}"
 module list
 
-spack config --scope user update --yes-to-all packages
-spack config --scope user update --yes-to-all config
-spack config --scope user add 'packages:all:permissions:read:world'
-spack config --scope user add 'packages:all:permissions:write:group'
-spack config --scope user add 'concretizer:unify:true'
-# spack config --scope user add 'modules:default:tcl:hash_length:4'
+which spack
 spack debug report
 
-# Inject PDI recipes into our repos.
 mkdir -p -- "${SPACK_USER_CONFIG_PATH}/external-repositories"
+
+# Inject recent PDI recipes into our repository.
 git clone https://github.com/pdidev/spack "${SPACK_USER_CONFIG_PATH}/external-repositories/pdi.spack" || true
 cd -- "${SPACK_USER_CONFIG_PATH}/external-repositories/pdi.spack" && git fetch && git checkout 5483cfea7d2d39d654c2962114248f597b3ecf46 && cd -
-spack repo add --scope user "${SPACK_USER_CONFIG_PATH}/external-repositories/pdi.spack" || true
 
-# NOTE: A sparse checkout would be great.
 git clone https://github.com/spack/spack spack.spack || true
 cd spack.spack && git fetch && git checkout 8e7489b && cd ..
-# NOTE: We may be overriding some CINES modified recipes.
 cp -rf -- spack.spack/var/spack/repos/builtin/packages/ginkgo "${SPACK_USER_CONFIG_PATH}/local-repo/packages"
 
-# FIXME: Add a recent Ginkgo to its package.py
 sed -i '/args.append("-DGINKGO_HIP_AMDGPU={0}".format(arch_str))/a\                args.append("-DCMAKE_HIP_ARCHITECTURES={0}".format(arch_str))' "${SPACK_USER_CONFIG_PATH}/local-repo/packages/ginkgo/package.py"
+
+spack env remove --yes-to-all gyselalibxx-spack-environment
+spack env create gyselalibxx-spack-environment "${TOOLCHAIN_ROOT_DIRECTORY}/gyselalibxx-spack-environment.yaml"
+
+spack --env gyselalibxx-spack-environment repo add "${SPACK_USER_CONFIG_PATH}/external-repositories/pdi.spack"
 
 echo "Preparing the Spack environment..."
 
-# # Either do not use the source mirrors or use --no-check-signature. There is an
-# # ongoing issue a CINES to fix the issue.
-# echo "# Disabled" >"${SPACK_USER_CONFIG_PATH}/mirrors.yaml"
+spack --env gyselalibxx-spack-environment concretize --force
 
-# We use GCC as a base compiler (c/c++/fortran) and implicitly, ROCm's hipcc when the +rocm variant is specified.
-PRODUCT_SPEC_LIST="
-ninja%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-libyaml%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-paraconf%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-pdi%gcc@13.2.1.genoa+python arch=linux-rhel8-zen4
-pdiplugin-decl-hdf5%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-pdiplugin-set-value%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-pdiplugin-trace%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-pdiplugin-mpi%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-ginkgo%gcc@13.2.1.genoa+openmp~shared arch=linux-rhel8-zen4
-eigen%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-py-matplotlib%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-py-xarray%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-py-h5py%gcc@13.2.1.genoa arch=linux-rhel8-zen4
-"
-# openblas@0.3.26%gcc@12.1.generic~bignuma~consistent_fpcsr+dynamic_dispatch+fortran~ilp64+locking+pic+shared build_system=makefile symbol_suffix=none threads=none arch=linux-rhel8-zen3
-
-which spack
-
-# If we start preparing a new environment, ensure we wont get name clashes by
-# uninstalling previous products.
-# NOTE: This may fail if the product are not already installed. IMO this is a
-# bug, a rm of something that does not exists is a success not a failure.
-echo "Removing old packages (errors are expected)."
-spack uninstall --dependents --all --yes-to-all ${PRODUCT_SPEC_LIST} || true
-echo "Installing new packages (errors are NOT expected)."
-spack spec -lt ${PRODUCT_SPEC_LIST}
-for ((i = 0; i < 2; ++i)); do
-    spack install --no-check-signature --fresh ${PRODUCT_SPEC_LIST}
+for ((i = 0; i < 8; ++i)); do
+    spack --env gyselalibxx-spack-environment install --jobs 12 &
 done
 
-# Ensure we expose modules for every installed software.
-spack module tcl refresh --delete-tree --yes-to-all ${PRODUCT_SPEC_LIST}
+wait
+
+# spack --env gyselalibxx-spack-environment module tcl refresh --delete-tree --yes-to-all
--- toolchains/mi250.hipcc.adastra.spack/environment.sh
+++ toolchains/mi250.hipcc.adastra.spack/environment.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
-ENVIRONMENT_ROOT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
-
-module purge
+TOOLCHAIN_ROOT_DIRECTORY="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]:-${0}}")")"
 
 SPACK_USER_VERSION="spack-user-4.0.0"
 
 export SPACK_USER_PREFIX="${SHAREDWORKDIR}/gyselalibxx-spack-install-MI250/Configuration.${SPACK_USER_VERSION}"
-module use "${SPACK_USER_PREFIX}/modules/tcl/linux-rhel8-zen3"
+export SPACK_USER_CACHE_PATH="${SPACK_USER_PREFIX}/cache"
 
-module load \
-    cmake \
-    gcc/13.2.1.mi250/zen3/libyaml \
-    gcc/13.2.1.mi250/zen3/paraconf \
-    gcc/13.2.1.mi250/zen3/pdi \
-    gcc/13.2.1.mi250/zen3/pdiplugin-decl-hdf5 \
-    gcc/13.2.1.mi250/zen3/pdiplugin-mpi \
-    gcc/13.2.1.mi250/zen3/pdiplugin-set-value \
-    gcc/13.2.1.mi250/zen3/pdiplugin-trace \
-    gcc/13.2.1.mi250/zen3/ginkgo \
-    gcc/13.2.1.mi250/zen3/eigen \
-    gcc/13.2.1.mi250/zen3/ninja \
-    gcc/13.2.1.mi250/zen3/py-matplotlib \
-    gcc/13.2.1.mi250/zen3/py-xarray \
-    gcc/13.2.1.mi250/zen3/py-h5py
-
-unset HIPCC_COMPILE_FLAGS_APPEND
+module purge
+module load "${SPACK_USER_VERSION}"
 
 module load cpe/24.07
-# FIXME:
-# craype-accel-amd-gfx90a Error with the cray wrappers clang: error: unsupported option '-fopenmp-targets=' for language mode 'HIP'
-module load craype-x86-trento
+module load craype-x86-trento craype-accel-amd-gfx90a
 # NOTE: Force 6.3.3 due to startup failures (https://github.com/gyselax/gyselalibxx/pull/198#issuecomment-2943081411)
 module load PrgEnv-gnu-amd amd-mixed/6.3.3
 module load rocm/6.3.3
 module load cray-fftw
-module load cray-hdf5-parallel
 module load cray-python
 
 module list
+
+which spack
+spack debug report
+
+eval "$(spack env activate --prompt --sh gyselalibxx-spack-environment)"
+
+unalias despacktivate
+unset despacktivate
+function despacktivate() {
+    eval "$(spack env deactivate --sh)"
+}
