b1a63a8181a1b9a89074726bf7f839a85605bc42
Emily Bourne <emily.bourne@epfl.ch>
2025-07-14 09:05:23 +0200

    Add comments

--- bin/ci_tools/check_naming_conventions.py
+++ bin/ci_tools/check_naming_conventions.py
@@ -1,9 +1,12 @@
-import argparse
+"""
+Check that the naming conventions documented in CODING_STANDARD.md are respected.
+This file should be run using ./bin/run_cppcheck
+"""
 from pathlib import Path
 import re
 import sys
 
-import cppcheckdata
+import cppcheckdata #pylint: disable=import-error
 
 ddc_keyword_map = {'DiscreteElement': 'Idx',
                    'DiscreteVector' : 'IdxStep',
@@ -28,20 +31,16 @@ file_exceptions = [HOME_DIR / 'src' / 'utils' / 'ddc_helper.hpp',
 RE_SNAKE_CASE = re.compile("[a-z0-9]+(?:_[a-z0-9]+)*")
 RE_CAMEL_CASE = re.compile("([A-Z][a-z0-9]*)+")
 
-class FileErrInfo:
-    def __init__(self, fname):
-        self.file = fname
-        self.linenr = 0
-        self.column = None
-
 def reportError(token, msg, errorId):
+    """ Report an incorrect naming convention.
+    """
     cppcheckdata.reportError(token, 'style', msg, 'naming', errorId)
 
 def main():
     parser = cppcheckdata.ArgumentParser()
     args = parser.parse_args()
 
-    dump_files, ctu_info_files = cppcheckdata.get_files(args)
+    dump_files, _ = cppcheckdata.get_files(args)
 
     for arg in dump_files:
         if not arg.endswith('.dump') or any(f'{f}.dump' == arg for f in file_exceptions):
@@ -56,6 +55,8 @@ def main():
         #    filename = arg.removesuffix(".dump")
         #    reportError(token,
         #                f'File {filename} violates snake-case naming convention', 'varname')
+
+        # Find any use of the DDC keywords listed in ddc_keyword_map in the raw code.
         ddc_usage = [tok for tok in data.rawTokens if tok.str in ddc_keyword_map]
         for tok in ddc_usage:
             key = tok.str
@@ -64,47 +65,64 @@ def main():
                         f'Please use the new naming conventions instead of DDC names ({key}->{new_key})',
                         'DDCNames')
 
+        # Loop over configurations. A configuration is created by an ifdef
         for cfg in data.iterconfigurations():
             print(f'Checking {arg}, config {cfg.name}...')
 
-
+            # Check that variables respect naming conventions
             for var in cfg.variables:
+                # If anonymous variable then move on to next element
                 if var.nameToken is None:
                     continue
-                if var.nameToken:
-                    res = re.match(RE_SNAKE_CASE, var.nameToken.str)
-                    if not res:
-                        reportError(var.typeStartToken,
-                                    f'Variable {var.nameToken.str} violates snake-case naming convention',
-                                    'varname')
-                if (var.access is None) or var.access != 'Private':
-                    continue
 
-                if not ((var.nameToken.str.startswith('m_') and not var.isStatic) or
-                        (var.nameToken.str.startswith('s_') and var.isStatic)):
+                # Check that variables use snake case by checking against a regex
+                res = re.match(RE_SNAKE_CASE, var.nameToken.str)
+                if not res:
                     reportError(var.typeStartToken,
-                                f'Private member variable {var.nameToken.str} violates naming convention',
-                                'privateMemberVariable')
+                                f'Variable {var.nameToken.str} violates snake-case naming convention',
+                                'varname')
 
+                # For class variables check that member variables start with m_
+                # and that static variables start with s_
+                if var.access in ('Private', 'Protected', 'Public'):
+                    name = var.nameToken.str
+                    if not ((name.startswith('m_') and not var.isStatic) or
+                            (name.startswith('s_') and var.isStatic)):
+                        if var.isStatic:
+                            expected = 's_' + name
+                        else:
+                            expected = 'm_' + name
+                        reportError(var.typeStartToken,
+                                    f'Private member variable {name} violates naming convention. Expected : {expected}',
+                                    'privateMemberVariable')
+
+            # Check naming conventions for functions and classes
             for scope in cfg.scopes:
+                # If scope has no body then it is irrelevant (e.g. global scope)
                 if scope.bodyStart is None:
                     continue
+
+                # Examine functions
                 if scope.type == 'Function':
                     function = scope.function
+                    # Naming conventions do not apply to constructors and destructors
+                    # whose name cannot be chosen at will
                     if function is not None and function.type in ('Constructor', 'Destructor', 'CopyConstructor', 'MoveConstructor'):
                         continue
+                    # Add an exception for gtest names
                     if scope.className == 'SetUpTestSuite' or scope.className.startswith('__'):
                         continue
+
+                    # Check that functions use snake case by checking against a regex
                     res = re.match(RE_SNAKE_CASE, scope.className)
                     if not res:
                         reportError(scope.bodyStart,
                             f'Function {scope.className} violates snake-case naming convention',
                             'functionName')
 
+                # Examine classes and structs
                 if scope.type in ('Class', 'Struct'):
-                    function = scope.function
-                    if function is not None and function.type in ('Constructor', 'Destructor', 'CopyConstructor', 'MoveConstructor'):
-                        continue
+                    # Check that classes use camel case by checking against a regex
                     res = re.match(RE_CAMEL_CASE, scope.className)
                     if not res:
                         reportError(scope.bodyStart,
