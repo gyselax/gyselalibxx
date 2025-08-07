75d73a515d69b97c797d5283d48094fa0ae2583e
Pauline Vidal <pauline.vidal@ipp.mpg.de>
2025-08-07 16:50:22 +0200

    fix ci tes t?

--- tests/geometryRTheta/time_solver/growth_rate_test.py
+++ tests/geometryRTheta/time_solver/growth_rate_test.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     path_data_structure = Path('data_structure_RTheta.yaml')
-    folder = args.data_dir.joinpath(f"/output/").resolve()
+    folder = args.data_dir.joinpath(f"/output/")
     ds = DiskStore(folder, data_structure=path_data_structure)
 
     # Get initial data
