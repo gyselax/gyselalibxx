2c2cc1b85ca49b0bb2b187fe18aa5a96ce4b5c35
Pauline Vidal <pauline.vidal@ipp.mpg.de>
2025-08-08 09:15:29 +0200

    test different path

--- tests/geometryRTheta/time_solver/growth_rate_test.py
+++ tests/geometryRTheta/time_solver/growth_rate_test.py
@@ -27,12 +27,12 @@ if __name__ == '__main__':
 
     path_data_structure = Path('data_structure_RTheta.yaml')
     # folder = args.data_dir.joinpath("/output/")
-    folder = Path.cwd().joinpath("/output/")
+    folder = Path.cwd()/"output"
     print("path_data_structure =", path_data_structure)
     print("Path.cwd() =", Path.cwd())
     print("args.data_dir =", args.data_dir)
     print("folder =", folder)
-    data = h5py.File(folder.joinpath("/GYSELALIBXX_initstate.h5"))
+    data = h5py.File(folder/"GYSELALIBXX_initstate.h5")
     print ("keys:", list(data.keys()))
 
     ds = DiskStore(folder, data_structure=path_data_structure)
