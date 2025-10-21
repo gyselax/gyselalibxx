b24e3c46c68bfb69a7f6f4c28f5cbf247680683e
Emily Bourne <emily.bourne@epfl.ch>
2025-10-21 11:27:10 +0200

    Revert changes

--- toolchains/cpu.spack.gyselalibxx_env/prepare.sh
+++ toolchains/cpu.spack.gyselalibxx_env/prepare.sh
@@ -39,29 +39,6 @@ wget https://raw.githubusercontent.com/spack/spack/b369d8b2509794c4f46f62c81f25c
 # Activate spack
 . ${SPACK_PATH}/share/spack/setup-env.sh
 
-if command -v spack >/dev/null 2>&1
-then
-    INSTALL_PYTHON=1
-else
-    if python3 -c "import sys; assert sys.version_info<(3,13)" >/dev/null 2>&1
-    then
-        INSTALL_PYTHON=0
-    else
-        INSTALL_PYTHON=0
-    fi
-fi
-
-echo "Need to install Python : ${INSTALL_PYTHON}"
-
-if [ ${INSTALL_PYTHON} ]
-then
-    # Build a known working Python (Spack-managed, no system dependency)
-    spack install python@3.11
-
-    # Export SPACK_PYTHON to point to that interpreter
-    export SPACK_PYTHON=$(spack location -i python@3.11)
-fi
-
 # Reduce the naming scheme of packages to avoid shebang issues.
 # Increase the time out that is by default too short for some packages (like PDI)
 spack config --scope site add 'config:connect_timeout:60'
@@ -69,12 +46,6 @@ spack config --scope site add 'config:connect_timeout:60'
 # Restrict `blas` and `lapack` providers to avoid `amdlibflame` and `amdblis`, for some reason they might get selected and fail to compile.
 spack config --scope site add 'packages:all:providers:blas:[openblas]'
 spack config --scope site add 'packages:all:providers:lapack:[openblas]'
-
-spack config get bootstrap
-spack config add 'bootstrap:spec:python@3.11'
-spack config get bootstrap
-spack --debug bootstrap now
-spack config get bootstrap
 
 # Add patched recipes
 git clone --branch releases/v0.23 https://github.com/gyselax/spack spack-0.23.1/var/spack/repos/gyselalibxx
@@ -87,25 +58,23 @@ spack repo add --scope site spack-0.23.1/var/spack/repos/pdi
 spack compiler find
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    COMPILER='apple-clang@14:'
+  COMPILER='apple-clang@14:'
 else
-    AVAILABLE_COMPILERS=$(spack compilers | grep "gcc@1[1-9]" || true)
+  AVAILABLE_COMPILERS=$(spack compilers | grep "gcc@1[1-9]" || true)
 
-    if [ -z "${AVAILABLE_COMPILERS}" ]
-    then
-        echo "A gcc compiler with a version of at least 11 was not found. Installing compiler"
-        spack install gcc@11
-        spack load gcc@11
-        spack compiler find
-    fi
+  if [ -z "${AVAILABLE_COMPILERS}" ]
+  then
+      echo "A gcc compiler with a version of at least 11 was not found. Installing compiler"
+      spack install gcc@11
+      spack load gcc@11
+      spack compiler find
+  fi
 
-    COMPILER='gcc@11:'
+  COMPILER='gcc@11:'
 fi
 
 spack env create gyselalibxx-env ${SCRIPT_DIR}/gyselalibxx-env-0.23.1.yaml
-echo "Created"
 spack --env gyselalibxx-env add --list-name compilers ${COMPILER}
-echo "Added compiler"
 spack --env gyselalibxx-env install --jobs 2
 spack env activate -p gyselalibxx-env
 PYTHON_EXECUTABLE=$(which python3)
