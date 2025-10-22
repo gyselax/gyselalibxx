b731689a4db860a0b93517fcf0b00b8a0316ba35
Emily Bourne <emily.bourne@epfl.ch>
2025-10-22 10:13:52 +0200

    Ensure group matches

--- toolchains/docker.gyselalibxx_env/environment.sh
+++ toolchains/docker.gyselalibxx_env/environment.sh
@@ -13,5 +13,5 @@ gyselalibxx_folder=$(realpath ${current_folder}/../..)
 kokkos_openmp_cmake_prefixes="/opt/openmp/kokkos/lib/cmake/Kokkos/:/opt/openmp/kokkos-kernels/lib/cmake/KokkosKernels/:/opt/openmp/kokkos-tools/lib/cmake/KokkosTools/:/opt/openmp/kokkos-fft/lib/cmake/KokkosFFT/"
 
 docker pull ghcr.io/gyselax/gyselalibxx_env:latest
-docker run -v ${gyselalibxx_folder}:/src --workdir "/src" -e CMAKE_PREFIX_PATH=${kokkos_openmp_cmake_prefixes} -it ghcr.io/gyselax/gyselalibxx_env:latest
+docker run -v ${gyselalibxx_folder}:/src --workdir "/src" --user :$(id -g) -e CMAKE_PREFIX_PATH=${kokkos_openmp_cmake_prefixes} -it ghcr.io/gyselax/gyselalibxx_env:latest
 
