4c78ea4c94dcea0835a0dbc0448197fbe0b1e9be
Emily Bourne <emily.bourne@epfl.ch>
2025-10-22 10:29:08 +0200

    Simplify CMAKE_PREFIX_PATH

--- toolchains/docker.gyselalibxx_env/environment.sh
+++ toolchains/docker.gyselalibxx_env/environment.sh
@@ -10,7 +10,7 @@ fi
 current_folder=$(realpath $(dirname ${BASH_SOURCE[0]}))
 gyselalibxx_folder=$(realpath ${current_folder}/../..)
 
-kokkos_openmp_cmake_prefixes="/opt/openmp/kokkos/lib/cmake/Kokkos/:/opt/openmp/kokkos-kernels/lib/cmake/KokkosKernels/:/opt/openmp/kokkos-tools/lib/cmake/KokkosTools/:/opt/openmp/kokkos-fft/lib/cmake/KokkosFFT/"
+kokkos_openmp_cmake_prefixes="/opt/openmp/kokkos/:/opt/openmp/kokkos-kernels/:/opt/openmp/kokkos-tools/:/opt/openmp/kokkos-fft/"
 
 docker pull ghcr.io/gyselax/gyselalibxx_env:latest
 docker run -v ${gyselalibxx_folder}:/src --workdir "/src" --user :$(id -g) -e CMAKE_PREFIX_PATH=${kokkos_openmp_cmake_prefixes} -it ghcr.io/gyselax/gyselalibxx_env:latest
