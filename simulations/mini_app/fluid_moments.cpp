77f91b67e4887ea769e3706b41bb81c11962b954
Philipp <philipp.l.krah@gmail.com>
2025-12-22 11:51:09 +0100

    Update simulations/mini_app/fluid_moments.cpp
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>

--- simulations/mini_app/fluid_moments.cpp
+++ simulations/mini_app/fluid_moments.cpp
@@ -7,7 +7,7 @@
 #include "trapezoid_quadrature.hpp"
 
 FluidMoments::FluidMoments(
-        Quadrature<IdxRangeVparMu, IdxRangeSpTor3DV2D, Kokkos::DefaultExecutionSpace::memory_space>
+        Quadrature<IdxRangeVparMu, IdxRangeSpTor3DV2D, Kokkos::DefaultExecutionSpace::memory_space> const&
                 integrate_vparmu)
     : m_integrate_vparmu(integrate_vparmu)
 {
