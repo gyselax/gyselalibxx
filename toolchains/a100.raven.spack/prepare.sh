9b9b6c66f4e8f9c5e4ce53414193dfb35c078b66
Yingzhe Li <yingzhe.li@ipp.mpg.de>
2026-01-21 15:32:18 +0100

    files used on 19.1.2026

--- toolchains/a100.raven.spack/prepare.sh
+++ toolchains/a100.raven.spack/prepare.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+
+export ALL_CCFRWORK=$HOME
+export ALL_CCFRSCRATCH=/ptmp/$USER
+mkdir -p $ALL_CCFRSCRATCH/pycache
+export PYTHONPYCACHEPREFIX=$ALL_CCFRSCRATCH/pycache
+
+
+
 # Ensures the script is not being sourced
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
     echo "This script must be executed, not sourced!" >&2
     return 1
 fi
 
-# Gets the slurm account to use for compiling on a compute node
-if [ $# -ne 1 ]; then
-    echo usage: ${0} a100_eaven_slurm_account
-    exit 1
-fi
-slurm_account=${1}
-
 TOOLCHAIN_ROOT_DIRECTORY="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]:-${0}}")")"
-CPUS_PER_GPU=24
 
 set -eu
 
@@ -35,21 +35,7 @@ git clone --branch v1.1.0 --depth 1 https://github.com/spack/spack.git $ALL_CCFR
 spack env remove --yes-to-all gyselalibxx-spack-environment
 spack env create gyselalibxx-spack-environment "${TOOLCHAIN_ROOT_DIRECTORY}/gyselalibxx-spack-environment.yaml"
 
-# Bootstrap must happen before getting on a compute node
-spack bootstrap now
-
 echo "Preparing the Spack environment..."
 
 # Concretize on the compute node
-GPUS=1
-CPUS_PER_TASK=$((${GPUS} * ${CPUS_PER_GPU}))
-srun --account=${slurm_account} --constraint=a100 --ntasks=1 --gres=gpu:${GPUS} --cpus-per-task=${CPUS_PER_TASK} --time=02:00:00 --qos=qos_gpu_a100-dev spack --env gyselalibxx-spack-environment concretize --force --jobs ${CPUS_PER_TASK}
-
-# Get sources on the login node
-spack --env gyselalibxx-spack-environment mirror create --all --directory ${SPACK_USER_CONFIG_PATH}/mirror
-spack --env gyselalibxx-spack-environment mirror add local_filesystem ${SPACK_USER_CONFIG_PATH}/mirror
-
-# Install on the compute node
-GPUS=3
-CPUS_PER_TASK=$((${GPUS} * ${CPUS_PER_GPU}))
-srun --account=${slurm_account} --constraint=a100 --ntasks=1 --gres=gpu:${GPUS} --cpus-per-task=${CPUS_PER_TASK} --time=02:00:00 --qos=qos_gpu_a100-dev spack --env gyselalibxx-spack-environment install --concurrent-packages ${GPUS} --jobs ${CPUS_PER_GPU}
+spack --env gyselalibxx-spack-environment install --concurrent-packages 2 --jobs 8
