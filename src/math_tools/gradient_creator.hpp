ebcf54b9e6d8d400f906f82780f44d046d502a58
Emily Bourne <emily.bourne@epfl.ch>
2026-03-23 13:18:02 +0100

    Docs

--- src/math_tools/gradient_creator.hpp
+++ src/math_tools/gradient_creator.hpp
@@ -7,7 +7,12 @@
 #include "vector_mapper.hpp"
 
 /**
+ * @brief Operator to calculate the gradient of a function.
  *
+ * @tparam IdxRangeFull The index range on which the function being differentiated
+ * should be defined.
+ * @tparam DerivativeDims The dimensions along which the function should be
+ * differentiated to construct the gradient.
  */
 template <typename IdxRangeFull, typename... DerivativeDims>
 class GradientCreator
@@ -20,6 +25,11 @@ private:
             m_derivative_creators;
 
 public:
+    /**
+     * @brief Create a GradientCreator.
+     * @param[in] partial_derivative_operator The operators used to calculate the
+     * derivative along each of the dimensions.
+     */
     GradientCreator(IPartialDerivativeCreator<
                     IdxRangeFull,
                     DerivativeDims> const&... partial_derivative_operator)
