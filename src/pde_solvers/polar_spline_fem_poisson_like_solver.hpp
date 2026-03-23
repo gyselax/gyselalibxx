accfe66018b94e7a3470ca4797de6326f3e777c9
Emily Bourne <emily.bourne@epfl.ch>
2026-03-23 13:31:20 +0100

    Fix compilation

--- src/pde_solvers/polar_spline_fem_poisson_like_solver.hpp
+++ src/pde_solvers/polar_spline_fem_poisson_like_solver.hpp
@@ -339,6 +339,7 @@ public:
 
         m_assembler.setup_sparse_matrix(
                 m_gko_matrix,
+                IdxRange<>(),
                 max_iter,
                 res_tol,
                 batch_solver_logger,
