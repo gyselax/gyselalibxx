4a0035b66250f94aaf91794669f3905d0cedcd72
Emily Bourne <emily.bourne@epfl.ch>
2026-03-24 13:27:48 +0100

    Clean up usage

--- src/interpolation/nd_lagrange_evaluator.hpp
+++ src/interpolation/nd_lagrange_evaluator.hpp
@@ -50,14 +50,12 @@ class NDLagrangeEvaluator
                      typename HeadEvaluator::data_type,
                      typename Evaluators1D::data_type> && ...));
 
+private:
     // Get the type of the (N-1)D LagrangeEvaluator
     using nd_minus_1_evaluator_type = std::conditional_t<
             (sizeof...(Evaluators1D) > 1),
             NDLagrangeEvaluator<Evaluators1D...>,
             ddc::type_seq_element_t<0, ddc::detail::TypeSeq<Evaluators1D...>>>;
-
-    HeadEvaluator m_head_evaluator;
-    nd_minus_1_evaluator_type m_tail_evaluator;
 
     using HeadBasis = typename HeadEvaluator::lagrange_basis_type;
     using HeadCoeffGrid = typename HeadEvaluator::coeff_grid_type;
@@ -65,6 +63,10 @@ class NDLagrangeEvaluator
 
     // Extract the InterpolationGrid from evaluation_idx_range_type = IdxRange<Grid>.
     using IdxRangeHeadEval = typename HeadEvaluator::evaluation_idx_range_type;
+
+private:
+    HeadEvaluator m_head_evaluator;
+    nd_minus_1_evaluator_type m_tail_evaluator;
 
 public:
     /// @brief The type of the Kokkos execution space.
@@ -107,8 +109,7 @@ public:
                     ddc::to_type_seq_t<evaluation_idx_range_type>>>;
 
     /// @brief The type of the ND Lagrange domain corresponding to the dimensions of interest.
-    using coeff_idx_range_type
-            = IdxRange<HeadCoeffGrid, typename Evaluators1D::coeff_grid_type...>;
+    using coeff_idx_range_type = IdxRange<HeadCoeffGrid, typename Evaluators1D::coeff_grid_type...>;
 
     /**
      * @brief The type of the whole Lagrange domain (cartesian product of ND Lagrange domain
@@ -132,9 +133,7 @@ public:
      * @param head_ev   The evaluator for the first dimension.
      * @param tail_evs  The evaluators for the remaining dimensions.
      */
-    explicit NDLagrangeEvaluator(
-            HeadEvaluator const& head_ev,
-            Evaluators1D const&... tail_evs)
+    explicit NDLagrangeEvaluator(HeadEvaluator const& head_ev, Evaluators1D const&... tail_evs)
         : m_head_evaluator(head_ev)
         , m_tail_evaluator(tail_evs...)
     {
@@ -175,13 +174,13 @@ public:
             ConstField<data_type, NdCoeffIdxRange, memory_space, Layout> const coeff) const
     {
         Coord<HeadContDim> coord_head(coord);
-        Idx<HeadCoeffGrid> first_lagrange_knot = find_stencil(coord_head);
+        Idx<HeadCoeffGrid> first_lagrange_knot = m_head_evaluator.find_stencil(coord_head);
 
         // Evaluate the Lagrange basis functions at the (adjusted) head coordinate.
         std::array<data_type, HeadBasis::degree() + 1> vals_ptr;
-        Kokkos::mdspan<data_type, Kokkos::extents<std::size_t, HeadBasis::degree() + 1>> const
-                vals(vals_ptr.data());
-        ddc::discrete_space<HeadBasis>().eval_basis(vals, coord_head_adj, first_lagrange_knot);
+        Kokkos::mdspan<data_type, Kokkos::extents<std::size_t, HeadBasis::degree() + 1>> const vals(
+                vals_ptr.data());
+        ddc::discrete_space<HeadBasis>().eval_basis(vals, coord_head, first_lagrange_knot);
 
         // Tensor-product recursion: for each stencil knot, slice the coefficient
         // array along the head dimension and delegate to the (N-1)D tail evaluator.
@@ -189,8 +188,7 @@ public:
         // and operator() applies its configured extrapolation rules.
         data_type result = 0.;
         for (std::size_t i = 0; i < HeadBasis::degree() + 1; ++i) {
-            Idx<HeadCoeffGrid> const knot_i
-                    = first_lagrange_knot + IdxStep<HeadCoeffGrid>(i);
+            Idx<HeadCoeffGrid> const knot_i = first_lagrange_knot + IdxStep<HeadCoeffGrid>(i);
             result += vals[i] * m_tail_evaluator(coord, coeff[knot_i]);
         }
         return result;
