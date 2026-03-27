b2359ccdcbabf57f4306a4d7422832726f363b9d
Emily Bourne <emily.bourne@epfl.ch>
2026-03-27 18:28:24 +0100

    Remove assert (difficult without tolerance)

--- src/interpolation/lagrange_evaluator.hpp
+++ src/interpolation/lagrange_evaluator.hpp
@@ -519,7 +519,6 @@ private:
                 }
             }
         }
-        KOKKOS_ASSERT(ddc::coordinate(first_lagrange_knot + lagrange_basis_type::degree()) > coord);
         return first_lagrange_knot;
     }
 
