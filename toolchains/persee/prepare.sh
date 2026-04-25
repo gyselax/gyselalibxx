634a658fb2055c8866b435c898d4cd5e8fe698db
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-04-25 23:28:01 +0200

    Always fresh

--- toolchains/persee/prepare.sh
+++ toolchains/persee/prepare.sh
@@ -45,12 +45,12 @@ echo "Preparing the Spack environment..."
 
 spack --env gyselalibxx-env-omp-cuda external find cuda
 spack --env gyselalibxx-env-omp-cuda repo update
-spack --env gyselalibxx-env-omp-cuda concretize --fresh --force
+spack --env gyselalibxx-env-omp-cuda concretize --force
 spack --env gyselalibxx-env-omp-cuda install --concurrent-packages 2 --jobs 16
 
 spack env remove --yes-to-all gyselalibxx-env-omp
 spack env create gyselalibxx-env-omp "${TOOLCHAIN_ROOT_DIRECTORY}/xeon/gyselalibxx-spack-environment.yaml"
 
 spack --env gyselalibxx-env-omp repo update
-spack --env gyselalibxx-env-omp concretize --fresh --force
+spack --env gyselalibxx-env-omp concretize --force
 spack --env gyselalibxx-env-omp install --concurrent-packages 2 --jobs 16
