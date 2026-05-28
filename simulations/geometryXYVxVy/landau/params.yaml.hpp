591c86d25e0a68dd3c437024db4ec544992e2e7d
Emily Bourne <emily.bourne@epfl.ch>
2026-05-28 09:37:23 +0200

    Use the same number of points for spline and Lagrange

--- simulations/geometryXYVxVy/landau/params.yaml.hpp
+++ simulations/geometryXYVxVy/landau/params.yaml.hpp
@@ -2,41 +2,13 @@
 
 #pragma once
 
+constexpr char const* const params_yaml =
 #ifdef SPLINE
-constexpr char const* const params_yaml = R"PDI_CFG(SplineMesh:
-  x_min: 0.0
-  x_max: 12.56637061435917
-  x_ncells: 32
-  y_min: 0.0
-  y_max: 12.56637061435917
-  y_ncells : 32
-  vx_min: -6.0
-  vx_max: +6.0
-  vx_ncells: 33
-  vy_min: -6.0
-  vy_max: +6.0
-  vy_ncells: 33
-
-SpeciesInfo:
-- charge: -1.
-  mass: 0.0005
-  density_eq: 1.
-  temperature_eq: 1.
-  mean_velocity_eq: 0.
-  perturb_amplitude: 0.05
-  perturb_mode: 1
-
-Algorithm:
-  deltat: 0.12
-  nbiter: 140
-
-Output:
-  time_diag: 0.24
-)PDI_CFG";
-
+        "SplineMesh:"
 #elif defined(LAGRANGE)
-
-constexpr char const* const params_yaml = R"PDI_CFG(Mesh:
+        "Mesh:"
+#endif
+        R"PDI_CFG(
   x_min: 0.0
   x_max: 12.56637061435917
   x_ncells: 32
@@ -66,5 +38,3 @@ Algorithm:
 Output:
   time_diag: 0.24
 )PDI_CFG";
-
-#endif
