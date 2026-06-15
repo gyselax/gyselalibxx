6c0b495e3442068d4b9acdd93c7539e41e9168cf
Emily Bourne <emily.bourne@epfl.ch>
2026-06-15 09:46:28 +0200

    Fix typo

--- tests/geometryRTheta/advection_rtheta/advection_selected_test.cpp
+++ tests/geometryRTheta/advection_rtheta/advection_selected_test.cpp
@@ -289,19 +289,19 @@ int main(int argc, char** argv)
     }
 
 #if defined(CIRCULAR_MAPPING_PHYSICAL) || defined(CZARNY_MAPPING_PHYSICAL)
-    constexpr FootFindingSpace FFSpace = FootFindingSpace::PHYSICAL,
+    constexpr FootFindingSpace FFSpace = FootFindingSpace::PHYSICAL;
 
 #elif defined(CZARNY_MAPPING_PSEUDO_CARTESIAN) || defined(DISCRETE_MAPPING_PSEUDO_CARTESIAN)
     constexpr FootFindingSpace FFSpace = FootFindingSpace::PSEUDO_PHYSICAL;
 #endif
 
-                               PolarFootFinder const foot_finder
-                               = make_polar_foot_finder<FFSpace, AdvectionFieldSpace::PHYSICAL>(
-                                       time_stepper,
-                                       to_physical_mapping,
-                                       grid,
-                                       builder,
-                                       spline_evaluator_extrapol);
+    PolarFootFinder const foot_finder
+            = make_polar_foot_finder<FFSpace, AdvectionFieldSpace::PHYSICAL>(
+                    time_stepper,
+                    to_physical_mapping,
+                    grid,
+                    builder,
+                    spline_evaluator_extrapol);
 
     BslAdvectionPolar
             advection_operator(builder, spline_evaluator, foot_finder, to_physical_mapping);
