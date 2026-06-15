68ea6306be94d910fa431f1c90cadbe1f4710557
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-06-15 11:31:24 +0200

    Remove default O1 optimisation flag (#688)

--- toolchains/common_toolchains/ci_coverage_toolchain.cmake
+++ toolchains/common_toolchains/ci_coverage_toolchain.cmake
@@ -7,8 +7,5 @@ set(CMAKE_BUILD_TYPE Debug)
 # Compiler options
 set(CMAKE_CXX_FLAGS_INIT "-Og --coverage -fprofile-update=atomic")
 
-# Gyselalibxx options
-set(GYSELALIBXX_DEFAULT_CXX_FLAGS "" CACHE STRING "Default flags for C++ specific to Gyselalib++" FORCE)
-
 # Activate/deactivate parts of the code
 set(GYSELALIBXX_ACTIVATE_RESTART_TESTS ON)
--- toolchains/common_toolchains/debug_toolchain.cmake
+++ toolchains/common_toolchains/debug_toolchain.cmake
@@ -5,7 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/importable_defaults.cmake)
 set(CMAKE_BUILD_TYPE Debug)
 
 # Compiler options
-set(CMAKE_CXX_FLAGS_INIT "-Wall -Werror=parentheses -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare")
+set(CMAKE_CXX_FLAGS_INIT "-O1 -Wall -Werror=parentheses -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare")
 
 # Activate/deactivate parts of the code
 set(GYSELALIBXX_ACTIVATE_RESTART_TESTS ON)
