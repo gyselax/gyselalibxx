67f6eaab55bbfe86f6850216edab04c7ed3ff9ce
Emily Bourne <emily.bourne@epfl.ch>
2026-07-03 13:03:18 +0200

    Docs

--- src/pde_solvers/gmg_polar_poisson_like_solver.hpp
+++ src/pde_solvers/gmg_polar_poisson_like_solver.hpp
@@ -196,6 +196,9 @@ public:
      * @param[in] to_physical The mapping from the logical to the physical domain.
      * @param[in] builder A builder to construct the coefficients of the interpolation.
      * @param[in] evaluator The evaluator for the interpolation.
+     * @param[in] max_iterations The maximum number of iterations that the solver should carry out.
+     * @param[in] absTol The absolute tolerance for the convergence of the solver.
+     * @param[in] relTol The relative tolerance for the convergence of the solver.
      */
     GMGPolarPoissonLikeSolver(
             ToPhysicalMapping to_physical,
