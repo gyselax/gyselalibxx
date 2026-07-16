24fe42f698a8bb4dd0714355648f1dfd9787c27d
Emily Bourne <emily.bourne@epfl.ch>
2026-07-16 14:11:45 +0200

    Docs

--- src/advection/bsl_advection_1d.hpp
+++ src/advection/bsl_advection_1d.hpp
@@ -297,6 +297,7 @@ public:
         FunctionBasisFieldMem function_coefs_alloc(
                 "function_coefs (BslAdvection1D::operator())",
                 batched_basis_idx_range(m_function_builder, idx_range_function));
+        // Use Kokkos view constructor to ensure correct layout
         Field<DataType, IdxRangeBSAdvection, MemSpace, FDistribLayout> function_coefs(
                 function_coefs_alloc.allocation_kokkos_view(),
                 get_idx_range(function_coefs_alloc));
