ad8b7bdb13b2ad9d8d6036d84e9b8ee91edf2ff3
BOURNE Emily EPFL <emily.bourne@epfl.ch>
2026-09-08 14:41:39 +0200

    Evaluate NDLagrange pointwise on correct execution space

--- tests/interpolation/nd_lagrange_interpolation.cpp
+++ tests/interpolation/nd_lagrange_interpolation.cpp
@@ -149,6 +149,22 @@ void fill_cos_2d(Field<DataType, IdxRange<GridX, GridY>> vals)
             });
 }
 
+template <class EvalType, class DataType, class IdxRangeCoeff, class... Dims>
+DataType get_pointwise_eval(
+        EvalType eval,
+        Coord<Dims...> coord,
+        ConstField<DataType, IdxRangeCoeff> coeffs)
+{
+    static_assert(Kokkos::SpaceAccessibility<Kokkos::DefaultExecutionSpace, typename EvalType::memory_space>::accessible);
+    double val = 0.0;
+    Kokkos::parallel_reduce(
+            "NDLagrange pointwise",
+            1,
+            KOKKOS_LAMBDA(const int& i, double& val_loc) { val_loc += eval(coord, coeffs); },
+            val);
+    return val;
+}
+
 } // namespace
 
 TYPED_TEST_SUITE(NDLagrangeNonPeriodicFixture, Cases);
@@ -546,10 +562,13 @@ TYPED_TEST(NDLagrangePeriodicFixture, PeriodicWraparound)
 
     Idx<TestGridX, TestGridY> test_idx(test_range.front() + IdxStep<TestGridX, TestGridY>(1, 2));
     Idx<TestGridX, TestGridY> test_periodic_idx(test_idx + IdxStep<TestGridX>(ntest - 1));
-    EXPECT_NEAR(
-            eval2d(ddc::coordinate(test_periodic_idx), get_const_field(coeffs_alloc)),
-            result_host(test_idx),
-            tol);
+
+    double out_of_bounds_val = get_pointwise_eval(
+            eval2d,
+            ddc::coordinate(test_periodic_idx),
+            get_const_field(coeffs_alloc));
+
+    EXPECT_NEAR(out_of_bounds_val, result_host(test_idx), tol);
 }
 
 /**
