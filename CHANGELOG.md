cef8fe6c5d3d5ff579b48a01f0e211907a3bfd1b
Emily Bourne <emily.bourne@epfl.ch>
2026-09-10 15:13:31 +0200

    Select the minimum fast math flags to eliminate '+ x*0' expressions from generated code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
-- Add `-ffast-math` flag to toolchains.
+- Add `-fno-signed-zeros -ffinite-math-only` flags to toolchains.
 
 ### Fixed
 
--- toolchains/common_toolchains/release_toolchain.cmake
+++ toolchains/common_toolchains/release_toolchain.cmake
@@ -5,7 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/importable_defaults.cmake)
 set(CMAKE_BUILD_TYPE Release)
 
 # Compiler options
-set(CMAKE_CXX_FLAGS_INIT "-Wall -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare -ffast-math")
+set(CMAKE_CXX_FLAGS_INIT "-Wall -Werror=unused-local-typedefs -Werror=unused-variable -Wno-sign-compare -fno-signed-zeros -ffinite-math-only")
 
 # Activate/deactivate parts of the code
 set(GYSELALIBXX_ACTIVATE_RESTART_TESTS OFF)
