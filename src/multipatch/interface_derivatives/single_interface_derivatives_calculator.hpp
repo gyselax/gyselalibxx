d74a052c93e4b5cf855722dc3c9107de81f47524
Pauline Vidal <pauline.vidal@ipp.mpg.de>
2026-09-23 11:18:20 +0200

    remove "const" not passing the test

--- src/multipatch/interface_derivatives/single_interface_derivatives_calculator.hpp
+++ src/multipatch/interface_derivatives/single_interface_derivatives_calculator.hpp
@@ -1190,13 +1190,13 @@ private:
     }
 
     /// @brief Compute the coefficient alpha_i.
-    static double get_alpha(double const cell_length_left, double const cell_length_right) const
+    static double get_alpha(double const cell_length_left, double const cell_length_right)
     {
         return -0.5 * cell_length_left / (cell_length_right + cell_length_left);
     }
 
     /// @brief Compute the coefficient beta_i.
-    static double get_beta(double const cell_length_left, double const cell_length_right) const
+    static double get_beta(double const cell_length_left, double const cell_length_right)
     {
         return -0.5 * cell_length_right / (cell_length_right + cell_length_left);
     }
