a924a0bcda74332d0175349e3b40863f5a4a58a3
Emily Bourne <emily.bourne@epfl.ch>
2026-09-25 10:08:50 +0200

    Use this-> over base_type:: to stop confusing linter

--- src/data_types/derivative_field_mem.hpp
+++ src/data_types/derivative_field_mem.hpp
@@ -211,14 +211,14 @@ private:
                     & (1 << ddc::type_seq_rank_v<
                                ddc::Deriv<typename QueryDDim::continuous_dimension_type>,
                                deriv_tags>)) {
-                IdxRangeSlice<QueryDDim> idx_range_local(base_type::m_cross_derivative_idx_range);
+                IdxRangeSlice<QueryDDim> idx_range_local(this->m_cross_derivative_idx_range);
                 return idx_range_local.extents().value();
             } else {
-                IdxRange<QueryDDim> idx_range_local(base_type::m_physical_idx_range);
+                IdxRange<QueryDDim> idx_range_local(this->m_physical_idx_range);
                 return idx_range_local.extents().value();
             }
         } else {
-            IdxRange<QueryDDim> idx_range_local(base_type::m_physical_idx_range);
+            IdxRange<QueryDDim> idx_range_local(this->m_physical_idx_range);
             return idx_range_local.extents().value();
         }
     }
@@ -244,8 +244,7 @@ private:
     template <std::size_t... ArrayIndex>
     void initialise_chunks(allocator_type allocator, std::index_sequence<ArrayIndex...>)
     {
-        ((base_type::internal_fields[ArrayIndex] = make_internal_mdspan<ArrayIndex>(allocator)),
-         ...);
+        ((this->internal_fields[ArrayIndex] = make_internal_mdspan<ArrayIndex>(allocator)), ...);
     }
 
     /// @brief Free the internal pointer that is saved in internal_fields at the index ArrayIndex.
@@ -260,8 +259,7 @@ private:
     template <std::size_t... ArrayIndex>
     void free_chunks(allocator_type allocator, std::index_sequence<ArrayIndex...>)
     {
-        (free_ptr<ArrayIndex>(base_type::internal_fields[ArrayIndex].data_handle(), allocator),
-         ...);
+        (free_ptr<ArrayIndex>(this->internal_fields[ArrayIndex].data_handle(), allocator), ...);
     }
 
 public:
@@ -337,7 +335,7 @@ public:
         static_assert((ddc::is_discrete_element_v<DElem> && ...));
         using full_index_type = detail::combine_t<DElem...>;
         full_index_type elem(elems...);
-        return base_type::get_internal_field(elem)();
+        return this->get_internal_field(elem)();
     }
 
     /**
@@ -355,7 +353,7 @@ public:
         static_assert((ddc::is_discrete_element_v<DElem> && ...));
         using full_index_type = detail::combine_t<DElem...>;
         full_index_type elem(elems...);
-        return base_type::get_internal_field(elem)();
+        return this->get_internal_field(elem)();
     }
 
     /**
@@ -370,7 +368,7 @@ public:
     template <class... QueryDDims>
     constexpr auto operator[](Idx<QueryDDims...> const& slice_spec) const
     {
-        return get_const_field(base_type::get_internal_field(slice_spec));
+        return get_const_field(this->get_internal_field(slice_spec));
     }
 
     /**
@@ -385,7 +383,7 @@ public:
     template <class... QueryDDims>
     constexpr auto operator[](Idx<QueryDDims...> const& slice_spec)
     {
-        return base_type::get_internal_field(slice_spec);
+        return this->get_internal_field(slice_spec);
     }
 
     /**
@@ -402,7 +400,7 @@ public:
     template <class... QueryDDims>
     KOKKOS_FUNCTION constexpr auto operator[](IdxRange<QueryDDims...> const& oidx_range)
     {
-        return base_type::get_internal_field(oidx_range);
+        return this->get_internal_field(oidx_range);
     }
 
     /**
@@ -419,7 +417,7 @@ public:
     template <class... QueryDDims>
     KOKKOS_FUNCTION constexpr auto operator[](IdxRange<QueryDDims...> const& oidx_range) const
     {
-        return get_const_field(base_type::get_internal_field(oidx_range));
+        return get_const_field(this->get_internal_field(oidx_range));
     }
 
     /**
