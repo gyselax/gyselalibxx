64cd126d4fd9caf6870d9d95430361e956f3d60e
Emily Bourne <emily.bourne@epfl.ch>
2026-09-25 10:13:03 +0200

    Use this-> over base_type:: to stop confusing linter

--- src/data_types/vector_field.hpp
+++ src/data_types/vector_field.hpp
@@ -213,7 +213,7 @@ private:
             Idx<ODDims...> const& delems,
             std::integer_sequence<T, ints...>) const noexcept
     {
-        return element_type((base_type::m_values[ints](delems))...);
+        return element_type((this->m_values[ints](delems))...);
     }
 
 public:
@@ -404,7 +404,7 @@ public:
     inline constexpr chunk_span_type get() const noexcept
     {
         static_assert(ddc::in_tags_v<QueryTag, NDTypeTag>, "requested Tag absent from Vector");
-        return ::get_field(base_type::m_values[ddc::type_seq_rank_v<QueryTag, NDTypeTag>]);
+        return ::get_field(this->m_values[ddc::type_seq_rank_v<QueryTag, NDTypeTag>]);
     }
 };
 
